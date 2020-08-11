4886d41e2e4dbfff44ad67812c85b3085c2e8f34
Alex Vanin <alexey@nspcc.ru>
2020-08-11 15:10:01 +0300

    [#7] Add container processor for inner ring
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

---
+++ pkg/innerring/processors/container/process_container.go
@@ -0,0 +1,26 @@
+package container
+
+import (
+	"github.com/nspcc-dev/neofs-node/pkg/innerring/invoke"
+	containerEvent "github.com/nspcc-dev/neofs-node/pkg/morph/event/container"
+	"go.uber.org/zap"
+)
+
+// Process new container from the user by checking container sanity
+// and sending approve tx back to morph.
+func (cp *Processor) processContainerPut(put *containerEvent.Put) {
+	if !cp.activeState.IsActive() {
+		cp.log.Info("passive mode, ignore container put")
+		return
+	}
+
+	err := invoke.RegisterContainer(cp.morphClient, cp.containerContract,
+		&invoke.ContainerParams{
+			Key:       put.PublicKey(),
+			Container: put.Container(),
+			Signature: put.Signature(),
+		})
+	if err != nil {
+		cp.log.Error("can't invoke new container", zap.Error(err))
+	}
+}
