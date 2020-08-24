f5a0e354ef745c683233cb911d490e80baa21469
Leonard Lyubich <leonard@nspcc.ru>
2020-08-24 18:50:57 +0300

    [#12] services/session: Implement service executor
    Implement Session service executor that generates private token and stores
    it to map.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/services/session/storage/storage.go
@@ -0,0 +1,51 @@
+package storage
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/mr-tron/base58"
+	"github.com/nspcc-dev/neofs-api-go/v2/refs"
+)
+
+type key struct {
+	tokenID string
+	ownerID string
+}
+
+type TokenStore struct {
+	mtx *sync.RWMutex
+
+	tokens map[key]*PrivateToken
+}
+
+var ErrNotFound = errors.New("private token not found")
+
+// New creates, initializes and returns a new TokenStore instance.
+//
+// The elements of the instance are stored in the map.
+func New() *TokenStore {
+	return &TokenStore{
+		mtx:    new(sync.RWMutex),
+		tokens: make(map[key]*PrivateToken),
+	}
+}
+
+// Get returns private token corresponding to the given identifiers.
+//
+// Returns nil is there is no element in storage.
+func (s *TokenStore) Get(ownerID *refs.OwnerID, tokenID []byte) *PrivateToken {
+	ownerBytes, err := ownerID.StableMarshal(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	s.mtx.RLock()
+	t := s.tokens[key{
+		tokenID: base58.Encode(tokenID),
+		ownerID: base58.Encode(ownerBytes),
+	}]
+	s.mtx.RUnlock()
+
+	return t
+}
