7f80e3623e192431cda2b7fdc0e804623620fde9
Leonard Lyubich <leonard@nspcc.ru>
2020-08-26 13:00:41 +0300

    [#13] services/util: Support client-side stream in SignService
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/services/util/sign.go
+++ pkg/services/util/sign.go
@@ -14,14 +14,26 @@ type SignService struct {
 	key *ecdsa.PrivateKey
 }
 
-type ServerStreamHandler func(context.Context, interface{}) (MessageReader, error)
+type ServerStreamHandler func(context.Context, interface{}) (ResponseMessageReader, error)
 
-type MessageReader func() (interface{}, error)
+type ResponseMessageReader func() (interface{}, error)
 
-type MessageStreamer struct {
+type ResponseMessageStreamer struct {
 	key *ecdsa.PrivateKey
 
-	recv MessageReader
+	recv ResponseMessageReader
+}
+
+type RequestMessageWriter func(interface{}) error
+
+type ClientStreamCloser func() (interface{}, error)
+
+type RequestMessageStreamer struct {
+	key *ecdsa.PrivateKey
+
+	send RequestMessageWriter
+
+	close ClientStreamCloser
 }
 
 func NewUnarySignService(key *ecdsa.PrivateKey) *SignService {
@@ -30,7 +42,37 @@ func NewUnarySignService(key *ecdsa.PrivateKey) *SignService {
 	}
 }
 
-func (s *MessageStreamer) Recv() (interface{}, error) {
+func (s *RequestMessageStreamer) Send(req interface{}) error {
+	// verify request signatures
+	if err := signature.VerifyServiceMessage(req); err != nil {
+		return errors.Wrap(err, "could not verify request")
+	}
+
+	return s.send(req)
+}
+
+func (s *RequestMessageStreamer) CloseAndRecv() (interface{}, error) {
+	resp, err := s.close()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not close stream and receive response")
+	}
+
+	if err := signature.SignServiceMessage(s.key, resp); err != nil {
+		return nil, errors.Wrap(err, "could not sign response")
+	}
+
+	return resp, nil
+}
+
+func (s *SignService) CreateRequestStreamer(sender RequestMessageWriter, closer ClientStreamCloser) *RequestMessageStreamer {
+	return &RequestMessageStreamer{
+		key:   s.key,
+		send:  sender,
+		close: closer,
+	}
+}
+
+func (s *ResponseMessageStreamer) Recv() (interface{}, error) {
 	m, err := s.recv()
 	if err != nil {
 		return nil, errors.Wrap(err, "could not receive response message for signing")
@@ -43,7 +85,7 @@ func (s *MessageStreamer) Recv() (interface{}, error) {
 	return m, nil
 }
 
-func (s *SignService) HandleServerStreamRequest(ctx context.Context, req interface{}, handler ServerStreamHandler) (*MessageStreamer, error) {
+func (s *SignService) HandleServerStreamRequest(ctx context.Context, req interface{}, handler ServerStreamHandler) (*ResponseMessageStreamer, error) {
 	// verify request signatures
 	if err := signature.VerifyServiceMessage(req); err != nil {
 		return nil, errors.Wrap(err, "could not verify request")
@@ -54,7 +96,7 @@ func (s *SignService) HandleServerStreamRequest(ctx context.Context, req interfa
 		return nil, errors.Wrap(err, "could not create message reader")
 	}
 
-	return &MessageStreamer{
+	return &ResponseMessageStreamer{
 		key:  s.key,
 		recv: msgRdr,
 	}, nil
