0e498baa3b0f7cdd8f5bf21cb719e69a31da7367
Alex Vanin <alexey@nspcc.ru>
2020-08-27 13:54:26 +0300

    [#14] Use stackitems in morph/client converters
    In neo-go v0.91.0 testinvoke returns stackitems.Item
    interface, so converters should work with this type.
    
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/morph/client/util.go
+++ pkg/morph/client/util.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 
 	sc "github.com/nspcc-dev/neo-go/pkg/smartcontract"
+	"github.com/nspcc-dev/neo-go/pkg/vm/stackitem"
 	"github.com/pkg/errors"
 )
 
@@ -129,4 +130,71 @@ func StringFromStackParameter(param sc.Parameter) (string, error) {
 	default:
 		return "", errors.Errorf("chain/client: %s is not a string type", param.Type)
 	}
+}
+
+// BoolFromStackItem receives boolean value from the value of a smart contract parameter.
+func BoolFromStackItem(param stackitem.Item) (bool, error) {
+	switch param.Type() {
+	case stackitem.BooleanT, stackitem.IntegerT, stackitem.ByteArrayT:
+		return param.Bool(), nil
+	default:
+		return false, errors.Errorf("chain/client: %s is not a bool type", param.Type())
+	}
+}
+
+// IntFromStackItem receives numerical value from the value of a smart contract parameter.
+func IntFromStackItem(param stackitem.Item) (int64, error) {
+	switch param.Type() {
+	case stackitem.IntegerT, stackitem.ByteArrayT:
+		i, err := param.TryInteger()
+		if err != nil {
+			return 0, err
+		}
+
+		return i.Int64(), nil
+	default:
+		return 0, errors.Errorf("chain/client: %s is not an integer type", param.Type())
+	}
+}
+
+// BytesFromStackItem receives binary value from the value of a smart contract parameter.
+func BytesFromStackItem(param stackitem.Item) ([]byte, error) {
+	if param.Type() != stackitem.ByteArrayT {
+		if param.Type() == stackitem.AnyT && param.Value() == nil {
+			return nil, nil
+		}
+
+		return nil, errors.Errorf("chain/client: %s is not a byte array type", param.Type())
+	}
+
+	return param.TryBytes()
+}
+
+// ArrayFromStackItem returns the slice contract parameters from passed parameter.
+//
+// If passed parameter carries boolean false value, (nil, nil) returns.
+func ArrayFromStackItem(param stackitem.Item) ([]stackitem.Item, error) {
+	// if param.Type()
+	switch param.Type() {
+	case stackitem.AnyT:
+		return nil, nil
+	case stackitem.ArrayT:
+		items, ok := param.Value().([]stackitem.Item)
+		if !ok {
+			return nil, errors.Errorf("chain/client: can't convert %T to parameter slice", param.Value())
+		}
+
+		return items, nil
+	default:
+		return nil, errors.Errorf("chain/client: %s is not an array type", param.Type())
+	}
+}
+
+// StringFromStackItem receives string value from the value of a smart contract parameter.
+func StringFromStackItem(param stackitem.Item) (string, error) {
+	if param.Type() != stackitem.ByteArrayT {
+		return "", errors.Errorf("chain/client: %s is not an integer type", param.Type())
+	}
+
+	return stackitem.ToString(param)
 }
