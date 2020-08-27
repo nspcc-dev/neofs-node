0c06eafc60411bdf952c708e63961fd076a5a655
Alex Vanin <alexey@nspcc.ru>
2020-08-27 13:54:26 +0300

    [#14] Use stackitems in morph/client converters
    In neo-go v0.91.0 testinvoke returns stackitems.Item
    interface, so converters should work with this type.
    
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/morph/client/util_test.go
+++ pkg/morph/client/util_test.go
@@ -1,9 +1,11 @@
 package client
 
 import (
+	"math/big"
 	"testing"
 
 	sc "github.com/nspcc-dev/neo-go/pkg/smartcontract"
+	"github.com/nspcc-dev/neo-go/pkg/vm/stackitem"
 	"github.com/stretchr/testify/require"
 )
 
@@ -47,6 +49,15 @@ var (
 		Type:  sc.ArrayType,
 		Value: []sc.Parameter{intParam, byteArrayParam},
 	}
+
+	stringByteItem     = stackitem.NewByteArray([]byte("Hello World"))
+	intItem            = stackitem.NewBigInteger(new(big.Int).SetInt64(1))
+	byteWithIntItem    = stackitem.NewByteArray([]byte{0x0a})
+	emptyByteArrayItem = stackitem.NewByteArray([]byte{})
+	trueBoolItem       = stackitem.NewBool(true)
+	falseBoolItem      = stackitem.NewBool(false)
+	arrayItem          = stackitem.NewArray([]stackitem.Item{intItem, stringByteItem})
+	anyTypeItem        = stackitem.Null{}
 )
 
 func TestBoolFromStackParameter(t *testing.T) {
@@ -140,6 +151,97 @@ func TestStringFromStackParameter(t *testing.T) {
 
 	t.Run("incorrect assert", func(t *testing.T) {
 		_, err := StringFromStackParameter(intParam)
+		require.Error(t, err)
+	})
+}
+
+func TestBoolFromStackItem(t *testing.T) {
+	t.Run("true assert", func(t *testing.T) {
+		val, err := BoolFromStackItem(trueBoolItem)
+		require.NoError(t, err)
+		require.True(t, val)
+
+		val, err = BoolFromStackItem(intItem)
+		require.NoError(t, err)
+		require.True(t, val)
+	})
+
+	t.Run("false assert", func(t *testing.T) {
+		val, err := BoolFromStackItem(falseBoolItem)
+		require.NoError(t, err)
+		require.False(t, val)
+
+		val, err = BoolFromStackItem(emptyByteArrayItem)
+		require.NoError(t, err)
+		require.False(t, val)
+	})
+
+	t.Run("incorrect assert", func(t *testing.T) {
+		_, err := BoolFromStackItem(arrayItem)
+		require.Error(t, err)
+	})
+}
+
+func TestArrayFromStackItem(t *testing.T) {
+	t.Run("correct assert", func(t *testing.T) {
+		val, err := ArrayFromStackItem(arrayItem)
+		require.NoError(t, err)
+		require.Len(t, val, len(arrayItem.Value().([]stackitem.Item)))
+	})
+	t.Run("incorrect assert", func(t *testing.T) {
+		_, err := ArrayFromStackItem(stringByteItem)
+		require.Error(t, err)
+	})
+	t.Run("nil array case", func(t *testing.T) {
+		val, err := ArrayFromStackItem(anyTypeItem)
+		require.NoError(t, err)
+		require.Nil(t, val)
+	})
+}
+
+func TestBytesFromStackItem(t *testing.T) {
+	t.Run("correct assert", func(t *testing.T) {
+		val, err := BytesFromStackItem(stringByteItem)
+		require.NoError(t, err)
+		require.Equal(t, stringByteItem.Value().([]byte), val)
+	})
+
+	t.Run("incorrect assert", func(t *testing.T) {
+		_, err := BytesFromStackItem(arrayItem)
+		require.Error(t, err)
+	})
+}
+
+func TestIntFromStackItem(t *testing.T) {
+	t.Run("correct assert", func(t *testing.T) {
+		val, err := IntFromStackItem(intItem)
+		require.NoError(t, err)
+		require.Equal(t, intItem.Value().(*big.Int).Int64(), val)
+
+		val, err = IntFromStackItem(byteWithIntItem)
+		require.NoError(t, err)
+		require.Equal(t, int64(0x0a), val)
+
+		val, err = IntFromStackItem(emptyByteArrayItem)
+		require.NoError(t, err)
+		require.Equal(t, int64(0), val)
+	})
+
+	t.Run("incorrect assert", func(t *testing.T) {
+		_, err := IntFromStackItem(arrayItem)
+		require.Error(t, err)
+	})
+}
+
+func TestStringFromStackItem(t *testing.T) {
+	t.Run("correct assert", func(t *testing.T) {
+		val, err := StringFromStackItem(stringByteItem)
+		require.NoError(t, err)
+		require.Equal(t, string(stringByteItem.Value().([]byte)), val)
+	})
+
+	t.Run("incorrect assert", func(t *testing.T) {
+		_, err := StringFromStackItem(intItem)
 		require.Error(t, err)
 	})
 }
