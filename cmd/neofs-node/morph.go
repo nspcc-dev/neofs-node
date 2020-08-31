a76a97ec014c81692fa165f37c55f30253fee141
Alex Vanin <alexey@nspcc.ru>
2020-08-31 18:20:02 +0300

    [#15] Add bootstrap routine to node application
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- cmd/neofs-node/morph.go
+++ cmd/neofs-node/morph.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"github.com/nspcc-dev/neo-go/pkg/util"
+	crypto "github.com/nspcc-dev/neofs-crypto"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/client"
+	"github.com/nspcc-dev/neofs-node/pkg/morph/client/netmap"
 )
 
 func initMorphComponents(c *cfg) {
 	var err error
 
 	c.cfgMorph.client, err = client.New(c.key, c.cfgMorph.endpoint)
-
 	fatalOnErr(err)
+}
+
+func bootstrapNode(c *cfg) {
+	if c.cfgNodeInfo.bootType == StorageNode {
+		u160, err := util.Uint160DecodeStringLE(c.cfgNetmap.scriptHash)
+		fatalOnErr(err)
+
+		staticClient, err := client.NewStatic(c.cfgMorph.client, u160, c.cfgContainer.fee)
+		fatalOnErr(err)
+
+		cli, err := netmap.New(staticClient)
+		fatalOnErr(err)
+
+		peerInfo := new(netmap.PeerInfo)
+		peerInfo.SetAddress(c.cfgNodeInfo.address)
+		peerInfo.SetPublicKey(crypto.MarshalPublicKey(&c.key.PublicKey))
+		// todo: add attributes as opts
+
+		args := new(netmap.AddPeerArgs)
+		args.SetInfo(*peerInfo)
+		err = cli.AddPeer(*args)
+		fatalOnErr(err)
+	}
 }
