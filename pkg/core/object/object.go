f26330355d42f889b6ee4c1ecbbc4d7c9732b5a8
Leonard Lyubich <leonard@nspcc.ru>
2020-09-02 15:45:46 +0300

    [#17] core/object: Implement Object and Address types
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/core/object/object.go
@@ -0,0 +1,63 @@
+package object
+
+import (
+	"github.com/nspcc-dev/neofs-api-go/pkg/object"
+	objectV2 "github.com/nspcc-dev/neofs-api-go/v2/object"
+)
+
+// Object represents the NeoFS object.
+//
+// Object inherits object type from NeoFS SDK.
+// It is used to implement some useful methods and functions
+// for convenient processing of an object by a node.
+type Object struct {
+	*object.Object
+}
+
+// MarshalStableV2 marshals Object to v2 binary format.
+func (o *Object) MarshalStableV2() ([]byte, error) {
+	if o != nil {
+		return o.ToV2().StableMarshal(nil)
+	}
+
+	return nil, nil
+}
+
+// Address returns address of the object.
+func (o *Object) Address() *Address {
+	if o != nil {
+		return &Address{
+			Address: o.Object.Address(),
+		}
+	}
+
+	return nil
+}
+
+// FromV2 converts v2 Object message to Object.
+func FromV2(oV2 *objectV2.Object) (*Object, error) {
+	if oV2 == nil {
+		return nil, nil
+	}
+
+	o, err := object.FromV2(oV2)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Object{
+		Object: o,
+	}, nil
+}
+
+// FromBytes restores Object from binary format.
+func FromBytes(data []byte) (*Object, error) {
+	o, err := object.FromBytes(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Object{
+		Object: o,
+	}, nil
+}
