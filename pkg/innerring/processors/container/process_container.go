0ab13d80366086b5795dca58f45767f8b087e453
Alex Vanin <alexey@nspcc.ru>
2020-09-02 19:04:29 +0300

    [#7] Add container delete notification handler
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/innerring/processors/container/process_container.go
+++ pkg/innerring/processors/container/process_container.go
@@ -24,3 +24,21 @@ func (cp *Processor) processContainerPut(put *containerEvent.Put) {
 		cp.log.Error("can't invoke new container", zap.Error(err))
 	}
 }
+
+// Process delete container operation from the user by checking container sanity
+// and sending approve tx back to morph.
+func (cp *Processor) processContainerDelete(delete *containerEvent.Delete) {
+	if !cp.activeState.IsActive() {
+		cp.log.Info("passive mode, ignore container put")
+		return
+	}
+
+	err := invoke.RemoveContainer(cp.morphClient, cp.containerContract,
+		&invoke.RemoveContainerParams{
+			ContainerID: delete.ContainerID(),
+			Signature:   delete.Signature(),
+		})
+	if err != nil {
+		cp.log.Error("can't invoke delete container", zap.Error(err))
+	}
+}
