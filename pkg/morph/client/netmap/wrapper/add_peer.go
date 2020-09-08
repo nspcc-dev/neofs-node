7af997586d2a00072ab79368dd271e48e400e1ad
Alex Vanin <alexey@nspcc.ru>
2020-09-08 16:14:22 +0300

    [#15] Fix wrapper for add peer contract method
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/morph/client/netmap/wrapper/add_peer.go
+++ pkg/morph/client/netmap/wrapper/add_peer.go
@@ -1,10 +1,27 @@
 package wrapper
 
-// NodeInfo groups information about NeoFS storage node.
-type NodeInfo struct{}
+import (
+	"github.com/nspcc-dev/neofs-node/pkg/morph/client/netmap"
+	"github.com/pkg/errors"
+)
 
 // AddPeer registers peer in NeoFS network through
 // Netmap contract call.
-func (w *Wrapper) AddPeer(nodeInfo NodeInfo) error {
-	panic("implement me")
+func (w *Wrapper) AddPeer(nodeInfo *netmap.NodeInfo) error {
+	if nodeInfo == nil {
+		return errors.New("nil node info")
+	}
+
+	rawNodeInfo, err := nodeInfo.StableMarshal(nil)
+	if err != nil {
+		return err
+	}
+
+	args := netmap.AddPeerArgs{}
+	args.SetInfo(rawNodeInfo)
+
+	return errors.Wrap(
+		w.client.AddPeer(args),
+		"could not invoke smart contract",
+	)
 }
