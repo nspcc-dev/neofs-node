39a35d6718ba7db2e3ac3e0de9d88b63c74f5666
Alex Vanin <alexey@nspcc.ru>
2020-09-08 12:37:53 +0300

    [#15] Use api-go definition of NodeInfo in node
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/morph/client/netmap/netmap.go
+++ pkg/morph/client/netmap/netmap.go
@@ -14,20 +14,20 @@ type GetNetMapArgs struct {
 // GetNetMapValues groups the stack parameters
 // returned by get network map test invoke.
 type GetNetMapValues struct {
-	peers []PeerInfo // peer list in a binary format
+	peers [][]byte
 }
 
-const nodeInfoFixedPrmNumber = 3
+const nodeInfoFixedPrmNumber = 1
 
 // Peers return the list of peers from
 // network map in a binary format.
-func (g GetNetMapValues) Peers() []PeerInfo {
+func (g GetNetMapValues) Peers() [][]byte {
 	return g.peers
 }
 
 // NetMap performs the test invoke of get network map
 // method of NeoFS Netmap contract.
-func (c *Client) NetMap(args GetNetMapArgs) (*GetNetMapValues, error) {
+func (c *Client) NetMap(_ GetNetMapArgs) (*GetNetMapValues, error) {
 	prms, err := c.client.TestInvoke(
 		c.netMapMethod,
 	)
@@ -43,7 +43,7 @@ func (c *Client) NetMap(args GetNetMapArgs) (*GetNetMapValues, error) {
 	}
 
 	res := &GetNetMapValues{
-		peers: make([]PeerInfo, 0, len(prms)),
+		peers: make([][]byte, 0, len(prms)),
 	}
 
 	for i := range prms {
@@ -52,48 +52,19 @@ func (c *Client) NetMap(args GetNetMapArgs) (*GetNetMapValues, error) {
 			return nil, errors.Wrapf(err, "could not parse stack item (Peer #%d)", i)
 		}
 
-		res.peers = append(res.peers, *peer)
+		res.peers = append(res.peers, peer)
 	}
 
 	return res, nil
 }
 
-func peerInfoFromStackItem(prm stackitem.Item) (*PeerInfo, error) {
+func peerInfoFromStackItem(prm stackitem.Item) ([]byte, error) {
 	prms, err := client.ArrayFromStackItem(prm)
 	if err != nil {
 		return nil, errors.Wrapf(err, "could not get stack item array (PeerInfo)")
 	} else if ln := len(prms); ln != nodeInfoFixedPrmNumber {
-		return nil, errors.Errorf("unexpected stack item count (PeerInfo): expected %d, has %d", 3, ln)
+		return nil, errors.Errorf("unexpected stack item count (PeerInfo): expected %d, has %d", 1, ln)
 	}
 
-	res := new(PeerInfo)
-
-	// Address
-	res.address, err = client.BytesFromStackItem(prms[0])
-	if err != nil {
-		return nil, errors.Wrap(err, "could not get byte array from stack item (Address)")
-	}
-
-	// Public key
-	if res.key, err = client.BytesFromStackItem(prms[1]); err != nil {
-		return nil, errors.Wrap(err, "could not get byte array from stack item (Public key)")
-	}
-
-	// Options
-	if prms, err = client.ArrayFromStackItem(prms[2]); err != nil {
-		return nil, errors.Wrapf(err, "could not get stack item array (Options)")
-	}
-
-	res.opts = make([][]byte, 0, len(prms))
-
-	for i := range prms {
-		opt, err := client.BytesFromStackItem(prms[i])
-		if err != nil {
-			return nil, errors.Wrapf(err, "could not get byte array from stack item (Option #%d)", i)
-		}
-
-		res.opts = append(res.opts, opt)
-	}
-
-	return res, nil
+	return client.BytesFromStackItem(prms[0])
 }
