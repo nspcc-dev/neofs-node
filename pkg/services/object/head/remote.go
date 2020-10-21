5017ff0e4abc7ca4c3b5729f79a4a59769ef44c4
Leonard Lyubich <leonard@nspcc.ru>
2020-10-21 12:18:36 +0300

    [#108] object/head: Export remote header retrieval utility
    Export remote head functionality in headsvc package. Refactor head service
    to use RemoteHeader.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/services/object/head/remote.go
+++ pkg/services/object/head/remote.go
@@ -4,53 +4,85 @@ import (
 	"context"
 
 	"github.com/nspcc-dev/neofs-api-go/pkg/client"
+	objectSDK "github.com/nspcc-dev/neofs-api-go/pkg/object"
 	"github.com/nspcc-dev/neofs-node/pkg/core/object"
 	"github.com/nspcc-dev/neofs-node/pkg/network"
 	"github.com/nspcc-dev/neofs-node/pkg/services/object/util"
 	"github.com/pkg/errors"
 )
 
-type remoteHeader struct {
+// RemoteHeader represents utility for getting
+// the object header from a remote host.
+type RemoteHeader struct {
 	keyStorage *util.KeyStorage
+}
+
+// RemoteHeadPrm groups remote header operation parameters.
+type RemoteHeadPrm struct {
+	commonHeadPrm *Prm
 
 	node *network.Address
 }
 
-func (h *remoteHeader) head(ctx context.Context, prm *Prm, handler func(*object.Object)) error {
-	key, err := h.keyStorage.GetKey(prm.common.SessionToken())
-	if err != nil {
-		return errors.Wrapf(err, "(%T) could not receive private key", h)
+// NewRemoteHeader creates, initializes and returns new RemoteHeader instance.
+func NewRemoteHeader(keyStorage *util.KeyStorage) *RemoteHeader {
+	return &RemoteHeader{
+		keyStorage: keyStorage,
+	}
+}
+
+// WithNodeAddress sets network address of the remote node.
+func (p *RemoteHeadPrm) WithNodeAddress(v *network.Address) *RemoteHeadPrm {
+	if p != nil {
+		p.node = v
 	}
 
-	addr, err := h.node.IPAddrString()
+	return p
+}
+
+// WithObjectAddress sets object address.
+func (p *RemoteHeadPrm) WithObjectAddress(v *objectSDK.Address) *RemoteHeadPrm {
+	if p != nil {
+		p.commonHeadPrm = new(Prm).WithAddress(v)
+	}
+
+	return p
+}
+
+// Head requests object header from the remote node.
+func (h *RemoteHeader) Head(ctx context.Context, prm *RemoteHeadPrm) (*object.Object, error) {
+	key, err := h.keyStorage.GetKey(prm.commonHeadPrm.common.SessionToken())
 	if err != nil {
-		return err
+		return nil, errors.Wrapf(err, "(%T) could not receive private key", h)
+	}
+
+	addr, err := prm.node.IPAddrString()
+	if err != nil {
+		return nil, err
 	}
 
 	c, err := client.New(key,
 		client.WithAddress(addr),
 	)
 	if err != nil {
-		return errors.Wrapf(err, "(%T) could not create SDK client %s", h, addr)
+		return nil, errors.Wrapf(err, "(%T) could not create SDK client %s", h, addr)
 	}
 
 	p := new(client.ObjectHeaderParams).
-		WithAddress(prm.addr)
+		WithAddress(prm.commonHeadPrm.addr)
 
-	if prm.short {
+	if prm.commonHeadPrm.short {
 		p = p.WithMainFields()
 	}
 
 	hdr, err := c.GetObjectHeader(ctx, p,
 		client.WithTTL(1), // FIXME: use constant
-		client.WithSession(prm.common.SessionToken()),
-		client.WithBearer(prm.common.BearerToken()),
+		client.WithSession(prm.commonHeadPrm.common.SessionToken()),
+		client.WithBearer(prm.commonHeadPrm.common.BearerToken()),
 	)
 	if err != nil {
-		return errors.Wrapf(err, "(%T) could not head object in %s", h, addr)
+		return nil, errors.Wrapf(err, "(%T) could not head object in %s", h, addr)
 	}
 
-	handler(object.NewFromSDK(hdr))
-
-	return nil
+	return object.NewFromSDK(hdr), nil
 }
