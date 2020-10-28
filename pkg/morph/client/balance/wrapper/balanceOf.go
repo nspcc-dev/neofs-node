03f52bca0168380dcc54ead2684bab7207187141
Alex Vanin <alexey@nspcc.ru>
2020-10-28 16:01:29 +0300

    [#122] Return big.Int in `balanceOf` function
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/morph/client/balance/wrapper/balanceOf.go
+++ pkg/morph/client/balance/wrapper/balanceOf.go
@@ -1,16 +1,18 @@
 package wrapper
 
 import (
+	"math/big"
+
 	"github.com/nspcc-dev/neofs-api-go/pkg/owner"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/client/balance"
 )
 
 // BalanceOf receives the amount of funds in the client's account
 // through the Balance contract call, and returns it.
-func (w *Wrapper) BalanceOf(id *owner.ID) (int64, error) {
+func (w *Wrapper) BalanceOf(id *owner.ID) (*big.Int, error) {
 	v, err := owner.ScriptHashBE(id)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
 	args := balance.GetBalanceOfArgs{}
@@ -18,7 +20,7 @@ func (w *Wrapper) BalanceOf(id *owner.ID) (int64, error) {
 
 	result, err := w.client.BalanceOf(args)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
 	return result.Amount(), nil
