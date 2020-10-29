559826242075f8185e7677e78c51b4458049e072
Leonard Lyubich <leonard@nspcc.ru>
2020-10-29 18:01:58 +0300

    [#125] services/eacl: Use latest object header keys
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/services/object/acl/eacl/v2/object.go
+++ pkg/services/object/acl/eacl/v2/object.go
@@ -6,6 +6,7 @@ import (
 	"github.com/nspcc-dev/neofs-api-go/pkg/container"
 	objectSDK "github.com/nspcc-dev/neofs-api-go/pkg/object"
 	"github.com/nspcc-dev/neofs-api-go/pkg/owner"
+	"github.com/nspcc-dev/neofs-api-go/v2/acl"
 	"github.com/nspcc-dev/neofs-node/pkg/core/object"
 	"github.com/nspcc-dev/neofs-node/pkg/services/object/acl/eacl"
 )
@@ -44,31 +45,27 @@ func headersFromObject(obj *object.Object) []eacl.Header {
 
 	for ; obj != nil; obj = obj.GetParent() {
 		res = append(res,
-			// object ID
-			&sysObjHdr{
-				k: objectSDK.HdrSysNameID,
-				v: idValue(obj.GetID()),
-			},
 			// container ID
 			&sysObjHdr{
-				k: objectSDK.HdrSysNameCID,
+				k: acl.FilterObjectContainerID,
 				v: cidValue(obj.GetContainerID()),
 			},
 			// owner ID
 			&sysObjHdr{
-				k: objectSDK.HdrSysNameOwnerID,
+				k: acl.FilterObjectOwnerID,
 				v: ownerIDValue(obj.GetOwnerID()),
 			},
 			// creation epoch
 			&sysObjHdr{
-				k: objectSDK.HdrSysNameCreatedEpoch,
+				k: acl.FilterObjectCreationEpoch,
 				v: u64Value(obj.GetCreationEpoch()),
 			},
 			// payload size
 			&sysObjHdr{
-				k: objectSDK.HdrSysNamePayloadLength,
+				k: acl.FilterObjectPayloadLength,
 				v: u64Value(obj.GetPayloadSize()),
 			},
+			// TODO: add others fields after neofs-api#84
 		)
 
 		attrs := obj.GetAttributes()
