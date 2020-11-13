d02c24db5c6b97e1a037636bba6a0201a8cfdd48
Alex Vanin <alexey@nspcc.ru>
2020-11-13 14:37:05 +0300

    [#170] Add alphabet vote invocation method
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/innerring/invoke/alphabet.go
+++ pkg/innerring/invoke/alphabet.go
@@ -1,12 +1,14 @@
 package invoke
 
 import (
+	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
 	"github.com/nspcc-dev/neo-go/pkg/util"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/client"
 )
 
 const (
 	emitMethod = "emit"
+	voteMethod = "vote"
 )
 
 // AlphabetEmit invokes emit method on alphabet contract.
@@ -17,4 +19,14 @@ func AlphabetEmit(cli *client.Client, con util.Uint160) error {
 
 	// there is no signature collecting, so we don't need extra fee
 	return cli.Invoke(con, 0, emitMethod)
+}
+
+// AlphabetVote invokes vote method on alphabet contract.
+func AlphabetVote(cli *client.Client, con util.Uint160, key keys.PublicKey) error {
+	if cli == nil {
+		return client.ErrNilClient
+	}
+
+	// there is no signature collecting, so we don't need extra fee
+	return cli.Invoke(con, 0, voteMethod, key.Bytes())
 }
