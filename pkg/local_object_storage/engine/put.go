f743e1b7ee898292519e2038087737ce2df52000
Leonard Lyubich <leonard@nspcc.ru>
2020-11-17 15:26:03 +0300

    [#176] localstore: Draft storage engine structure and ops
    Implement the primary structure and operation of the local object
    storage engine.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/local_object_storage/engine/put.go
@@ -0,0 +1,94 @@
+package engine
+
+import (
+	"errors"
+
+	"github.com/nspcc-dev/neofs-node/pkg/core/object"
+	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/shard"
+	"go.uber.org/zap"
+)
+
+// PutPrm groups the parameters of Put operation.
+type PutPrm struct {
+	obj *object.Object
+}
+
+// PutRes groups resulting values of Put operation.
+type PutRes struct{}
+
+var errPutShard = errors.New("could not put object to any shard")
+
+// WithObject is a Put option to set object to save.
+//
+// Option is required.
+func (p *PutPrm) WithObject(obj *object.Object) *PutPrm {
+	if p != nil {
+		p.obj = obj
+	}
+
+	return p
+}
+
+// Put saves the object to local storage.
+//
+// Returns any error encountered that
+// did not allow to completely save the object.
+func (e *StorageEngine) Put(prm *PutPrm) (*PutRes, error) {
+	e.mtx.Lock()
+	defer e.mtx.Unlock()
+
+	// choose shards through sorting by weight
+	sortedShards := e.sortShardsByWeight(prm.obj.Address())
+
+	// check object existence
+	if e.objectExists(prm.obj, sortedShards) {
+		return nil, nil
+	}
+
+	shPrm := new(shard.PutPrm)
+
+	// save the object into the "largest" possible shard
+	for _, sh := range sortedShards {
+		_, err := sh.Put(
+			shPrm.WithObject(prm.obj),
+		)
+
+		if err != nil {
+			// TODO: smth wrong with shard, need to be processed
+			e.log.Warn("could not save object in shard",
+				zap.Stringer("shard", sh.ID()),
+				zap.String("error", err.Error()),
+			)
+		} else {
+			return nil, nil
+		}
+	}
+
+	return nil, errPutShard
+}
+
+func (e *StorageEngine) objectExists(obj *object.Object, shards []*shard.Shard) bool {
+	exists := false
+
+	for _, sh := range shards {
+		res, err := sh.Exists(
+			new(shard.ExistsPrm).
+				WithAddress(obj.Address()),
+		)
+
+		if err != nil {
+			// TODO: smth wrong with shard, need to be processed
+			e.log.Warn("could not check object existence",
+				zap.String("error", err.Error()),
+			)
+
+			continue
+		}
+
+		if exists = res.Exists(); exists {
+			break
+		}
+	}
+
+	return exists
+}
---
+++ pkg/local_object_storage/engine/shards.go
@@ -0,0 +1,71 @@
+package engine
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/nspcc-dev/hrw"
+	"github.com/nspcc-dev/neofs-api-go/pkg/object"
+	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/shard"
+	"github.com/pkg/errors"
+)
+
+// AddShard adds a new shard to the storage engine.
+//
+// Returns any error encountered that did not allow adding a shard.
+// Otherwise returns the ID of the added shard.
+func (e *StorageEngine) AddShard(opts ...shard.Option) (*shard.ID, error) {
+	e.mtx.Lock()
+	defer e.mtx.Unlock()
+
+	id, err := e.generateShardID()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not generate shard ID")
+	}
+
+	e.shards[id.String()] = shard.New(append(opts, shard.WithID(id))...)
+
+	return id, nil
+}
+
+func (e *StorageEngine) generateShardID() (*shard.ID, error) {
+	uid, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	bin, err := uid.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	return shard.NewIDFromBytes(bin), nil
+}
+
+func (e *StorageEngine) shardWeight(sh *shard.Shard) float64 {
+	weightValues := sh.WeightValues()
+
+	return float64(weightValues.FreeSpace)
+}
+
+func (e *StorageEngine) sortShardsByWeight(objAddr fmt.Stringer) []*shard.Shard {
+	shards := make([]*shard.Shard, 0, len(e.shards))
+	weights := make([]float64, 0, len(e.shards))
+
+	for _, sh := range e.shards {
+		shards = append(shards, sh)
+		weights = append(weights, e.shardWeight(sh))
+	}
+
+	hrw.SortSliceByWeightValue(shards, weights, hrw.Hash([]byte(objAddr.String())))
+
+	return shards
+}
+
+func (e *StorageEngine) iterateOverSortedShards(addr *object.Address, handler func(*shard.Shard) (stop bool)) {
+	for _, sh := range e.sortShardsByWeight(addr) {
+		if handler(sh) {
+			break
+		}
+	}
+}
