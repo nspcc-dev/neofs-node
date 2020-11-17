05db49d3421031103e2e1c4c3685e830e712f481
Leonard Lyubich <leonard@nspcc.ru>
2020-11-17 20:39:43 +0300

    [#176] localstore: Implement shard methods via Metabase and BlobStor
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/local_object_storage/shard/select.go
+++ pkg/local_object_storage/shard/select.go
@@ -2,6 +2,7 @@ package shard
 
 import (
 	objectSDK "github.com/nspcc-dev/neofs-api-go/pkg/object"
+	"github.com/pkg/errors"
 )
 
 // SelectPrm groups the parameters of Select operation.
@@ -36,7 +37,12 @@ func (s *Shard) Select(prm *SelectPrm) (*SelectRes, error) {
 	s.mtx.RLock()
 	defer s.mtx.RUnlock()
 
-	// FIXME: implement me
+	addrList, err := s.metaBase.Select(prm.filters)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not select objects from metabase")
+	}
 
-	return nil, nil
+	return &SelectRes{
+		addrList: addrList,
+	}, nil
 }
