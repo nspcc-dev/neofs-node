e2ebf84f4cad6317fa9772754d6ca4b5589ecc5b
Leonard Lyubich <leonard@nspcc.ru>
2020-11-17 15:23:15 +0300

    [#176] localstore: Draft storage engine shard structure and ops
    Implement the primary structure and operation of the local object
    storage shard.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/local_object_storage/shard/weight.go
@@ -0,0 +1,12 @@
+package shard
+
+// WeightValues groups values of Shard weight parameters.
+type WeightValues struct {
+	// Amount of free disk space. Measured in kilobytes.
+	FreeSpace uint64
+}
+
+// WeightValues returns current weight values of the Shard.
+func (s *Shard) WeightValues() WeightValues {
+	return s.weight
+}
