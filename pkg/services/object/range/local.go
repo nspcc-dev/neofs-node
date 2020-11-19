c97872e8f20da19b0d0c39bae717b971b3176a91
Leonard Lyubich <leonard@nspcc.ru>
2020-11-19 11:28:58 +0300

    [#186] object/range: Use new storage engine for work
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/services/object/range/local.go
+++ pkg/services/object/range/local.go
@@ -4,7 +4,7 @@ import (
 	"io"
 
 	"github.com/nspcc-dev/neofs-api-go/pkg/object"
-	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/localstore"
+	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/engine"
 	"github.com/pkg/errors"
 )
 
@@ -13,24 +13,16 @@ type localRangeWriter struct {
 
 	rng *object.Range
 
-	storage *localstore.Storage
+	storage *engine.StorageEngine
 }
 
 func (l *localRangeWriter) WriteTo(w io.Writer) (int64, error) {
-	obj, err := l.storage.Get(l.addr)
+	rngData, err := engine.GetRange(l.storage, l.addr, l.rng)
 	if err != nil {
 		return 0, errors.Wrapf(err, "(%T) could not get object from local storage", l)
 	}
 
-	payload := obj.Payload()
-	left := l.rng.GetOffset()
-	right := left + l.rng.GetLength()
-
-	if ln := uint64(len(payload)); ln < right {
-		return 0, errors.Errorf("(%T) object range is out-of-boundaries (size %d, range [%d:%d]", l, ln, left, right)
-	}
-
-	n, err := w.Write(payload[left:right])
+	n, err := w.Write(rngData)
 
 	return int64(n), err
 }
