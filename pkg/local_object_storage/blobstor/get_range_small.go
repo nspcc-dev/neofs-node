846a78fe45e55700f96d1323bc4891e7effb9281
Leonard Lyubich <leonard@nspcc.ru>
2020-11-24 17:19:44 +0300

    [#211] blobstor: Define GetRangeSmall operation interface
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/local_object_storage/blobstor/get_range_small.go
@@ -0,0 +1,24 @@
+package blobstor
+
+// GetRangeSmallPrm groups the parameters of GetRangeSmall operation.
+type GetRangeSmallPrm struct {
+	address
+	rwRange
+	rwBlobovniczaID
+}
+
+// GetRangeSmallRes groups resulting values of GetRangeSmall operation.
+type GetRangeSmallRes struct {
+	rangeData
+}
+
+// GetRangeSmall reads data of object payload range from blobovnicza of BLOB storage.
+//
+// If blobovnicza ID is not set or set to nil, BlobStor tries to get payload range
+// from any blobovnicza.
+//
+// Returns any error encountered that
+// did not allow to completely read the object payload range.
+func (b *BlobStor) GetRangeSmall(prm *GetRangeBigPrm) (*GetRangeBigRes, error) {
+	panic("implement me")
+}
