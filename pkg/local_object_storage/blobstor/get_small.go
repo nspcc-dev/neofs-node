2ba4ebc8c971088e9396fa11f36482cf4108cd10
Leonard Lyubich <leonard@nspcc.ru>
2020-11-24 17:02:41 +0300

    [#211] blobstor: Implement no-op GetSmall operation
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/local_object_storage/blobstor/get_small.go
@@ -0,0 +1,27 @@
+package blobstor
+
+import (
+	"github.com/pkg/errors"
+)
+
+// GetSmallPrm groups the parameters of GetSmallPrm operation.
+type GetSmallPrm struct {
+	address
+	rwBlobovniczaID
+}
+
+// GetBigRes groups resulting values of GetBig operation.
+type GetSmallRes struct {
+	roObject
+}
+
+// GetSmall reads the object from blobovnicza of BLOB storage by address.
+//
+// If blobovnicza ID is not set or set to nil, BlobStor tries to get object
+// from any blobovnicza.
+//
+// Returns any error encountered that
+// did not allow to completely read the object.
+func (b *BlobStor) GetSmall(prm *GetSmallPrm) (*GetSmallRes, error) {
+	return nil, errors.New("method GetSmall is not implemented yet")
+}
