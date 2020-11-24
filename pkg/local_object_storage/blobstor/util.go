c6fd6fc326081ae51736e147e42c5450f12afdef
Leonard Lyubich <leonard@nspcc.ru>
2020-11-24 17:02:41 +0300

    [#211] blobstor: Implement no-op GetSmall operation
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/local_object_storage/blobstor/util.go
+++ pkg/local_object_storage/blobstor/util.go
@@ -3,6 +3,7 @@ package blobstor
 import (
 	objectSDK "github.com/nspcc-dev/neofs-api-go/pkg/object"
 	"github.com/nspcc-dev/neofs-node/pkg/core/object"
+	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/blobovnicza"
 )
 
 type address struct {
@@ -30,4 +31,22 @@ type rwObject struct {
 // SetObject sets the object.
 func (o *rwObject) SetObject(obj *object.Object) {
 	o.obj = obj
+}
+
+type roBlobovniczaID struct {
+	blobovniczaID *blobovnicza.ID
+}
+
+// BlobovniczaID returns blobovnicza ID.
+func (v roBlobovniczaID) BlobovniczaID() *blobovnicza.ID {
+	return v.blobovniczaID
+}
+
+type rwBlobovniczaID struct {
+	roBlobovniczaID
+}
+
+// SetBlobovniczaID sets blobovnicza ID.
+func (v *rwBlobovniczaID) SetBlobovniczaID(id *blobovnicza.ID) {
+	v.blobovniczaID = id
 }
