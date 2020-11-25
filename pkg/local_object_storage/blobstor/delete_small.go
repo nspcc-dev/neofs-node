2bcd857d1f46b84f66b7827df69656d3caf40cf6
Leonard Lyubich <leonard@nspcc.ru>
2020-11-25 12:11:39 +0300

    [#211] blobstor: Define DeleteSmall operation
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/local_object_storage/blobstor/delete_small.go
@@ -0,0 +1,22 @@
+package blobstor
+
+// DeleteSmallPrm groups the parameters of DeleteSmall operation.
+type DeleteSmallPrm struct {
+	address
+}
+
+// DeleteSmallRes groups resulting values of DeleteSmall operation.
+type DeleteSmallRes struct{}
+
+// DeleteSmall removes object from blobovnicza of BLOB storage.
+//
+// If blobovnicza ID is not set or set to nil, BlobStor tries to
+// find and remove object from any blobovnicza.
+//
+// Returns any error encountered that did not allow
+// to completely remove the object.
+//
+// Returns ErrObjectNotFound if there is no object to delete.
+func (b *BlobStor) DeleteSmall(prm *DeleteSmallPrm) (*DeleteSmallRes, error) {
+	panic("implement me")
+}
