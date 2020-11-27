54cd91adffd3ae324dc6e0f352b731a720ed86af
Alex Vanin <alexey@nspcc.ru>
2020-11-27 12:03:35 +0300

    [#199] Simplify tests
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/local_object_storage/metabase/v2/inhume_test.go
+++ pkg/local_object_storage/metabase/v2/inhume_test.go
@@ -3,7 +3,6 @@ package meta_test
 import (
 	"testing"
 
-	"github.com/nspcc-dev/neofs-node/pkg/core/object"
 	meta "github.com/nspcc-dev/neofs-node/pkg/local_object_storage/metabase/v2"
 	"github.com/stretchr/testify/require"
 )
@@ -15,18 +14,17 @@ func TestDB_Inhume(t *testing.T) {
 	raw := generateRawObject(t)
 	addAttribute(raw, "foo", "bar")
 
-	obj := object.NewFromV2(raw.ToV2())
 	tombstoneID := generateAddress()
 
-	err := db.Put(obj, nil)
+	err := db.Put(raw.Object(), nil)
 	require.NoError(t, err)
 
-	err = db.Inhume(obj.Address(), tombstoneID)
+	err = db.Inhume(raw.Object().Address(), tombstoneID)
 	require.NoError(t, err)
 
-	_, err = db.Exists(obj.Address())
+	_, err = db.Exists(raw.Object().Address())
 	require.EqualError(t, err, meta.ErrAlreadyRemoved.Error())
 
-	_, err = db.Get(obj.Address())
+	_, err = db.Get(raw.Object().Address())
 	require.EqualError(t, err, meta.ErrAlreadyRemoved.Error())
 }
