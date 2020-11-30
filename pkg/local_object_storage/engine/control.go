30df0cb5dc6914f2dcf050417f160a7e4ac4d753
Leonard Lyubich <leonard@nspcc.ru>
2020-11-30 17:59:00 +0300

    [#220] localstorage: Implement Open/Init/Close methods
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/local_object_storage/engine/control.go
@@ -0,0 +1,51 @@
+package engine
+
+import (
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+// Open opens all StorageEngine's components.
+func (e *StorageEngine) Open() error {
+	e.mtx.RLock()
+	defer e.mtx.RUnlock()
+
+	for id, sh := range e.shards {
+		if err := sh.Open(); err != nil {
+			return errors.Wrapf(err, "could not initialize shard %s", id)
+		}
+	}
+
+	return nil
+}
+
+// Init initializes all StorageEngine's components.
+func (e *StorageEngine) Init() error {
+	e.mtx.RLock()
+	defer e.mtx.RUnlock()
+
+	for id, sh := range e.shards {
+		if err := sh.Init(); err != nil {
+			return errors.Wrapf(err, "could not initialize shard %s", id)
+		}
+	}
+
+	return nil
+}
+
+// Close releases all StorageEngine's components.
+func (e *StorageEngine) Close() error {
+	e.mtx.RLock()
+	defer e.mtx.RUnlock()
+
+	for id, sh := range e.shards {
+		if err := sh.Close(); err != nil {
+			e.log.Debug("could not close shard",
+				zap.String("id", id),
+				zap.String("error", err.Error()),
+			)
+		}
+	}
+
+	return nil
+}
