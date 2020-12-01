6177e4b9d8c6aa6fe51658b5dbaf8101231b5837
Alex Vanin <alexey@nspcc.ru>
2020-12-01 13:26:53 +0300

    [#222] Update Delete method in storage engine
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/local_object_storage/engine/delete.go
+++ pkg/local_object_storage/engine/delete.go
@@ -8,36 +8,33 @@ import (
 
 // DeletePrm groups the parameters of Delete operation.
 type DeletePrm struct {
-	addr *objectSDK.Address
+	addr []*objectSDK.Address
 }
 
 // DeleteRes groups resulting values of Delete operation.
 type DeleteRes struct{}
 
-// WithAddress is a Delete option to set the address of the object to delete.
+// WithAddress is a Delete option to set the addresses of the objects to delete.
 //
 // Option is required.
-func (p *DeletePrm) WithAddress(addr *objectSDK.Address) *DeletePrm {
+func (p *DeletePrm) WithAddress(addr ...*objectSDK.Address) *DeletePrm {
 	if p != nil {
-		p.addr = addr
+		p.addr = append(p.addr, addr...)
 	}
 
 	return p
 }
 
-// Delete marks object to delete from local storage.
-//
-// Returns any error encountered that did not allow to completely
-// mark the object to delete.
+// Delete removes objects from the shards.
 func (e *StorageEngine) Delete(prm *DeletePrm) (*DeleteRes, error) {
 	shPrm := new(shard.DeletePrm).
-		WithAddress(prm.addr)
+		WithAddress(prm.addr...)
 
-	e.iterateOverSortedShards(prm.addr, func(_ int, sh *shard.Shard) (stop bool) {
+	e.iterateOverUnsortedShards(func(sh *shard.Shard) (stop bool) {
 		_, err := sh.Delete(shPrm)
 		if err != nil {
 			// TODO: smth wrong with shard, need to be processed
-			e.log.Warn("could not get object from shard",
+			e.log.Warn("could not delete object from shard",
 				zap.Stringer("shard", sh.ID()),
 				zap.String("error", err.Error()),
 			)
