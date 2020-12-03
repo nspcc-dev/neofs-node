a1cb48f80085f9adfa2cb43e0dbfa493fb492812
Alex Vanin <alexey@nspcc.ru>
2020-12-03 14:29:33 +0300

    [#231] metabase: Add container lister
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

---
+++ pkg/local_object_storage/metabase/v2/containers_test.go
@@ -0,0 +1,36 @@
+package meta_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDB_Containers(t *testing.T) {
+	db := newDB(t)
+	defer releaseDB(db)
+
+	const N = 10
+
+	cids := make(map[string]int, N)
+
+	for i := 0; i < N; i++ {
+		obj := generateRawObject(t)
+
+		cids[obj.ContainerID().String()] = 0
+
+		err := db.Put(obj.Object(), nil)
+		require.NoError(t, err)
+	}
+
+	lst, err := db.Containers()
+	require.NoError(t, err)
+
+	for _, cid := range lst {
+		i, ok := cids[cid.String()]
+		require.True(t, ok)
+		require.Equal(t, 0, i)
+
+		cids[cid.String()] = 1
+	}
+}
