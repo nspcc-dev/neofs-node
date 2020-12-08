69dee6ec6d982bbdacdd95a552ed21df13e17776
Alex Vanin <alexey@nspcc.ru>
2020-12-08 14:42:44 +0300

    [#238] shard: Support raw flag in Head method
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/local_object_storage/shard/head_test.go
+++ pkg/local_object_storage/shard/head_test.go
@@ -1,8 +1,10 @@
 package shard_test
 
 import (
+	"errors"
 	"testing"
 
+	objectSDK "github.com/nspcc-dev/neofs-api-go/pkg/object"
 	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/shard"
 	"github.com/stretchr/testify/require"
 )
@@ -26,19 +28,55 @@ func TestShard_Head(t *testing.T) {
 }
 
 func testShardHead(t *testing.T, sh *shard.Shard) {
-	obj := generateRawObject(t)
-	addAttribute(obj, "foo", "bar")
-
 	putPrm := new(shard.PutPrm)
-	putPrm.WithObject(obj.Object())
-
-	_, err := sh.Put(putPrm)
-	require.NoError(t, err)
-
 	headPrm := new(shard.HeadPrm)
-	headPrm.WithAddress(obj.Object().Address())
 
-	res, err := sh.Head(headPrm)
-	require.NoError(t, err)
-	require.Equal(t, obj.Object(), res.Object())
+	t.Run("regular object", func(t *testing.T) {
+		obj := generateRawObject(t)
+		addAttribute(obj, "foo", "bar")
+
+		putPrm.WithObject(obj.Object())
+
+		_, err := sh.Put(putPrm)
+		require.NoError(t, err)
+
+		headPrm.WithAddress(obj.Object().Address())
+
+		res, err := sh.Head(headPrm)
+		require.NoError(t, err)
+		require.Equal(t, obj.Object(), res.Object())
+	})
+
+	t.Run("virtual object", func(t *testing.T) {
+		cid := generateCID()
+		splitID := objectSDK.NewSplitID()
+
+		parent := generateRawObjectWithCID(t, cid)
+		addAttribute(parent, "foo", "bar")
+
+		child := generateRawObjectWithCID(t, cid)
+		child.SetParent(parent.Object().SDK())
+		child.SetParentID(parent.ID())
+		child.SetSplitID(splitID)
+
+		putPrm.WithObject(child.Object())
+
+		_, err := sh.Put(putPrm)
+		require.NoError(t, err)
+
+		headPrm.WithAddress(parent.Object().Address())
+		headPrm.WithRaw(true)
+
+		var siErr *objectSDK.SplitInfoError
+
+		_, err = sh.Head(headPrm)
+		require.True(t, errors.As(err, &siErr))
+
+		headPrm.WithAddress(parent.Object().Address())
+		headPrm.WithRaw(false)
+
+		head, err := sh.Head(headPrm)
+		require.NoError(t, err)
+		require.Equal(t, parent.Object(), head.Object())
+	})
 }
