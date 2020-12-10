f585772746e494d8a900ea1e37f281792369bfa8
Alex Vanin <alexey@nspcc.ru>
2020-12-10 16:20:38 +0300

    [#242] shard: Support ContainerID argument in metabase selects
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/local_object_storage/shard/select.go
+++ pkg/local_object_storage/shard/select.go
@@ -1,6 +1,7 @@
 package shard
 
 import (
+	"github.com/nspcc-dev/neofs-api-go/pkg/container"
 	objectSDK "github.com/nspcc-dev/neofs-api-go/pkg/object"
 	meta "github.com/nspcc-dev/neofs-node/pkg/local_object_storage/metabase"
 	"github.com/pkg/errors"
@@ -8,12 +9,22 @@ import (
 
 // SelectPrm groups the parameters of Select operation.
 type SelectPrm struct {
+	cid     *container.ID
 	filters objectSDK.SearchFilters
 }
 
 // SelectRes groups resulting values of Select operation.
 type SelectRes struct {
 	addrList []*objectSDK.Address
+}
+
+// WithContainerID is a Select option to set the container id to search in.
+func (p *SelectPrm) WithContainerID(cid *container.ID) *SelectPrm {
+	if p != nil {
+		p.cid = cid
+	}
+
+	return p
 }
 
 // WithFilters is a Select option to set the object filters.
@@ -35,7 +46,7 @@ func (r *SelectRes) AddressList() []*objectSDK.Address {
 // Returns any error encountered that
 // did not allow to completely select the objects.
 func (s *Shard) Select(prm *SelectPrm) (*SelectRes, error) {
-	addrList, err := meta.Select(s.metaBase, prm.filters)
+	addrList, err := meta.Select(s.metaBase, prm.cid, prm.filters)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not select objects from metabase")
 	}
