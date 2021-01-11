195d251e635cd11444a6c87c24a14cbbb5758352
Leonard Lyubich <leonard@nspcc.ru>
2021-01-11 17:36:55 +0300

    [#298] morph/netmap: Use netmap.NodeState argument in UpdatePeerState
    Replace custom NodeState enum with the one from netmap lib. Remove no longer
    used NodeState enum.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/morph/client/netmap/wrapper/update_state.go
+++ pkg/morph/client/netmap/wrapper/update_state.go
@@ -1,26 +1,17 @@
 package wrapper
 
 import (
+	"github.com/nspcc-dev/neofs-api-go/pkg/netmap"
 	contract "github.com/nspcc-dev/neofs-node/pkg/morph/client/netmap"
 	"github.com/pkg/errors"
 )
 
-// NodeState is a type of node states enumeration.
-type NodeState int64
-
-const (
-	_ NodeState = iota
-
-	// StateOffline is an offline node state value.
-	StateOffline
-)
-
 // UpdatePeerState changes peer status through Netmap contract
 // call.
-func (w *Wrapper) UpdatePeerState(key []byte, state NodeState) error {
+func (w *Wrapper) UpdatePeerState(key []byte, state netmap.NodeState) error {
 	args := contract.UpdateStateArgs{}
 	args.SetPublicKey(key)
-	args.SetState(int64(state))
+	args.SetState(int64(state.ToV2()))
 
 	// invoke smart contract call
 	//
