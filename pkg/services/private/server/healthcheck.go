8dfd881a203702a19faaaed54ec93d9366d96bcc
Leonard Lyubich <leonard@nspcc.ru>
2021-01-13 15:49:46 +0300

    [#306] private: Implement server of gRPC private node service
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/services/private/server/healthcheck.go
@@ -0,0 +1,35 @@
+package private
+
+import (
+	"context"
+
+	"github.com/nspcc-dev/neofs-node/pkg/services/private"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// HealthCheck returns health status of the local node.
+//
+// If request is unsigned or signed by disallowed key, permission error returns.
+func (s *Server) HealthCheck(_ context.Context, req *private.HealthCheckRequest) (*private.HealthCheckResponse, error) {
+	// verify request
+	if err := s.isValidRequest(req); err != nil {
+		return nil, status.Error(codes.PermissionDenied, err.Error())
+	}
+
+	// create and fill response
+	resp := new(private.HealthCheckResponse)
+
+	body := new(private.HealthCheckResponse_Body)
+	resp.SetBody(body)
+
+	// FIXME: calculate the status
+	body.SetStatus(private.HealthStatus_ONLINE)
+
+	// sign the response
+	if err := SignMessage(s.key, resp); err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return resp, nil
+}
