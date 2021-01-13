ae2680e43f81c5d541035aa8c21ddbe880627b73
Leonard Lyubich <leonard@nspcc.ru>
2021-01-13 15:48:33 +0300

    [#306] private: Implement setters on generated proto messages
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/services/private/types.go
@@ -0,0 +1,15 @@
+package private
+
+// SetKey sets public key used for signing.
+func (m *Signature) SetKey(v []byte) {
+	if m != nil {
+		m.Key = v
+	}
+}
+
+// SetSign sets binary signature.
+func (m *Signature) SetSign(v []byte) {
+	if m != nil {
+		m.Sign = v
+	}
+}
