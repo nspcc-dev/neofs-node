23c220ae28f232128cd0d857201d24e3e3be3e01
Leonard Lyubich <leonard@nspcc.ru>
2021-01-20 20:08:06 +0300

    [#324] morph/listener: Register handlers of new chain blocks
    Extend Listener with RegisterBlockHandler method. All block handlers are
    called on each block read from Subscriber.BlockNotifications channel.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/morph/event/handler.go
+++ pkg/morph/event/handler.go
@@ -1,7 +1,14 @@
 package event
 
+import (
+	"github.com/nspcc-dev/neo-go/pkg/core/block"
+)
+
 // Handler is an Event processing function.
 type Handler func(Event)
+
+// BlockHandler is a chain block processing function.
+type BlockHandler func(*block.Block)
 
 // HandlerInfo is a structure that groups
 // the parameters of the handler of particular
