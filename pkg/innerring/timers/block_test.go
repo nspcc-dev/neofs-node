476349dd1c77e7bd9064293ce3eb7ac6efbabab6
Leonard Lyubich <leonard@nspcc.ru>
2021-01-21 19:32:41 +0300

    [#324] ir/timers: Implement chain block timer
    Implement a timer that can tick chain blocks and perform actions at time
    intervals in the blocks.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/innerring/timers/block_test.go
@@ -0,0 +1,56 @@
+package timers_test
+
+import (
+	"testing"
+
+	"github.com/nspcc-dev/neofs-node/pkg/innerring/timers"
+	"github.com/stretchr/testify/require"
+)
+
+func tickN(t *timers.BlockTimer, n uint32) {
+	for i := uint32(0); i < n; i++ {
+		t.Tick()
+	}
+}
+
+func TestBlockTimer(t *testing.T) {
+	blockDur := uint32(10)
+	baseCallCounter := uint32(0)
+
+	bt := timers.NewBlockTimer(timers.StaticBlockMeter(blockDur), func() {
+		baseCallCounter++
+	})
+
+	require.NoError(t, bt.Reset())
+
+	intervalNum := uint32(7)
+
+	tickN(bt, intervalNum*blockDur)
+
+	require.Equal(t, intervalNum, uint32(baseCallCounter))
+
+	// add half-interval handler
+	halfCallCounter := uint32(0)
+
+	bt.OnDelta(1, 2, func() {
+		halfCallCounter++
+	})
+
+	// add double interval handler
+	doubleCallCounter := uint32(0)
+
+	bt.OnDelta(2, 1, func() {
+		doubleCallCounter++
+	})
+
+	require.NoError(t, bt.Reset())
+
+	baseCallCounter = 0
+	intervalNum = 20
+
+	tickN(bt, intervalNum*blockDur)
+
+	require.Equal(t, intervalNum*2, uint32(halfCallCounter))
+	require.Equal(t, intervalNum, uint32(baseCallCounter))
+	require.Equal(t, intervalNum/2, uint32(doubleCallCounter))
+}
