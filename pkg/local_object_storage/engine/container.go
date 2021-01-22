8509488b98c53015e99f8f0312bcaea3237e12b0
Alex Vanin <alexey@nspcc.ru>
2021-01-22 16:21:45 +0300

    [#337] engine: Add container listing for both engine and shard
    Container listing already supported in the metabase for `engine.List`
    operation. To get container statistics engine should provide both the
    option to get container volume estimation and list of all containers.
    
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/local_object_storage/engine/container.go
+++ pkg/local_object_storage/engine/container.go
@@ -14,6 +14,12 @@ type ContainerSizeRes struct {
 	size uint64
 }
 
+type ListContainersPrm struct{}
+
+type ListContainersRes struct {
+	containers []*container.ID
+}
+
 func (p *ContainerSizePrm) WithContainerID(cid *container.ID) *ContainerSizePrm {
 	if p != nil {
 		p.cid = cid
@@ -24,6 +30,10 @@ func (p *ContainerSizePrm) WithContainerID(cid *container.ID) *ContainerSizePrm 
 
 func (r *ContainerSizeRes) Size() uint64 {
 	return r.size
+}
+
+func (r *ListContainersRes) Containers() []*container.ID {
+	return r.containers
 }
 
 // ContainerSize returns sum of estimation container sizes among all shards.
@@ -56,4 +66,47 @@ func (e *StorageEngine) containerSize(id *container.ID) (total uint64) {
 	})
 
 	return total
+}
+
+// ListContainers returns unique container IDs presented in the engine objects.
+func (e *StorageEngine) ListContainers(_ *ListContainersPrm) *ListContainersRes {
+	return &ListContainersRes{
+		containers: e.listContainers(),
+	}
+}
+
+// ListContainers returns unique container IDs presented in the engine objects.
+func ListContainers(e *StorageEngine) []*container.ID {
+	return e.ListContainers(&ListContainersPrm{}).Containers()
+}
+
+func (e *StorageEngine) listContainers() []*container.ID {
+	uniqueIDs := make(map[string]*container.ID)
+
+	e.iterateOverUnsortedShards(func(s *shard.Shard) (stop bool) {
+		cnrs, err := shard.ListContainers(s)
+		if err != nil {
+			e.log.Warn("can't get list of containers",
+				zap.Stringer("shard_id", s.ID()),
+				zap.String("error", err.Error()))
+
+			return false
+		}
+
+		for i := range cnrs {
+			id := cnrs[i].String()
+			if _, ok := uniqueIDs[id]; !ok {
+				uniqueIDs[id] = cnrs[i]
+			}
+		}
+
+		return false
+	})
+
+	result := make([]*container.ID, 0, len(uniqueIDs))
+	for _, v := range uniqueIDs {
+		result = append(result, v)
+	}
+
+	return result
 }
