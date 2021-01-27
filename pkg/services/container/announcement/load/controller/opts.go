cc88320d6beb031b75cdba20dac5835dd98ea214
Leonard Lyubich <leonard@nspcc.ru>
2021-01-27 22:00:35 +0300

    [#328] container/load: Implement a metrics exchange controller
    Implement a component that connects the value stores of the used space of
    containers. Implement the Start/Stop operations on it, which will later
    become the application handlers of the corresponding events from the
    sidechain. The main task of the controller is to temporarily synchronize the
    stages of calculating the global estimate of the used space in the
    container. The details of the score calculation (the way of collecting /
    transmitting local scores, the final score formula and writing to the
    contract) are encapsulated in the dependency components, the controller is
    abstracted from them.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/services/container/announcement/load/controller/opts.go
@@ -0,0 +1,28 @@
+package loadcontroller
+
+import (
+	"github.com/nspcc-dev/neofs-node/pkg/util/logger"
+	"go.uber.org/zap"
+)
+
+// Option sets an optional parameter of Controller.
+type Option func(*options)
+
+type options struct {
+	log *logger.Logger
+}
+
+func defaultOpts() *options {
+	return &options{
+		log: zap.L(),
+	}
+}
+
+// WithLogger returns option to specify logging component.
+func WithLogger(l *logger.Logger) Option {
+	return func(o *options) {
+		if l != nil {
+			o.log = l
+		}
+	}
+}
