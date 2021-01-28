4415f8dc5b626e30cfe521b167e1eb21cc88427f
Leonard Lyubich <leonard@nspcc.ru>
2021-01-28 14:10:16 +0300

    [#328] container/load: Implement simple Iterator and Writer providers
    Implement functions to wrap Writer or Iterator. The resulting wrapper
    provides WriterProvider or IteratorProvider interface respectively.
    Such a wrapper can be used as a single storage instance provider
    regardless of context.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/services/container/announcement/load/controller/util.go
+++ pkg/services/container/announcement/load/controller/util.go
@@ -1,9 +1,38 @@
 package loadcontroller
 
-import "github.com/nspcc-dev/neofs-api-go/pkg/container"
+import (
+	"context"
+
+	"github.com/nspcc-dev/neofs-api-go/pkg/container"
+)
 
 func usedSpaceFilterEpochEQ(epoch uint64) UsedSpaceFilter {
 	return func(a container.UsedSpaceAnnouncement) bool {
 		return a.Epoch() == epoch
+	}
+}
+
+type storageWrapper struct {
+	w Writer
+	i Iterator
+}
+
+func (s storageWrapper) InitIterator(context.Context) (Iterator, error) {
+	return s.i, nil
+}
+
+func (s storageWrapper) InitWriter(context.Context) (Writer, error) {
+	return s.w, nil
+}
+
+func SimpleIteratorProvider(i Iterator) IteratorProvider {
+	return &storageWrapper{
+		i: i,
+	}
+}
+
+func SimpleWriterProvider(w Writer) WriterProvider {
+	return &storageWrapper{
+		w: w,
 	}
 }
