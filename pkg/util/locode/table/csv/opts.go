75a9916731f41145d55d5fd12fc3695cacc571c2
Leonard Lyubich <leonard@nspcc.ru>
2021-02-08 20:40:36 +0300

    [#316] locode: Implement csv UN/LOCODE table
    Define csv UN/LOCODE table. Implement iterator over table entries. Implement
    method that resolves country/subdivision code pair to subdivision name.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/util/locode/table/csv/opts.go
@@ -0,0 +1,28 @@
+package csvlocode
+
+import (
+	"os"
+)
+
+// Option sets an optional parameter of Table.
+type Option func(*options)
+
+type options struct {
+	mode os.FileMode
+
+	extraPaths []string
+}
+
+func defaultOpts() *options {
+	return &options{
+		mode: 0700,
+	}
+}
+
+// WithExtraPaths returns option to add extra paths
+// to UN/LOCODE tables in csv format.
+func WithExtraPaths(ps ...string) Option {
+	return func(o *options) {
+		o.extraPaths = append(o.extraPaths, ps...)
+	}
+}
