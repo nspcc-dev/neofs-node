13e0801a5f21941f6ee6297a3087e997bb2f4c1f
Leonard Lyubich <leonard@nspcc.ru>
2021-02-19 12:49:23 +0300

    [#395] metabase: Do not add tombstone-on-tombstone records to graveyard
    Creating tombstones for tombstones is prohibited in NeoFS system. Metabase
    graveyard contains records of the form {address: address}: key is an address
    of inhumed object, value is an address of the tombstone. To prevent creation
    tombstones for tombstones metabase must control incoming Inhume calls:
    
     * if Inhume target is a tombstone, then "grave" should not be added;
     * if {a1:a2} "grave" was created earlier and {a2: a3} "grave" came later,
       then first "grave" must be removed as tomb-on-tomb.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/local_object_storage/metabase/inhume_test.go
+++ pkg/local_object_storage/metabase/inhume_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/nspcc-dev/neofs-node/pkg/core/object"
 	meta "github.com/nspcc-dev/neofs-node/pkg/local_object_storage/metabase"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 )
 
@@ -28,4 +29,60 @@ func TestDB_Inhume(t *testing.T) {
 
 	_, err = meta.Get(db, raw.Object().Address())
 	require.EqualError(t, err, object.ErrAlreadyRemoved.Error())
+}
+
+func TestInhumeTombOnTomb(t *testing.T) {
+	db := newDB(t)
+	defer releaseDB(db)
+
+	var (
+		err error
+
+		addr1     = generateAddress()
+		addr2     = generateAddress()
+		addr3     = generateAddress()
+		inhumePrm = new(meta.InhumePrm)
+		existsPrm = new(meta.ExistsPrm)
+	)
+
+	// inhume addr1 via addr2
+	_, err = db.Inhume(inhumePrm.
+		WithAddresses(addr1).
+		WithTombstoneAddress(addr2),
+	)
+	require.NoError(t, err)
+
+	// addr1 should become inhumed {addr1:addr2}
+	_, err = db.Exists(existsPrm.WithAddress(addr1))
+	require.True(t, errors.Is(err, object.ErrAlreadyRemoved))
+
+	// try to inhume addr3 via addr1
+	_, err = db.Inhume(inhumePrm.
+		WithAddresses(addr3).
+		WithTombstoneAddress(addr1),
+	)
+	require.NoError(t, err)
+
+	// record with {addr1:addr2} should be removed from graveyard
+	// as a tomb-on-tomb
+	res, err := db.Exists(existsPrm.WithAddress(addr1))
+	require.NoError(t, err)
+	require.False(t, res.Exists())
+
+	// addr3 should be inhumed {addr3: addr1}
+	_, err = db.Exists(existsPrm.WithAddress(addr3))
+	require.True(t, errors.Is(err, object.ErrAlreadyRemoved))
+
+	// try to inhume addr1 (which is already a tombstone in graveyard)
+	_, err = db.Inhume(inhumePrm.
+		WithAddresses(addr1).
+		WithTombstoneAddress(generateAddress()),
+	)
+	require.NoError(t, err)
+
+	// record with addr1 key should not appear in graveyard
+	// (tomb can not be inhumed)
+	res, err = db.Exists(existsPrm.WithAddress(addr1))
+	require.NoError(t, err)
+	require.False(t, res.Exists())
 }
