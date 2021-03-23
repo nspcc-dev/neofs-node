166392bb8fda772e1d98a17e335db586225ec498
Alex Vanin <alexey@nspcc.ru>
2021-03-23 13:54:48 +0300

    [#446] innerring: Remove inner ring list method
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/innerring/invoke/neofs.go
+++ pkg/innerring/invoke/neofs.go
@@ -1,11 +1,7 @@
 package invoke
 
 import (
-	"bytes"
-	"crypto/ecdsa"
-
 	"github.com/nspcc-dev/neo-go/pkg/util"
-	crypto "github.com/nspcc-dev/neofs-crypto"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/client"
 )
 
@@ -29,8 +25,7 @@ const (
 	// execution stage. Otherwise invocation will fail due to gas limit.
 	extraFee = 2_0000_0000 // 2.0 Fixed8 gas
 
-	innerRingListMethod = "innerRingList"
-	chequeMethod        = "cheque"
+	chequeMethod = "cheque"
 )
 
 // CashOutCheque invokes Cheque method.
@@ -45,46 +40,4 @@ func CashOutCheque(cli *client.Client, con util.Uint160, p *ChequeParams) error 
 		p.Amount,
 		p.LockAccount.BytesBE(),
 	)
-}
-
-// InnerRingIndex returns index of the `key` in the inner ring list from sidechain
-// along with total size of inner ring list. If key is not in the inner ring list,
-// then returns `-1` as index.
-func InnerRingIndex(cli *client.Client, con util.Uint160, key *ecdsa.PublicKey) (int32, int32, error) {
-	if cli == nil {
-		return 0, 0, client.ErrNilClient
-	}
-
-	nodePublicKey := crypto.MarshalPublicKey(key)
-
-	data, err := cli.TestInvoke(con, innerRingListMethod)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	irNodes, err := client.ArrayFromStackItem(data[0])
-	if err != nil {
-		return 0, 0, err
-	}
-
-	var result int32 = -1
-
-	for i := range irNodes {
-		key, err := client.ArrayFromStackItem(irNodes[i])
-		if err != nil {
-			return 0, 0, err
-		}
-
-		keyValue, err := client.BytesFromStackItem(key[0])
-		if err != nil {
-			return 0, 0, err
-		}
-
-		if bytes.Equal(keyValue, nodePublicKey) {
-			result = int32(i)
-			break
-		}
-	}
-
-	return result, int32(len(irNodes)), nil
 }
