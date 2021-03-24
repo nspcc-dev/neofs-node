eaedc58c075612730615feed25e93b21f74a6ace
Leonard Lyubich <leonard@nspcc.ru>
2021-03-24 12:55:07 +0300

    [#444] reputation: Implement sign and response servers
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/services/reputation/rpc/response.go
@@ -0,0 +1,37 @@
+package reputationrpc
+
+import (
+	"context"
+
+	"github.com/nspcc-dev/neofs-api-go/v2/reputation"
+	"github.com/nspcc-dev/neofs-node/pkg/services/util"
+	"github.com/nspcc-dev/neofs-node/pkg/services/util/response"
+)
+
+type responseService struct {
+	respSvc *response.Service
+
+	svc Server
+}
+
+// NewResponseService returns reputation service server instance that passes
+// internal service call to response service.
+func NewResponseService(cnrSvc Server, respSvc *response.Service) Server {
+	return &responseService{
+		respSvc: respSvc,
+		svc:     cnrSvc,
+	}
+}
+
+func (s *responseService) SendLocalTrust(ctx context.Context, req *reputation.SendLocalTrustRequest) (*reputation.SendLocalTrustResponse, error) {
+	resp, err := s.respSvc.HandleUnaryRequest(ctx, req,
+		func(ctx context.Context, req interface{}) (util.ResponseMessage, error) {
+			return s.svc.SendLocalTrust(ctx, req.(*reputation.SendLocalTrustRequest))
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.(*reputation.SendLocalTrustResponse), nil
+}
---
+++ pkg/services/reputation/rpc/sign.go
@@ -0,0 +1,35 @@
+package reputationrpc
+
+import (
+	"context"
+	"crypto/ecdsa"
+
+	"github.com/nspcc-dev/neofs-api-go/v2/reputation"
+	"github.com/nspcc-dev/neofs-node/pkg/services/util"
+)
+
+type signService struct {
+	sigSvc *util.SignService
+
+	svc Server
+}
+
+func NewSignService(key *ecdsa.PrivateKey, svc Server) Server {
+	return &signService{
+		sigSvc: util.NewUnarySignService(key),
+		svc:    svc,
+	}
+}
+
+func (s *signService) SendLocalTrust(ctx context.Context, req *reputation.SendLocalTrustRequest) (*reputation.SendLocalTrustResponse, error) {
+	resp, err := s.sigSvc.HandleUnaryRequest(ctx, req,
+		func(ctx context.Context, req interface{}) (util.ResponseMessage, error) {
+			return s.svc.SendLocalTrust(ctx, req.(*reputation.SendLocalTrustRequest))
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.(*reputation.SendLocalTrustResponse), nil
+}
