f4e39678f1348d682bfbb0217d0003c51a4129f1
Alex Vanin <alexey@nspcc.ru>
2021-03-24 18:31:01 +0300

    [#421] governance: Add list update functions for inner ring
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/innerring/processors/governance/list.go
+++ pkg/innerring/processors/governance/list.go
@@ -7,7 +7,10 @@ import (
 	"github.com/pkg/errors"
 )
 
-var errNotEnoughKeys = errors.New("alphabet list in mainnet is too short")
+var (
+	errNotEnoughKeys = errors.New("alphabet list in mainnet is too short")
+	errNotEqualLen   = errors.New("old and new alphabet lists have different length")
+)
 
 // newAlphabetList returns updated list of sidechain keys with no more than 1\3
 // of new keys from mainnet list. Function returns `errNotEnoughKeys` if
@@ -59,6 +62,32 @@ func newAlphabetList(sidechain, mainnet keys.PublicKeys) (keys.PublicKeys, error
 	}
 
 	sort.Sort(result)
+
+	return result, nil
+}
+
+// updateInnerRing function removes `before` keys from `innerRing` and adds
+// `after` keys in the list. If length of `before` and `after` is not the same
+// then function returns errNotEqualLen
+func updateInnerRing(innerRing, before, after keys.PublicKeys) (keys.PublicKeys, error) {
+	lnBefore := len(before)
+	if lnBefore != len(after) {
+		return nil, errNotEqualLen
+	}
+
+	result := make(keys.PublicKeys, 0, len(innerRing))
+
+	// O(n^2) for 7 nodes is not THAT bad.
+loop:
+	for i := range innerRing {
+		for j := range before {
+			if innerRing[i].Equal(before[j]) {
+				result = append(result, after[j])
+				continue loop
+			}
+		}
+		result = append(result, innerRing[i])
+	}
 
 	return result, nil
 }
