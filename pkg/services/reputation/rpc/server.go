df97e35f30f13d331a9654a05d49484c09ccbdd8
Leonard Lyubich <leonard@nspcc.ru>
2021-03-24 12:52:35 +0300

    [#444] reputation: Define RPC Server interface
    Define `Server` interface of NeoFS API V2 Reputation service server.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/services/reputation/rpc/server.go
@@ -0,0 +1,12 @@
+package reputationrpc
+
+import (
+	"context"
+
+	"github.com/nspcc-dev/neofs-api-go/v2/reputation"
+)
+
+// Server is an interface of the NeoFS API v2 Reputation service server.
+type Server interface {
+	SendLocalTrust(context.Context, *reputation.SendLocalTrustRequest) (*reputation.SendLocalTrustResponse, error)
+}
