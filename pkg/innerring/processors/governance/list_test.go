58119e606583e8a29ecac16da34cdc176dc7b6c8
Alex Vanin <alexey@nspcc.ru>
2021-03-25 21:13:05 +0300

    [#421] governance: Don't construct alphabet list with empty sidechain
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/innerring/processors/governance/list_test.go
+++ pkg/innerring/processors/governance/list_test.go
@@ -14,6 +14,11 @@ func TestNewAlphabetList(t *testing.T) {
 
 	orig := keys.PublicKeys{k[0], k[1], k[2], k[3], k[4], k[5], k[6]}
 
+	t.Run("no sidechain keys", func(t *testing.T) {
+		_, err := newAlphabetList(nil, orig)
+		require.Error(t, err)
+	})
+
 	t.Run("same keys", func(t *testing.T) {
 		list, err := newAlphabetList(orig, orig)
 		require.NoError(t, err)
