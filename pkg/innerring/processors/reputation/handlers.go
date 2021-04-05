9f7675ed2e373e3dd6330ce5b19edbbe506fbe80
Alex Vanin <alexey@nspcc.ru>
2021-04-05 12:29:33 +0300

    [#452] Use API structures for reputation PeerID and GlobalTrust
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/innerring/processors/reputation/handlers.go
+++ pkg/innerring/processors/reputation/handlers.go
@@ -10,16 +10,18 @@ import (
 
 func (rp *Processor) handlePutReputation(ev event.Event) {
 	put := ev.(reputationEvent.Put)
+	peerID := put.PeerID()
+
 	rp.log.Info("notification",
 		zap.String("type", "reputation put"),
-		zap.String("peer_id", hex.EncodeToString(put.PeerID().Bytes())))
+		zap.String("peer_id", hex.EncodeToString(peerID.ToV2().GetValue())))
 
 	// send event to the worker pool
 
 	err := rp.pool.Submit(func() {
 		rp.processPut(
 			put.Epoch(),
-			put.PeerID(),
+			peerID,
 			put.Value(),
 		)
 	})
--- pkg/morph/event/reputation/put_test.go
+++ pkg/morph/event/reputation/put_test.go
@@ -5,18 +5,33 @@ import (
 	"testing"
 
 	"github.com/nspcc-dev/neo-go/pkg/vm/stackitem"
+	"github.com/nspcc-dev/neofs-api-go/pkg/reputation"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/event"
-	"github.com/nspcc-dev/neofs-node/pkg/services/reputation"
 	"github.com/stretchr/testify/require"
 )
 
 func TestParsePut(t *testing.T) {
 	var (
+		peerID reputation.PeerID
+
+		value      reputation.GlobalTrust
+		trust      reputation.Trust
+		trustValue float64 = 64
+
 		epoch uint64 = 42
 
-		peerID = reputation.PeerIDFromBytes([]byte("peerID"))
-		value  = []byte("There should be marshalled structure")
+		rawPeerID = [33]byte{1, 2, 3, 4, 5, 6}
 	)
+
+	peerID.SetPublicKey(rawPeerID)
+
+	trust.SetValue(trustValue)
+	trust.SetPeer(&peerID)
+
+	value.SetTrust(&trust)
+
+	rawValue, err := value.Marshal()
+	require.NoError(t, err)
 
 	t.Run("wrong number of parameters", func(t *testing.T) {
 		prms := []stackitem.Item{
@@ -48,7 +63,7 @@ func TestParsePut(t *testing.T) {
 	t.Run("wrong value parameter", func(t *testing.T) {
 		_, err := ParsePut([]stackitem.Item{
 			stackitem.NewBigInteger(new(big.Int).SetUint64(epoch)),
-			stackitem.NewByteArray(peerID.Bytes()),
+			stackitem.NewByteArray(rawPeerID[:]),
 			stackitem.NewMap(),
 		})
 
@@ -58,8 +73,8 @@ func TestParsePut(t *testing.T) {
 	t.Run("correct behavior", func(t *testing.T) {
 		ev, err := ParsePut([]stackitem.Item{
 			stackitem.NewBigInteger(new(big.Int).SetUint64(epoch)),
-			stackitem.NewByteArray(peerID.Bytes()),
-			stackitem.NewByteArray(value),
+			stackitem.NewByteArray(rawPeerID[:]),
+			stackitem.NewByteArray(rawValue),
 		})
 		require.NoError(t, err)
 
