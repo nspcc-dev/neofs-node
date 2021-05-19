eb26f9267892ea9c12310586ef251fb44d68fec7
Leonard Lyubich <leonard@nspcc.ru>
2021-05-19 12:21:33 +0300

    [#505] neofsid: Implement wrapper over contract client
    Implement wrapper over NeoFS ID contact's client which allows to which
    allows you to conveniently interact with the contract. Implement
    `AccountKeys` method for getting a list of keys by account ID.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/morph/client/neofsid/wrapper/keys.go
@@ -0,0 +1,38 @@
+package neofsid
+
+import (
+	"crypto/elliptic"
+	"fmt"
+
+	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
+	"github.com/nspcc-dev/neofs-api-go/pkg/owner"
+	"github.com/nspcc-dev/neofs-node/pkg/morph/client/neofsid"
+)
+
+// AccountKeys requests public keys of NeoFS account from NeoFS ID contract.
+func (x *ClientWrapper) AccountKeys(id *owner.ID) (keys.PublicKeys, error) {
+	var args neofsid.KeyListingArgs
+
+	args.SetOwnerID(id.ToV2().GetValue())
+
+	res, err := (*neofsid.Client)(x).AccountKeys(args)
+	if err != nil {
+		return nil, err
+	}
+
+	binKeys := res.Keys()
+	ks := make(keys.PublicKeys, 0, len(binKeys))
+
+	curve := elliptic.P256()
+
+	for i := range binKeys {
+		k, err := keys.NewPublicKeyFromBytes(binKeys[i], curve)
+		if err != nil {
+			return nil, fmt.Errorf("received invalid key: %w", err)
+		}
+
+		ks = append(ks, k)
+	}
+
+	return ks, nil
+}
