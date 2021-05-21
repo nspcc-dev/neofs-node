7182d7a2ab061efc5802ade2d68fd46101e21396
Leonard Lyubich <leonard@nspcc.ru>
2021-05-21 20:40:43 +0300

    [#493] node/config: Implement ENV variable key constructor
    Add `internal.Env` function which converts path to config value to ENV
    variable key.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- cmd/neofs-node/config/internal/env.go
+++ cmd/neofs-node/config/internal/env.go
@@ -1,8 +1,22 @@
 package internal
 
+import (
+	"strings"
+)
+
 // EnvPrefix is a prefix of ENV variables related
 // to storage node configuration.
 const EnvPrefix = "neofs"
 
 // EnvSeparator is a section separator in ENV variables.
 const EnvSeparator = "_"
+
+// Env returns ENV variable key for particular config parameter.
+func Env(path ...string) string {
+	return strings.ToUpper(
+		strings.Join(
+			append([]string{EnvPrefix}, path...),
+			EnvSeparator,
+		),
+	)
+}
