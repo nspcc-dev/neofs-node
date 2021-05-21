270b147205132ed370a7e277f87855f6976b26cd
Leonard Lyubich <leonard@nspcc.ru>
2021-05-21 20:36:37 +0300

    [#493] node/config: Export ENV constants from internal package
    Replace ENV prefix and separator to `internal` package in order to reuse
    them for testing.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ cmd/neofs-node/config/internal/env.go
@@ -0,0 +1,8 @@
+package internal
+
+// EnvPrefix is a prefix of ENV variables related
+// to storage node configuration.
+const EnvPrefix = "neofs"
+
+// EnvSeparator is a section separator in ENV variables.
+const EnvSeparator = "_"
