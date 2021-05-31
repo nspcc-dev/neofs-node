f76083484bfc3b697c0fdaf0226248b29f480cca
Leonard Lyubich <leonard@nspcc.ru>
2021-05-31 21:09:02 +0300

    [#556] morph/container: Do not parse public key in Put event parser
    Morph event structures defined in `pkg/morph/event` should only carry
    notification values without any additional interpretation. All logical work
    should be concentrated on app-side. Change `Bind.User` / `Unbind.User` to
    return byte slice. Change `Bind.Keys` / `Unbind.Keys` to return `[][]byte`.
    `ParseBind` / `ParseUnbind` don't decode data from now.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/morph/event/neofs/bind_test.go
+++ pkg/morph/event/neofs/bind_test.go
@@ -1,26 +1,20 @@
 package neofs
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"testing"
 
-	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
-	"github.com/nspcc-dev/neo-go/pkg/util"
 	"github.com/nspcc-dev/neo-go/pkg/vm/stackitem"
-	crypto "github.com/nspcc-dev/neofs-crypto"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/event"
-	"github.com/nspcc-dev/neofs-node/pkg/util/test"
 	"github.com/stretchr/testify/require"
 )
 
 func TestParseBind(t *testing.T) {
 	var (
-		user       = util.Uint160{0x1, 0x2, 0x3}
-		publicKeys = []*ecdsa.PublicKey{
-			&test.DecodeKey(1).PublicKey,
-			&test.DecodeKey(2).PublicKey,
-			&test.DecodeKey(3).PublicKey,
+		user       = []byte{0x1, 0x2, 0x3}
+		publicKeys = [][]byte{
+			[]byte("key1"),
+			[]byte("key2"),
+			[]byte("key3"),
 		}
 	)
 
@@ -43,7 +37,7 @@ func TestParseBind(t *testing.T) {
 
 	t.Run("wrong second parameter", func(t *testing.T) {
 		_, err := ParseBind([]stackitem.Item{
-			stackitem.NewByteArray(user.BytesBE()),
+			stackitem.NewByteArray(user),
 			stackitem.NewMap(),
 		})
 
@@ -51,27 +45,19 @@ func TestParseBind(t *testing.T) {
 	})
 
 	t.Run("correct", func(t *testing.T) {
-
 		ev, err := ParseBind([]stackitem.Item{
-			stackitem.NewByteArray(user.BytesBE()),
+			stackitem.NewByteArray(user),
 			stackitem.NewArray([]stackitem.Item{
-				stackitem.NewByteArray(crypto.MarshalPublicKey(publicKeys[0])),
-				stackitem.NewByteArray(crypto.MarshalPublicKey(publicKeys[1])),
-				stackitem.NewByteArray(crypto.MarshalPublicKey(publicKeys[2])),
+				stackitem.NewByteArray(publicKeys[0]),
+				stackitem.NewByteArray(publicKeys[1]),
+				stackitem.NewByteArray(publicKeys[2]),
 			}),
 		})
 		require.NoError(t, err)
 
-		expKeys := make([]*keys.PublicKey, len(publicKeys))
-		for i := range publicKeys {
-			expKeys[i], err = keys.NewPublicKeyFromBytes(
-				crypto.MarshalPublicKey(publicKeys[i]), elliptic.P256())
-			require.NoError(t, err)
-		}
+		e := ev.(Bind)
 
-		require.Equal(t, Bind{
-			user: user,
-			keys: expKeys,
-		}, ev)
+		require.Equal(t, user, e.User())
+		require.Equal(t, publicKeys, e.Keys())
 	})
 }
