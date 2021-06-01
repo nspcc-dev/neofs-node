1b1cf73883b3bd950c17a133337ffacd3a1d7340
Alex Vanin <alexey@nspcc.ru>
2021-06-01 12:47:01 +0300

    [#493] cmd/node: Add .env file for tests
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- cmd/neofs-node/config/logger/config_test.go
+++ cmd/neofs-node/config/logger/config_test.go
@@ -1,34 +1,30 @@
 package loggerconfig_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-node/config"
-	"github.com/nspcc-dev/neofs-node/cmd/neofs-node/config/internal"
 	loggerconfig "github.com/nspcc-dev/neofs-node/cmd/neofs-node/config/logger"
 	configtest "github.com/nspcc-dev/neofs-node/cmd/neofs-node/config/test"
 	"github.com/stretchr/testify/require"
 )
 
 func TestLoggerSection_Level(t *testing.T) {
-	configtest.ForEachFileType("../../../../config/example/node", func(c *config.Config) {
-		v := loggerconfig.Level(c)
-		require.Equal(t, "debug", v)
+	t.Run("defaults", func(t *testing.T) {
+		v := loggerconfig.Level(configtest.EmptyConfig())
+		require.Equal(t, loggerconfig.LevelDefault, v)
 	})
 
-	v := loggerconfig.Level(configtest.EmptyConfig())
-	require.Equal(t, loggerconfig.LevelDefault, v)
+	const path = "../../../../config/example/node"
+
+	var fileConfigTest = func(c *config.Config) {
+		v := loggerconfig.Level(c)
+		require.Equal(t, "debug", v)
+	}
+
+	configtest.ForEachFileType(path, fileConfigTest)
 
 	t.Run("ENV", func(t *testing.T) {
-		// TODO: read from file
-		err := os.Setenv(
-			internal.Env("logger", "level"),
-			"debug",
-		)
-		require.NoError(t, err)
-
-		v := loggerconfig.Level(configtest.EmptyConfig())
-		require.Equal(t, "debug", v)
+		configtest.ForEnvFileType(path, fileConfigTest)
 	})
 }
