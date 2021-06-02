85c0e46a7cf1c35c1d6662ef296fd67017775f81
Alex Vanin <alexey@nspcc.ru>
2021-06-02 15:31:29 +0300

    [#493] cmd/node: Add object service section to config
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

---
+++ cmd/neofs-node/config/object/config.go
@@ -0,0 +1,41 @@
+package objectconfig
+
+import (
+	"github.com/nspcc-dev/neofs-node/cmd/neofs-node/config"
+)
+
+// PutConfig is a wrapper over "put" config section which provides access
+// to object put pipeline configuration of object service.
+type PutConfig struct {
+	cfg *config.Config
+}
+
+const (
+	subsection = "object"
+
+	putSubsection = "put"
+
+	// PutPoolSizeDefault is a default value of routine pool size to
+	// process object.Put requests in object service.
+	PutPoolSizeDefault = 10
+)
+
+// Put returns structure that provides access to "put" subsection of
+// "object" section.
+func Put(c *config.Config) PutConfig {
+	return PutConfig{
+		c.Sub(subsection).Sub(putSubsection),
+	}
+}
+
+// PoolSize returns value of "pool_size" config parameter.
+//
+// Returns PutPoolSizeDefault if value is not positive number.
+func (g PutConfig) PoolSize() int {
+	v := config.Int(g.cfg, "pool_size")
+	if v > 0 {
+		return int(v)
+	}
+
+	return PutPoolSizeDefault
+}
---
+++ cmd/neofs-node/config/object/config_test.go
@@ -0,0 +1,30 @@
+package objectconfig_test
+
+import (
+	"testing"
+
+	"github.com/nspcc-dev/neofs-node/cmd/neofs-node/config"
+	objectconfig "github.com/nspcc-dev/neofs-node/cmd/neofs-node/config/object"
+	configtest "github.com/nspcc-dev/neofs-node/cmd/neofs-node/config/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestObjectSection(t *testing.T) {
+	t.Run("defaults", func(t *testing.T) {
+		empty := configtest.EmptyConfig()
+
+		require.Equal(t, objectconfig.PutPoolSizeDefault, objectconfig.Put(empty).PoolSize())
+	})
+
+	const path = "../../../../config/example/node"
+
+	var fileConfigTest = func(c *config.Config) {
+		require.Equal(t, 100, objectconfig.Put(c).PoolSize())
+	}
+
+	configtest.ForEachFileType(path, fileConfigTest)
+
+	t.Run("ENV", func(t *testing.T) {
+		configtest.ForEnvFileType(path, fileConfigTest)
+	})
+}
