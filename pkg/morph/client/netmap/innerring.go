14ea9c178db0791cf2e6da439cd2d2e34f8c73b4
Pavel Karpy <carpawell@nspcc.ru>
2021-07-19 20:15:41 +0300

    [#705] pkg/morph/netmap: Add `innerRingList` method
    Add `innerRingList` method to `netmap` client
    wrapper. Method parses current IR nodes list.
    
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/morph/client/netmap/innerring.go
+++ pkg/morph/client/netmap/innerring.go
@@ -1,6 +1,13 @@
 package netmap
 
-import "github.com/nspcc-dev/neo-go/pkg/crypto/keys"
+import (
+	"crypto/elliptic"
+	"fmt"
+
+	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
+	"github.com/nspcc-dev/neo-go/pkg/vm/stackitem"
+	"github.com/nspcc-dev/neofs-node/pkg/morph/client"
+)
 
 // SetInnerRing updates inner ring members in netmap contract.
 func (c *Client) SetInnerRing(keys keys.PublicKeys) error {
@@ -10,4 +17,61 @@ func (c *Client) SetInnerRing(keys keys.PublicKeys) error {
 	}
 
 	return c.client.Invoke(c.setInnerRing, args)
+}
+
+// InnerRingList returns public keys of inner ring members in
+// netmap contract.
+func (c *Client) InnerRingList() (keys.PublicKeys, error) {
+	prms, err := c.client.TestInvoke(
+		c.innerRingList,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("could not perform test invocation (%s): %w", c.innerRingList, err)
+	}
+
+	return irKeysFromStackItem(prms, c.innerRingList)
+}
+
+func irKeysFromStackItem(stack []stackitem.Item, method string) (keys.PublicKeys, error) {
+	if ln := len(stack); ln != 1 {
+		return nil, fmt.Errorf("unexpected stack item count (%s): %d", method, ln)
+	}
+
+	irs, err := client.ArrayFromStackItem(stack[0])
+	if err != nil {
+		return nil, fmt.Errorf("could not get stack item array from stack item (%s): %w", method, err)
+	}
+
+	irKeys := make(keys.PublicKeys, len(irs))
+
+	for i := range irs {
+		irKeys[i], err = irKeyFromStackItem(irs[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return irKeys, nil
+}
+
+const irNodeFixedPrmNumber = 1
+
+func irKeyFromStackItem(prm stackitem.Item) (*keys.PublicKey, error) {
+	prms, err := client.ArrayFromStackItem(prm)
+	if err != nil {
+		return nil, fmt.Errorf("could not get stack item array (IRNode): %w", err)
+	} else if ln := len(prms); ln != irNodeFixedPrmNumber {
+		return nil, fmt.Errorf(
+			"unexpected stack item count (IRNode): expected %d, has %d",
+			irNodeFixedPrmNumber,
+			ln,
+		)
+	}
+
+	byteKey, err := client.BytesFromStackItem(prms[0])
+	if err != nil {
+		return nil, fmt.Errorf("could not parse bytes from stack item (IRNode): %w", err)
+	}
+
+	return keys.NewPublicKeyFromBytes(byteKey, elliptic.P256())
 }
