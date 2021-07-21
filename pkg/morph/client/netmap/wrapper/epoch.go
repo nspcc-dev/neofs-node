2daa3f2e5cd7ba0e1ab1a902e2c399f44a210787
Alex Vanin <alexey@nspcc.ru>
2021-07-21 15:55:05 +0300

    [#708] morph/client: Add epoch block getter from netmap
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pkg/morph/client/netmap/wrapper/epoch.go
+++ pkg/morph/client/netmap/wrapper/epoch.go
@@ -18,3 +18,16 @@ func (w *Wrapper) Epoch() (uint64, error) {
 
 	return uint64(vals.Number()), nil
 }
+
+// LastEpochBlock receives block number of current NeoFS epoch
+// through the Netmap contract call.
+func (w *Wrapper) LastEpochBlock() (uint32, error) {
+	args := netmap.EpochBlockArgs{}
+
+	vals, err := w.client.LastEpochBlock(args)
+	if err != nil {
+		return 0, fmt.Errorf("(%T) could not get epoch block number: %w", w, err)
+	}
+
+	return uint32(vals.Block()), nil
+}
