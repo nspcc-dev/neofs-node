33c3f18b4fe9412a170990f3886d25fcea035b28
Evgenii Stratonikov <evgeniy@nspcc.ru>
2021-08-06 14:28:07 +0300

    [#748] neofs-adm: allow to update contracts
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

---
+++ cmd/neofs-adm/internal/modules/morph/update.go
@@ -0,0 +1,21 @@
+package morph
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func updateContracts(cmd *cobra.Command, _ []string) error {
+	wCtx, err := newInitializeContext(cmd, viper.GetViper())
+	if err != nil {
+		return fmt.Errorf("initialization error: %w", err)
+	}
+
+	if err := wCtx.deployContracts("migrate"); err != nil {
+		return err
+	}
+
+	return wCtx.setNNS()
+}
