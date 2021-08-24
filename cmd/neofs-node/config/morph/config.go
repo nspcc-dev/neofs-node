e6a0bc3e2e3dd4a7ab9f1ad169d269155e8c0c71
Pavel Karpy <carpawell@nspcc.ru>
2021-08-24 17:33:18 +0300

    [#770] node/config: Add notary deposit config
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- cmd/neofs-node/config/morph/config.go
+++ cmd/neofs-node/config/morph/config.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/nspcc-dev/neo-go/pkg/encoding/fixedn"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-node/config"
 )
 
 const (
-	subsection = "morph"
+	subsection       = "morph"
+	notarySubsection = "notary"
 
 	// DialTimeoutDefault is a default dial timeout of morph chain client connection.
 	DialTimeoutDefault = 5 * time.Second
+
+	// NotaryDepositAmountDefault is a default deposit amount to notary contract.
+	NotaryDepositAmountDefault = 5000_0000 // 0.5 Fixed8
+
+	// NotaryDepositDurationDefault is a default deposit duration.
+	NotaryDepositDurationDefault uint32 = 1000
 )
 
 // RPCEndpoint returns list of values of "rpc_endpoint" config parameter
@@ -57,4 +65,40 @@ func DialTimeout(c *config.Config) time.Duration {
 // from "morph" section.
 func DisableCache(c *config.Config) bool {
 	return config.BoolSafe(c.Sub(subsection), "disable_cache")
+}
+
+// NotaryConfig is a wrapper over the config section
+// which provides access to notary configurations
+// of the side chain.
+type NotaryConfig struct {
+	cfg *config.Config
+}
+
+// Notary returns structure that provides access to "notary"
+// subsection of "morph" section.
+func Notary(c *config.Config) *NotaryConfig {
+	return &NotaryConfig{
+		cfg: c.Sub(subsection).Sub(notarySubsection),
+	}
+}
+
+// Amount returns value of "deposit_amount" config parameter.
+//
+// Returns NotaryDepositAmountDefault if value is not positive amount.
+func (n NotaryConfig) Amount() fixedn.Fixed8 {
+	v := config.UintSafe(n.cfg, "deposit_amount")
+	if v > 0 {
+		return fixedn.Fixed8(v)
+	}
+
+	return fixedn.Fixed8(NotaryDepositAmountDefault)
+}
+
+func (n NotaryConfig) Duration() uint32 {
+	v := config.Uint32Safe(n.cfg, "deposit_duration")
+	if v > 0 {
+		return v
+	}
+
+	return NotaryDepositDurationDefault
 }
