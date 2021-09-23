8a3dc968f99e2f9ca3ff9651c75fb46e827424c7
Pavel Karpy <carpawell@nspcc.ru>
2021-09-23 20:13:59 +0300

    [#846] innerring: Use epoch as nonce in cleanup netmap process
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/innerring/processors/netmap/process_cleanup.go
+++ pkg/innerring/processors/netmap/process_cleanup.go
@@ -3,17 +3,18 @@ package netmap
 import (
 	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
 	"github.com/nspcc-dev/neofs-api-go/pkg/netmap"
+	netmapEvent "github.com/nspcc-dev/neofs-node/pkg/morph/event/netmap"
 	"go.uber.org/zap"
 )
 
-func (np *Processor) processNetmapCleanupTick(epoch uint64) {
+func (np *Processor) processNetmapCleanupTick(ev netmapCleanupTick) {
 	if !np.alphabetState.IsAlphabet() {
 		np.log.Info("non alphabet mode, ignore new netmap cleanup tick")
 
 		return
 	}
 
-	err := np.netmapSnapshot.forEachRemoveCandidate(epoch, func(s string) error {
+	err := np.netmapSnapshot.forEachRemoveCandidate(ev.epoch, func(s string) error {
 		key, err := keys.NewPublicKeyFromString(s)
 		if err != nil {
 			np.log.Warn("can't decode public key of netmap node",
@@ -24,7 +25,20 @@ func (np *Processor) processNetmapCleanupTick(epoch uint64) {
 
 		np.log.Info("vote to remove node from netmap", zap.String("key", s))
 
-		err = np.netmapClient.UpdatePeerState(key.Bytes(), netmap.NodeStateOffline)
+		if np.notaryDisabled {
+			err = np.netmapClient.UpdatePeerState(key.Bytes(), netmap.NodeStateOffline)
+		} else {
+			// use epoch as TX nonce to prevent collisions
+			err = np.netmapClient.Morph().NotaryInvoke(
+				np.netmapContract,
+				0,
+				uint32(ev.epoch),
+				netmapEvent.UpdateStateNotaryEvent,
+				int64(netmap.NodeStateOffline.ToV2()),
+				key.Bytes(),
+			)
+		}
+
 		if err != nil {
 			np.log.Error("can't invoke netmap.UpdateState", zap.Error(err))
 		}
