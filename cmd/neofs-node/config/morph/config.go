2f2258733b80088ef08bd5a07ab2a31b806fc7e2
Pavel Karpy <carpawell@nspcc.ru>
2021-09-30 22:14:02 +0300

    [#873] node: Delete useless notary deposit `Amount`, `Duration` envs
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- cmd/neofs-node/config/morph/config.go
+++ cmd/neofs-node/config/morph/config.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/nspcc-dev/neo-go/pkg/encoding/fixedn"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-node/config"
 )
 
@@ -65,40 +64,4 @@ func DialTimeout(c *config.Config) time.Duration {
 // from "morph" section.
 func DisableCache(c *config.Config) bool {
 	return config.BoolSafe(c.Sub(subsection), "disable_cache")
-}
-
-// NotaryConfig is a wrapper over the config section
-// which provides access to notary configurations
-// of the side chain.
-type NotaryConfig struct {
-	cfg *config.Config
-}
-
-// Notary returns structure that provides access to "notary"
-// subsection of "morph" section.
-func Notary(c *config.Config) *NotaryConfig {
-	return &NotaryConfig{
-		cfg: c.Sub(subsection).Sub(notarySubsection),
-	}
-}
-
-// Amount returns value of "deposit_amount" config parameter.
-//
-// Returns NotaryDepositAmountDefault if value is not positive amount.
-func (n NotaryConfig) Amount() fixedn.Fixed8 {
-	v := config.UintSafe(n.cfg, "deposit_amount")
-	if v > 0 {
-		return fixedn.Fixed8(v)
-	}
-
-	return fixedn.Fixed8(NotaryDepositAmountDefault)
-}
-
-func (n NotaryConfig) Duration() uint32 {
-	v := config.Uint32Safe(n.cfg, "deposit_duration")
-	if v > 0 {
-		return v
-	}
-
-	return NotaryDepositDurationDefault
 }
