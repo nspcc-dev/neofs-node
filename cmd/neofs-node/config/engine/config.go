2126235f0e1999da151bb02e54e3d424696e735b
Leonard Lyubich <leonard@nspcc.ru>
2021-10-08 16:25:56 +0300

    [#674] node: Configure size of per-shard worker pools
    Add `shard_pool_size` config to `storage` section. Set app default to 20.
    Pass the value to `WithShardPoolSize` option.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- cmd/neofs-node/config/engine/config.go
+++ cmd/neofs-node/config/engine/config.go
@@ -7,13 +7,21 @@ import (
 	shardconfig "github.com/nspcc-dev/neofs-node/cmd/neofs-node/config/engine/shard"
 )
 
+const (
+	subsection = "storage"
+
+	// ShardPoolSizeDefault is a default value of routine pool size per-shard to
+	// process object PUT operations in storage engine.
+	ShardPoolSizeDefault = 20
+)
+
 // IterateShards iterates over subsections ["0":"N") (N - "shard_num" value)
 // of "shard" subsection of "storage" section of c, wrap them into
 // shardconfig.Config and passes to f.
 //
 // Panics if N is not a positive number.
 func IterateShards(c *config.Config, f func(*shardconfig.Config)) {
-	c = c.Sub("storage")
+	c = c.Sub(subsection)
 
 	num := config.Uint(c, "shard_num")
 	if num == 0 {
@@ -31,4 +39,16 @@ func IterateShards(c *config.Config, f func(*shardconfig.Config)) {
 
 		f(sc)
 	}
+}
+
+// ShardPoolSize returns value of "shard_pool_size" config parameter from "storage" section.
+//
+// Returns ShardPoolSizeDefault if value is not a positive number.
+func ShardPoolSize(c *config.Config) uint32 {
+	v := config.Uint32Safe(c.Sub(subsection), "shard_pool_size")
+	if v > 0 {
+		return v
+	}
+
+	return ShardPoolSizeDefault
 }
