4dd10ba4351caeafdf14e06519109a2037fd384f
Leonard Lyubich <leonard@nspcc.ru>
2021-11-09 18:40:08 +0300

    [#922] storage engine: Remove unused Exists method
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/local_object_storage/engine/exists.go
+++ pkg/local_object_storage/engine/exists.go
@@ -9,46 +9,6 @@ import (
 	"go.uber.org/zap"
 )
 
-// ExistsPrm groups the parameters of Exists operation.
-type ExistsPrm struct {
-	addr *objectSDK.Address
-}
-
-// ExistsRes groups resulting values of Exists operation.
-type ExistsRes struct {
-	ex bool
-}
-
-// WithAddress is an Exists option to set object checked for existence.
-func (p *ExistsPrm) WithAddress(addr *objectSDK.Address) *ExistsPrm {
-	if p != nil {
-		p.addr = addr
-	}
-
-	return p
-}
-
-// Exists returns the fact that the object is in the storage engine.
-func (p *ExistsRes) Exists() bool {
-	return p.ex
-}
-
-// Exists checks if object is presented in storage engine
-//
-// Returns any error encountered that does not allow to
-// unambiguously determine the presence of an object.
-func (e *StorageEngine) Exists(prm *ExistsPrm) (*ExistsRes, error) {
-	if e.metrics != nil {
-		defer elapsed(e.metrics.AddExistsDuration)()
-	}
-
-	exists, err := e.exists(prm.addr)
-
-	return &ExistsRes{
-		ex: exists,
-	}, err
-}
-
 func (e *StorageEngine) exists(addr *objectSDK.Address) (bool, error) {
 	shPrm := new(shard.ExistsPrm).WithAddress(addr)
 	alreadyRemoved := false
