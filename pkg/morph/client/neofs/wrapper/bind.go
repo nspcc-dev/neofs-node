b1b1e38ffb9afe85004d1ecb87aa15f44ae1701d
Pavel Karpy <carpawell@nspcc.ru>
2021-11-10 13:41:27 +0300

    [#971] morph/neofs: Add optional parameters
    Add optional parameters to the client call
    signature. Group parameters of a client call
    into struct to improve future codebase
    support.
    
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/morph/client/neofs/wrapper/bind.go
+++ pkg/morph/client/neofs/wrapper/bind.go
@@ -1,14 +1,40 @@
 package neofscontract
 
 import (
+	"github.com/nspcc-dev/neofs-node/pkg/morph/client"
 	neofscontract "github.com/nspcc-dev/neofs-node/pkg/morph/client/neofs"
 )
 
+// ManageKeysPrm groups parameters of ManageKeys operation.
+type ManageKeysPrm struct {
+	scriptHash []byte
+	ks         [][]byte
+	bind       bool
+
+	client.InvokePrmOptional
+}
+
+// SetScriptHash sets script hash.
+func (m *ManageKeysPrm) SetScriptHash(scriptHash []byte) {
+	m.scriptHash = scriptHash
+}
+
+// SetKeys sets keys.
+func (m *ManageKeysPrm) SetKeys(ks [][]byte) {
+	m.ks = ks
+}
+
+// SetBind sets operation type: bind/unbind.
+func (m *ManageKeysPrm) SetBind(bind bool) {
+	m.bind = bind
+}
+
 // ManageKeys binds/unbinds list of public keys from NeoFS account by script hash.
-func (x *ClientWrapper) ManageKeys(scriptHash []byte, ks [][]byte, bind bool) error {
+func (x *ClientWrapper) ManageKeys(prm ManageKeysPrm) error {
 	type args interface {
 		SetScriptHash([]byte)
 		SetKeys([][]byte)
+		SetOptionalPrm(optional client.InvokePrmOptional)
 	}
 
 	var (
@@ -16,7 +42,7 @@ func (x *ClientWrapper) ManageKeys(scriptHash []byte, ks [][]byte, bind bool) er
 		call func(args) error
 	)
 
-	if bind {
+	if prm.bind {
 		a = new(neofscontract.BindKeysArgs)
 		call = func(a args) error {
 			return x.client.BindKeys(*a.(*neofscontract.BindKeysArgs))
@@ -28,8 +54,9 @@ func (x *ClientWrapper) ManageKeys(scriptHash []byte, ks [][]byte, bind bool) er
 		}
 	}
 
-	a.SetScriptHash(scriptHash)
-	a.SetKeys(ks)
+	a.SetScriptHash(prm.scriptHash)
+	a.SetKeys(prm.ks)
+	a.SetOptionalPrm(prm.InvokePrmOptional)
 
 	return call(a)
 }
