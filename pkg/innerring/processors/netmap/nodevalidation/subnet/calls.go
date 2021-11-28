556877fb5e5f2e11c9954fd2a8c3eea85095bc29
Pavel Karpy <carpawell@nspcc.ru>
2021-11-28 15:56:46 +0300

    [#990] nodeValidators: Add subnet entrance validator
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

---
+++ pkg/innerring/processors/netmap/nodevalidation/subnet/calls.go
@@ -0,0 +1,41 @@
+package subnet
+
+import (
+	"fmt"
+
+	morphsubnet "github.com/nspcc-dev/neofs-node/pkg/morph/client/subnet"
+	"github.com/nspcc-dev/neofs-sdk-go/netmap"
+	subnetid "github.com/nspcc-dev/neofs-sdk-go/subnet/id"
+)
+
+// VerifyAndUpdate calls subnet contract's `NodeAllowed` method.
+// Removes subnets that have not been approved by the contract.
+func (v *Validator) VerifyAndUpdate(n *netmap.NodeInfo) error {
+	prm := morphsubnet.NodeAllowedPrm{}
+
+	err := n.IterateSubnets(func(id subnetid.ID) error {
+		rawSubnetID, err := id.Marshal()
+		if err != nil {
+			return fmt.Errorf("could not marshal subnetwork ID: %w", err)
+		}
+
+		prm.SetID(rawSubnetID)
+		prm.SetNode(n.PublicKey())
+
+		res, err := v.subnetClient.NodeAllowed(prm)
+		if err != nil {
+			return fmt.Errorf("could not call `NodeAllowed` contract method: %w", err)
+		}
+
+		if !res.Allowed() {
+			return netmap.ErrRemoveSubnet
+		}
+
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("could not verify subnet entrance of the node: %w", err)
+	}
+
+	return nil
+}
