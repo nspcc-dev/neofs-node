cd75638ce30e10fda10df5880add70e86406e4c6
Evgenii Stratonikov <evgeniy@nspcc.ru>
2022-01-20 19:32:49 +0300

    [#1085] blobstor: allow to ignore errors during iteration
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- pkg/local_object_storage/blobstor/iterate_test.go
+++ pkg/local_object_storage/blobstor/iterate_test.go
@@ -1,10 +1,15 @@
 package blobstor
 
 import (
+	"bytes"
 	"encoding/binary"
+	"errors"
 	"os"
+	"path/filepath"
+	"strconv"
 	"testing"
 
+	"github.com/klauspost/compress/zstd"
 	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/blobovnicza"
 	"github.com/nspcc-dev/neofs-sdk-go/object"
 	objecttest "github.com/nspcc-dev/neofs-sdk-go/object/test"
@@ -88,4 +93,112 @@ func TestIterateObjects(t *testing.T) {
 	})
 	require.NoError(t, err)
 	require.Empty(t, mObjs)
+}
+
+func TestIterate_IgnoreErrors(t *testing.T) {
+	dir := t.TempDir()
+
+	const (
+		smallSize = 512
+		objCount  = 5
+	)
+	bsOpts := []Option{
+		WithCompressObjects(true),
+		WithRootPath(dir),
+		WithSmallSizeLimit(smallSize * 2), // + header
+		WithBlobovniczaOpenedCacheSize(1),
+		WithBlobovniczaShallowWidth(1),
+		WithBlobovniczaShallowDepth(1)}
+	bs := New(bsOpts...)
+	require.NoError(t, bs.Open())
+	require.NoError(t, bs.Init())
+
+	addrs := make([]*object.Address, objCount)
+	for i := range addrs {
+		addrs[i] = objecttest.Address()
+		obj := object.NewRaw()
+		obj.SetContainerID(addrs[i].ContainerID())
+		obj.SetID(addrs[i].ObjectID())
+		obj.SetPayload(make([]byte, smallSize<<(i%2)))
+
+		objData, err := obj.Marshal()
+		require.NoError(t, err)
+
+		_, err = bs.PutRaw(addrs[i], objData, true)
+		require.NoError(t, err)
+	}
+
+	// Construct corrupted compressed object.
+	buf := bytes.NewBuffer(nil)
+	badObject := make([]byte, smallSize/2+1)
+	enc, err := zstd.NewWriter(buf)
+	require.NoError(t, err)
+	rawData := enc.EncodeAll(badObject, nil)
+	for i := 4; /* magic size */ i < len(rawData); i += 2 {
+		rawData[i] ^= 0xFF
+	}
+	// Will be put uncompressed but fetched as compressed because of magic.
+	_, err = bs.PutRaw(objecttest.Address(), rawData, false)
+	require.NoError(t, err)
+	require.NoError(t, bs.fsTree.Put(objecttest.Address(), rawData))
+
+	require.NoError(t, bs.Close())
+
+	// Increase width to have blobovnicza which is definitely empty.
+	b := New(append(bsOpts, WithBlobovniczaShallowWidth(2))...)
+	require.NoError(t, b.Open())
+	require.NoError(t, b.Init())
+
+	var p string
+	for i := 0; i < 2; i++ {
+		bp := filepath.Join(bs.blzRootPath, "1", strconv.FormatUint(uint64(i), 10))
+		if _, ok := bs.blobovniczas.opened.Get(bp); !ok {
+			p = bp
+			break
+		}
+	}
+	require.NotEqual(t, "", p, "expected to not have at least 1 blobovnicza in cache")
+	require.NoError(t, os.Chmod(p, 0))
+
+	var prm IteratePrm
+	prm.SetIterationHandler(func(e IterationElement) error {
+		return nil
+	})
+	_, err = bs.Iterate(prm)
+	require.Error(t, err)
+
+	prm.IgnoreErrors()
+
+	t.Run("skip invalid objects", func(t *testing.T) {
+		actual := make([]*object.Address, 0, len(addrs))
+		prm.SetIterationHandler(func(e IterationElement) error {
+			obj := object.New()
+			err := obj.Unmarshal(e.data)
+			if err != nil {
+				return err
+			}
+
+			addr := object.NewAddress()
+			addr.SetContainerID(obj.ContainerID())
+			addr.SetObjectID(obj.ID())
+			actual = append(actual, addr)
+			return nil
+		})
+
+		_, err := bs.Iterate(prm)
+		require.NoError(t, err)
+		require.ElementsMatch(t, addrs, actual)
+	})
+	t.Run("return errors from handler", func(t *testing.T) {
+		n := 0
+		expectedErr := errors.New("expected error")
+		prm.SetIterationHandler(func(e IterationElement) error {
+			if n++; n == objCount/2 {
+				return expectedErr
+			}
+			return nil
+		})
+		_, err := bs.Iterate(prm)
+		require.True(t, errors.Is(err, expectedErr), "got: %v")
+	})
 }
