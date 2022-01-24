ef9fb6091ff74a49737af06056543af4c88b43e0
Evgenii Stratonikov <evgeniy@nspcc.ru>
2022-01-24 13:13:37 +0300

    [#1086] services/control: implement `DumpShard` RPC
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

---
+++ pkg/local_object_storage/engine/dump.go
@@ -0,0 +1,19 @@
+package engine
+
+import "github.com/nspcc-dev/neofs-node/pkg/local_object_storage/shard"
+
+// DumpShard dumps objects from the shard with provided identifier.
+//
+// Returns an error if shard is not read-only.
+func (e *StorageEngine) DumpShard(id *shard.ID, prm *shard.DumpPrm) error {
+	e.mtx.RLock()
+	defer e.mtx.RUnlock()
+
+	sh, ok := e.shards[id.String()]
+	if !ok {
+		return errShardNotFound
+	}
+
+	_, err := sh.Dump(prm)
+	return err
+}
