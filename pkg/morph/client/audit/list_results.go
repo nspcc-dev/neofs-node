819d80a7a9da589f58e7384e1996867b125d73c4
Evgenii Stratonikov <evgeniy@nspcc.ru>
2022-01-31 15:32:51 +0300

    [#625] client/audit: remove intermediate wrapper
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- pkg/morph/client/audit/list_results.go
+++ pkg/morph/client/audit/list_results.go
@@ -5,125 +5,74 @@ import (
 
 	"github.com/nspcc-dev/neo-go/pkg/vm/stackitem"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/client"
+	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
 )
 
-// ListResultsArgs groups the arguments
-// of "list all audit result IDs" test invoke call.
-type ListResultsArgs struct{}
-
-// ListResultsByEpochArgs groups the arguments
-// of "list audit result IDs by epoch" test invoke call.
-type ListResultsByEpochArgs struct {
-	epoch int64
-}
-
-// ListResultsByCIDArgs groups the arguments
-// of "list audit result IDs by epoch and CID" test invoke call.
-type ListResultsByCIDArgs struct {
-	ListResultsByEpochArgs
-
-	cid []byte
-}
-
-// ListResultsByNodeArgs groups the arguments
-// of "list audit result IDs by epoch, CID, and node key" test invoke call.
-type ListResultsByNodeArgs struct {
-	ListResultsByCIDArgs
-
-	nodeKey []byte
-}
-
-// ListResultsValues groups the stack parameters
-// returned by "list audit results" test invoke.
-type ListResultsValues struct {
-	rawResults [][]byte // audit results in a binary format
-}
-
-// RawResults returns list of audit result IDs
-// in a binary format.
-func (v *ListResultsValues) RawResults() [][]byte {
-	return v.rawResults
-}
-
-// SetEpoch sets epoch of listing audit results.
-func (v *ListResultsByEpochArgs) SetEpoch(epoch int64) {
-	v.epoch = epoch
-}
-
-// SetCID sets container ID of listing audit results.
-func (v *ListResultsByCIDArgs) SetCID(cid []byte) {
-	v.cid = cid
-}
-
-// SetNodeKey sets public key of node that produced listing audit results.
-func (v *ListResultsByNodeArgs) SetNodeKey(key []byte) {
-	v.nodeKey = key
-}
-
-// ListAuditResults performs the test invoke of "list all audit result IDs"
-// method of NeoFS Audit contract.
-func (c *Client) ListAuditResults(args ListResultsArgs) (*ListResultsValues, error) {
+// ListAllAuditResultID returns a list of all audit result IDs inside audit contract.
+func (c *Client) ListAllAuditResultID() ([]ResultID, error) {
 	invokePrm := client.TestInvokePrm{}
-
 	invokePrm.SetMethod(listResultsMethod)
 
 	items, err := c.client.TestInvoke(invokePrm)
 	if err != nil {
 		return nil, fmt.Errorf("could not perform test invocation (%s): %w", listResultsMethod, err)
 	}
-
 	return parseAuditResults(items, listResultsMethod)
 }
 
-// ListAuditResultsByEpoch performs the test invoke of "list audit result IDs
-// by epoch" method of NeoFS Audit contract.
-func (c *Client) ListAuditResultsByEpoch(args ListResultsByEpochArgs) (*ListResultsValues, error) {
-	invokePrm := client.TestInvokePrm{}
+// ListAuditResultIDByEpoch returns a list of audit result IDs inside audit
+// contract for specific epoch number.
+func (c *Client) ListAuditResultIDByEpoch(epoch uint64) ([]ResultID, error) {
+	prm := client.TestInvokePrm{}
+	prm.SetMethod(listByEpochResultsMethod)
+	prm.SetArgs(int64(epoch))
 
-	invokePrm.SetMethod(listByEpochResultsMethod)
-	invokePrm.SetArgs(args.epoch)
-
-	items, err := c.client.TestInvoke(invokePrm)
+	items, err := c.client.TestInvoke(prm)
 	if err != nil {
 		return nil, fmt.Errorf("could not perform test invocation (%s): %w", listByEpochResultsMethod, err)
 	}
-
 	return parseAuditResults(items, listByEpochResultsMethod)
 }
 
-// ListAuditResultsByCID performs the test invoke of "list audit result IDs
-// by epoch and CID" method of NeoFS Audit contract.
-func (c *Client) ListAuditResultsByCID(args ListResultsByCIDArgs) (*ListResultsValues, error) {
-	invokePrm := client.TestInvokePrm{}
+// ListAuditResultIDByCID returns a list of audit result IDs inside audit
+// contract for specific epoch number and container ID.
+func (c *Client) ListAuditResultIDByCID(epoch uint64, cid *cid.ID) ([]ResultID, error) {
+	v2 := cid.ToV2()
+	if v2 == nil {
+		return nil, errUnsupported // use other major version if there any
+	}
 
-	invokePrm.SetMethod(listByCIDResultsMethod)
-	invokePrm.SetArgs(args.epoch, args.cid)
+	prm := client.TestInvokePrm{}
+	prm.SetMethod(listByCIDResultsMethod)
+	prm.SetArgs(int64(epoch), v2.GetValue())
 
-	items, err := c.client.TestInvoke(invokePrm)
+	items, err := c.client.TestInvoke(prm)
 	if err != nil {
 		return nil, fmt.Errorf("could not perform test invocation (%s): %w", listByCIDResultsMethod, err)
 	}
-
 	return parseAuditResults(items, listByCIDResultsMethod)
 }
 
-// ListAuditResultsByNode performs the test invoke of "list audit result IDs
-// by epoch, CID, and node key" method of NeoFS Audit contract.
-func (c *Client) ListAuditResultsByNode(args ListResultsByNodeArgs) (*ListResultsValues, error) {
-	invokePrm := client.TestInvokePrm{}
+// ListAuditResultIDByNode returns a list of audit result IDs inside audit
+// contract for specific epoch number, container ID and inner ring public key.
+func (c *Client) ListAuditResultIDByNode(epoch uint64, cid *cid.ID, nodeKey []byte) ([]ResultID, error) {
+	v2 := cid.ToV2()
+	if v2 == nil {
+		return nil, errUnsupported // use other major version if there any
+	}
 
-	invokePrm.SetMethod(listByNodeResultsMethod)
-	invokePrm.SetArgs(args.epoch, args.cid, args.nodeKey)
+	prm := client.TestInvokePrm{}
+	prm.SetMethod(listByNodeResultsMethod)
+	prm.SetArgs(int64(epoch), v2.GetValue(), nodeKey)
 
-	items, err := c.client.TestInvoke(invokePrm)
+	items, err := c.client.TestInvoke(prm)
 	if err != nil {
 		return nil, fmt.Errorf("could not perform test invocation (%s): %w", listByNodeResultsMethod, err)
 	}
-
 	return parseAuditResults(items, listByNodeResultsMethod)
 }
 
-func parseAuditResults(items []stackitem.Item, method string) (*ListResultsValues, error) {
+func parseAuditResults(items []stackitem.Item, method string) ([]ResultID, error) {
 	if ln := len(items); ln != 1 {
 		return nil, fmt.Errorf("unexpected stack item count (%s): %d", method, ln)
 	}
@@ -133,17 +82,14 @@ func parseAuditResults(items []stackitem.Item, method string) (*ListResultsValue
 		return nil, fmt.Errorf("could not get stack item array from stack item (%s): %w", method, err)
 	}
 
-	res := &ListResultsValues{
-		rawResults: make([][]byte, 0, len(items)),
-	}
-
+	res := make([]ResultID, 0, len(items))
 	for i := range items {
 		rawRes, err := client.BytesFromStackItem(items[i])
 		if err != nil {
 			return nil, fmt.Errorf("could not get byte array from stack item (%s): %w", method, err)
 		}
 
-		res.rawResults = append(res.rawResults, rawRes)
+		res = append(res, rawRes)
 	}
 
 	return res, nil
