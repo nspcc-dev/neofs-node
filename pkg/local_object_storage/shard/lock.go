156972d79d35f23d5dbb8a0b5ef5e5ebab2e363c
Leonard Lyubich <leonard@nspcc.ru>
2022-02-16 01:20:44 +0300

    [#1175] shard: Implement Lock operation
    Implement `Shard.Lock` method which required rw mode and calls `Lock` on
    underlying metabase.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/local_object_storage/shard/lock.go
@@ -0,0 +1,27 @@
+package shard
+
+import (
+	"fmt"
+
+	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
+	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
+)
+
+// Lock marks objects as locked with another object. All objects from the
+// specified container.
+//
+// Allows locking regular objects only (otherwise returns apistatus.IrregularObjectLock).
+//
+// Locked list should be unique. Panics if it is empty.
+func (s *Shard) Lock(idCnr cid.ID, locker oid.ID, locked []oid.ID) error {
+	if s.GetMode() == ModeReadOnly {
+		return ErrReadOnlyMode
+	}
+
+	err := s.metaBase.Lock(idCnr, locker, locked)
+	if err != nil {
+		return fmt.Errorf("metabase lock: %w", err)
+	}
+
+	return nil
+}
