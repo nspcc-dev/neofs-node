006d6e8b48b947c0383de6fa0de9b69976a0e783
Evgenii Stratonikov <evgeniy@nspcc.ru>
2022-03-28 12:36:04 +0300

    [#1216] neofs-cli: Reuse `GetSDKClient` between CLI modules
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

---
+++ cmd/neofs-cli/internal/client/sdk.go
@@ -0,0 +1,35 @@
+package internal
+
+import (
+	"crypto/ecdsa"
+	"crypto/tls"
+	"fmt"
+
+	"github.com/nspcc-dev/neofs-node/pkg/network"
+	"github.com/nspcc-dev/neofs-sdk-go/client"
+)
+
+// GetSDKClient returns default neofs-sdk-go client.
+func GetSDKClient(key *ecdsa.PrivateKey, addr network.Address) (*client.Client, error) {
+	var (
+		c       client.Client
+		prmInit client.PrmInit
+		prmDial client.PrmDial
+	)
+
+	prmInit.SetDefaultPrivateKey(*key)
+	prmInit.ResolveNeoFSFailures()
+	prmDial.SetServerURI(addr.HostAddr())
+
+	if addr.TLSEnabled() {
+		prmDial.SetTLSConfig(&tls.Config{})
+	}
+
+	c.Init(prmInit)
+
+	if err := c.Dial(prmDial); err != nil {
+		return nil, fmt.Errorf("can't init SDK client: %w", err)
+	}
+
+	return &c, nil
+}
