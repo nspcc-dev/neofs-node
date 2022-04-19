3e8735bfe5e42f21f27dc6eff96e0430ea55430f
Pavel Karpy <carpawell@nspcc.ru>
2022-04-19 20:59:13 +0300

    [#1318] service: Add tombstone checker service
    The service fetches tombstones from the network via object service, every
    request is handled in the following order:
    1. checks local LRU cache;
    2. checks local storage engine;
    3. tries to find object in the placement nodes.
    
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

---
+++ pkg/services/object_manager/tombstone/constructor.go
@@ -0,0 +1,84 @@
+package tombstone
+
+import (
+	"fmt"
+
+	lru "github.com/hashicorp/golang-lru"
+	"go.uber.org/zap"
+)
+
+const defaultLRUCacheSize = 100
+
+type cfg struct {
+	log *zap.Logger
+
+	cacheSize int
+
+	tsSource Source
+}
+
+// Option is an option of ExpirationChecker's constructor.
+type Option func(*cfg)
+
+func defaultCfg() *cfg {
+	return &cfg{
+		log:       zap.NewNop(),
+		cacheSize: defaultLRUCacheSize,
+	}
+}
+
+// NewChecker creates, initializes and returns tombstone ExpirationChecker.
+// The returned structure is ready to use.
+//
+// Panics if any of the provided options does not allow
+// constructing a valid tombstone ExpirationChecker.
+func NewChecker(oo ...Option) *ExpirationChecker {
+	cfg := defaultCfg()
+
+	for _, o := range oo {
+		o(cfg)
+	}
+
+	panicOnNil := func(v interface{}, name string) {
+		if v == nil {
+			panic(fmt.Sprintf("tombstone getter constructor: %s is nil", name))
+		}
+	}
+
+	panicOnNil(cfg.tsSource, "Tombstone source")
+
+	cache, err := lru.New(cfg.cacheSize)
+	if err != nil {
+		panic(fmt.Errorf("could not create LRU cache with %d size: %w", cfg.cacheSize, err))
+	}
+
+	return &ExpirationChecker{
+		cache:    cache,
+		log:      cfg.log,
+		tsSource: cfg.tsSource,
+	}
+}
+
+// WithLogger returns an option to specify
+// logger.
+func WithLogger(v *zap.Logger) Option {
+	return func(c *cfg) {
+		c.log = v
+	}
+}
+
+// WithCacheSize returns an option to specify
+// LRU cache size.
+func WithCacheSize(v int) Option {
+	return func(c *cfg) {
+		c.cacheSize = v
+	}
+}
+
+// WithTombstoneSource returns an option
+// to specify tombstone source.
+func WithTombstoneSource(v Source) Option {
+	return func(c *cfg) {
+		c.tsSource = v
+	}
+}
