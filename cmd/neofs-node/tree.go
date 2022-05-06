e6104d8a630edf0083c30360f92ae3700ce9e63c
Evgenii Stratonikov <evgeniy@nspcc.ru>
2022-05-06 14:58:36 +0300

    [#1342] neofs-node: Use the default endpoint for tree service
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- cmd/neofs-node/tree.go
+++ cmd/neofs-node/tree.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"context"
-	"net"
 
-	treeconfig "github.com/nspcc-dev/neofs-node/cmd/neofs-node/config/tree"
 	"github.com/nspcc-dev/neofs-node/pkg/services/tree"
-	"google.golang.org/grpc"
 )
 
 func initTreeService(c *cfg) {
-	endpoint := treeconfig.GRPC(c.appCfg).Endpoint()
-	if endpoint == treeconfig.GRPCEndpointDefault {
-		return
-	}
-
 	treeSvc := tree.New(
 		tree.WithContainerSource(c.cfgObject.cnrSource),
 		tree.WithNetmapSource(c.netMapSource),
@@ -22,20 +14,13 @@ func initTreeService(c *cfg) {
 		tree.WithLogger(c.log),
 		tree.WithStorage(c.cfgObject.cfgLocalStorage.localStorage))
 
-	treeServer := grpc.NewServer()
-
-	c.onShutdown(func() {
-		stopGRPC("NeoFS Tree Service API", treeServer, c.log)
-		treeSvc.Shutdown()
-	})
-
-	lis, err := net.Listen("tcp", endpoint)
-	fatalOnErr(err)
-
-	tree.RegisterTreeServiceServer(treeServer, treeSvc)
+	for _, srv := range c.cfgGRPC.servers {
+		tree.RegisterTreeServiceServer(srv, treeSvc)
+	}
 
 	c.workers = append(c.workers, newWorkerFromFunc(func(ctx context.Context) {
 		treeSvc.Start(ctx)
-		fatalOnErr(treeServer.Serve(lis))
 	}))
+
+	c.onShutdown(treeSvc.Shutdown)
 }
