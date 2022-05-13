61c58319fbc2b0bcf0eca6d4ae6a02780be88d1e
Pavel Karpy <carpawell@nspcc.ru>
2022-05-13 21:35:37 +0300

    [#1224] morph: Call Alphabet methods flexibly
    Call `UpdateStateIR` and `AddPeerIR` method instead of `UpdateState` and
    `AddPeer` if calling client is configured as Alphabet in notary enabled
    environment.
    
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/morph/client/netmap/add_peer.go
+++ pkg/morph/client/netmap/add_peer.go
@@ -22,22 +22,31 @@ func (a *AddPeerPrm) SetNodeInfo(nodeInfo *netmap.NodeInfo) {
 // AddPeer registers peer in NeoFS network through
 // Netmap contract call.
 func (c *Client) AddPeer(p AddPeerPrm) error {
+	var method = addPeerMethod
+
+	if c.client.WithNotary() && c.client.IsAlpha() {
+		// In notary environments Alphabet must calls AddPeerIR method instead of AddPeer.
+		// It differs from AddPeer only by name, so we can do this in the same form.
+		// See https://github.com/nspcc-dev/neofs-contract/issues/154.
+		method += "IR"
+	}
+
 	if p.nodeInfo == nil {
-		return fmt.Errorf("nil node info (%s)", addPeerMethod)
+		return fmt.Errorf("nil node info (%s)", method)
 	}
 
 	rawNodeInfo, err := p.nodeInfo.Marshal()
 	if err != nil {
-		return fmt.Errorf("can't marshal node info (%s): %w", addPeerMethod, err)
+		return fmt.Errorf("can't marshal node info (%s): %w", method, err)
 	}
 
 	prm := client.InvokePrm{}
-	prm.SetMethod(addPeerMethod)
+	prm.SetMethod(method)
 	prm.SetArgs(rawNodeInfo)
 	prm.InvokePrmOptional = p.InvokePrmOptional
 
 	if err := c.client.Invoke(prm); err != nil {
-		return fmt.Errorf("could not invoke method (%s): %w", addPeerMethod, err)
+		return fmt.Errorf("could not invoke method (%s): %w", method, err)
 	}
 	return nil
 }
