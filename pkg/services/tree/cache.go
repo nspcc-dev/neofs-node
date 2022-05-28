c4dd30d93a90b50142f82cf0b49b78697305e8b7
Evgenii Stratonikov <evgeniy@nspcc.ru>
2022-05-28 12:27:55 +0300

    [#1446] services/tree: Cache connections to the container nodes
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

---
+++ pkg/services/tree/cache.go
@@ -0,0 +1,63 @@
+package tree
+
+import (
+	"context"
+	"sync"
+
+	"github.com/hashicorp/golang-lru/simplelru"
+	"github.com/nspcc-dev/neofs-node/pkg/network"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
+)
+
+type clientCache struct {
+	sync.Mutex
+	simplelru.LRU
+}
+
+const defaultClientCacheSize = 10
+
+func (c *clientCache) init() {
+	l, _ := simplelru.NewLRU(defaultClientCacheSize, func(key, value interface{}) {
+		_ = value.(*grpc.ClientConn).Close()
+	})
+	c.LRU = *l
+}
+
+func (c *clientCache) get(ctx context.Context, netmapAddr string) (TreeServiceClient, error) {
+	c.Lock()
+	ccInt, ok := c.LRU.Get(netmapAddr)
+	c.Unlock()
+
+	if ok {
+		cc := ccInt.(*grpc.ClientConn)
+		if s := cc.GetState(); s == connectivity.Idle || s == connectivity.Ready {
+			return NewTreeServiceClient(cc), nil
+		}
+		_ = cc.Close()
+	}
+
+	cc, err := dialTreeService(ctx, netmapAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	c.Lock()
+	c.LRU.Add(netmapAddr, cc)
+	c.Unlock()
+
+	return NewTreeServiceClient(cc), nil
+}
+
+func dialTreeService(ctx context.Context, netmapAddr string) (*grpc.ClientConn, error) {
+	var netAddr network.Address
+	if err := netAddr.FromString(netmapAddr); err != nil {
+		return nil, err
+	}
+
+	cc, err := grpc.DialContext(ctx, netAddr.URIAddr(), grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	return cc, nil
+}
