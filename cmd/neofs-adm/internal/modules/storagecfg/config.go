190378c0d9017a3a94ef5ea907d6e1d8a788900c
Evgenii Stratonikov <evgeniy@nspcc.ru>
2022-07-19 14:13:36 +0300

    [#1610] neofs-adm: Fix generated storage config
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- cmd/neofs-adm/internal/modules/storagecfg/config.go
+++ cmd/neofs-adm/internal/modules/storagecfg/config.go
@@ -44,32 +44,30 @@ morph:
 {{if not .Relay }}
 storage:
   shard_pool_size: 15  # size of per-shard worker pools used for PUT operations
-  shard_num: 1  # total number of shards
-
-  default: # section with the default shard parameters
-    metabase:
-      perm: 0644  # permissions for metabase files(directories: +x for current user and group)
-
-    blobstor:
-      perm: 0644  # permissions for blobstor files(directories: +x for current user and group)
-      depth: 2  # max depth of object tree storage in FS
-      small_object_size: 102400  # 100KiB, size threshold for "small" objects which are stored in key-value DB, not in FS, bytes
-      compress: true  # turn on/off Zstandard compression (level 3) of stored objects
-      compression_exclude_content_types:
-        - audio/*
-        - video/*
-
-      blobovnicza:
-        size: 1073741824  # approximate size limit of single blobovnicza instance, total size will be: size*width^(depth+1), bytes
-        depth: 1  # max depth of object tree storage in key-value DB
-        width: 4   # max width of object tree storage in key-value DB
-        opened_cache_capacity: 50  # maximum number of opened database files
-
-    gc:
-      remover_batch_size: 200  # number of objects to be removed by the garbage collector
-      remover_sleep_interval: 5m  # frequency of the garbage collector invocation
 
   shard:
+    default: # section with the default shard parameters
+      metabase:
+        perm: 0644  # permissions for metabase files(directories: +x for current user and group)
+
+      blobstor:
+        perm: 0644  # permissions for blobstor files(directories: +x for current user and group)
+        depth: 2  # max depth of object tree storage in FS
+        small_object_size: 102400  # 100KiB, size threshold for "small" objects which are stored in key-value DB, not in FS, bytes
+        compress: true  # turn on/off Zstandard compression (level 3) of stored objects
+        compression_exclude_content_types:
+          - audio/*
+          - video/*
+
+        blobovnicza:
+          size: 1073741824  # approximate size limit of single blobovnicza instance, total size will be: size*width^(depth+1), bytes
+          depth: 1  # max depth of object tree storage in key-value DB
+          width: 4   # max width of object tree storage in key-value DB
+          opened_cache_capacity: 50  # maximum number of opened database files
+
+      gc:
+        remover_batch_size: 200  # number of objects to be removed by the garbage collector
+        remover_sleep_interval: 5m  # frequency of the garbage collector invocation
     0:
       mode: "read-write"  # mode of the shard, must be one of the: "read-write" (default), "read-only"
 
