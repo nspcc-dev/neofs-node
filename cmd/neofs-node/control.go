23906784344d9a32a417d20d7d946742b8e9b1b9
Pavel Karpy <carpawell@nspcc.ru>
2022-07-19 18:17:12 +0300

    [#1614] node: Update health metric on its change
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- cmd/neofs-node/control.go
+++ cmd/neofs-node/control.go
@@ -69,6 +69,10 @@ func (c *cfg) NetmapStatus() control.NetmapStatus {
 
 func (c *cfg) setHealthStatus(st control.HealthStatus) {
 	c.healthStatus.Store(int32(st))
+
+	if c.metricsCollector != nil {
+		c.metricsCollector.SetHealth(int32(st))
+	}
 }
 
 func (c *cfg) HealthStatus() control.HealthStatus {
