c7c1c257e1b0da0eafff9943f3297581817778f1
Pavel Karpy <carpawell@nspcc.ru>
2022-08-25 18:58:18 +0300

    [#1658] node: Init object counter on meta's `Init`
    Includes:
    1. Renaming counter key to distinguish logical and physical objects
    2. Version update dropping since changes could be done in a compatible way
    
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/local_object_storage/metabase/counter.go
+++ pkg/local_object_storage/metabase/counter.go
@@ -2,11 +2,14 @@ package meta
 
 import (
 	"encoding/binary"
+	"fmt"
 
+	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
+	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
 	"go.etcd.io/bbolt"
 )
 
-var shardCounterKey = []byte("counter")
+var objectCounterKey = []byte("phy_counter")
 
 // ObjectCounter returns object count that metabase has
 // tracked since it was opened and initialized.
@@ -17,7 +20,7 @@ func (db *DB) ObjectCounter() (counter uint64, err error) {
 	err = db.boltDB.View(func(tx *bbolt.Tx) error {
 		b := tx.Bucket(shardInfoBucket)
 		if b != nil {
-			data := b.Get(shardCounterKey)
+			data := b.Get(objectCounterKey)
 			if len(data) == 8 {
 				counter = binary.LittleEndian.Uint64(data)
 			}
@@ -39,7 +42,7 @@ func (db *DB) updateCounter(tx *bbolt.Tx, delta uint64, inc bool) error {
 
 	var counter uint64
 
-	data := b.Get(shardCounterKey)
+	data := b.Get(objectCounterKey)
 	if len(data) == 8 {
 		counter = binary.LittleEndian.Uint64(data)
 	}
@@ -55,5 +58,42 @@ func (db *DB) updateCounter(tx *bbolt.Tx, delta uint64, inc bool) error {
 	newCounter := make([]byte, 8)
 	binary.LittleEndian.PutUint64(newCounter, counter)
 
-	return b.Put(shardCounterKey, newCounter)
+	return b.Put(objectCounterKey, newCounter)
+}
+
+// syncCounter updates object counter according to metabase state:
+// it counts all the physically stored objects using internal indexes.
+// Tx MUST be writable.
+//
+// Does nothing if counter not empty.
+func syncCounter(tx *bbolt.Tx) error {
+	var counter uint64
+
+	b, err := tx.CreateBucketIfNotExists(shardInfoBucket)
+	if err != nil {
+		return fmt.Errorf("could not get shard info bucket: %w", err)
+	}
+
+	data := b.Get(objectCounterKey)
+	if len(data) == 8 {
+		return nil
+	}
+
+	err = iteratePhyObjects(tx, func(_ cid.ID, _ oid.ID) error {
+		counter++
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("count not iterate objects: %w", err)
+	}
+
+	data = make([]byte, 8)
+	binary.LittleEndian.PutUint64(data, counter)
+
+	err = b.Put(objectCounterKey, data)
+	if err != nil {
+		return fmt.Errorf("could not update object counter: %w", err)
+	}
+
+	return nil
 }
