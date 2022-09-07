fdea829141734cf0a0a5d58126ff3a3ed8db3c74
Evgenii Stratonikov <evgeniy@morphbits.ru>
2022-09-07 12:11:06 +0300

    [#1630] neofs-node: Remove trees on container removal event
    Signed-off-by: Evgenii Stratonikov <evgeniy@morphbits.ru>

--- cmd/neofs-node/tree.go
+++ cmd/neofs-node/tree.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"errors"
 
 	treeconfig "github.com/nspcc-dev/neofs-node/cmd/neofs-node/config/tree"
+	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/pilorama"
+	"github.com/nspcc-dev/neofs-node/pkg/morph/event"
+	containerEvent "github.com/nspcc-dev/neofs-node/pkg/morph/event/container"
 	"github.com/nspcc-dev/neofs-node/pkg/services/tree"
+	"go.uber.org/zap"
 )
 
 func initTreeService(c *cfg) {
@@ -31,6 +36,19 @@ func initTreeService(c *cfg) {
 	c.workers = append(c.workers, newWorkerFromFunc(func(ctx context.Context) {
 		c.treeService.Start(ctx)
 	}))
+
+	subscribeToContainerRemoval(c, func(e event.Event) {
+		ev := e.(containerEvent.DeleteSuccess)
+
+		// This is executed asynchronously, so we don't care about the operation taking some time.
+		err := c.treeService.DropTree(context.Background(), ev.ID, "")
+		if err != nil && !errors.Is(err, pilorama.ErrTreeNotFound) {
+			// Ignore pilorama.ErrTreeNotFound but other errors, including shard.ErrReadOnly, should be logged.
+			c.log.Error("container removal event received, but trees weren't removed",
+				zap.Stringer("cid", ev.ID),
+				zap.String("error", err.Error()))
+		}
+	})
 
 	c.onShutdown(c.treeService.Shutdown)
 }
