175f76504a57c9c0646e15fda0c1cf0633857fe0
Evgenii Stratonikov <evgeniy@morphbits.ru>
2022-09-28 11:10:27 +0300

    [#1819] common: Add `ErrNoSpace`
    Add a common error for this case because it is not an error
    which should increase error counter. Single error simplifies checks on
    the call-site.
    
    Signed-off-by: Evgenii Stratonikov <evgeniy@morphbits.ru>

--- pkg/local_object_storage/blobstor/common/errors.go
+++ pkg/local_object_storage/blobstor/common/errors.go
@@ -5,3 +5,6 @@ import "errors"
 // ErrReadOnly MUST be returned for modifying operations when the storage was opened
 // in readonly mode.
 var ErrReadOnly = errors.New("opened as read-only")
+
+// ErrNoSpace MUST be returned when there is no space to put an object on the device.
+var ErrNoSpace = errors.New("no free space")
