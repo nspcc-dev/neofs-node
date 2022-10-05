6d7ffefec528243afdbdcc4e53e956b4bac700ae
Evgenii Stratonikov <evgeniy@morphbits.ru>
2022-10-05 15:18:01 +0300

    [#1840] blobstor/test: Add tests for auxiliary functions
    Signed-off-by: Evgenii Stratonikov <evgeniy@morphbits.ru>

--- pkg/local_object_storage/blobstor/fstree/generic_test.go
+++ pkg/local_object_storage/blobstor/fstree/generic_test.go
@@ -13,16 +13,27 @@ import (
 func TestGeneric(t *testing.T) {
 	defer func() { _ = os.RemoveAll(t.Name()) }()
 
-	var n int
-	newTree := func(t *testing.T) common.Storage {
-		dir := filepath.Join(t.Name(), strconv.Itoa(n))
+	helper := func(t *testing.T, dir string) common.Storage {
 		return New(
 			WithPath(dir),
 			WithDepth(2),
 			WithDirNameLen(2))
 	}
 
+	var n int
+	newTree := func(t *testing.T) common.Storage {
+		dir := filepath.Join(t.Name(), strconv.Itoa(n))
+		return helper(t, dir)
+	}
+
 	blobstortest.TestAll(t, newTree, 2048, 16*1024)
+
+	t.Run("info", func(t *testing.T) {
+		dir := filepath.Join(t.Name(), "info")
+		blobstortest.TestInfo(t, func(t *testing.T) common.Storage {
+			return helper(t, dir)
+		}, Type, dir)
+	})
 }
 
 func TestControl(t *testing.T) {
