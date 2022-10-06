f4a3fa2977de8b929af1b9cdd7b245af2c4ad297
Pavel Karpy <carpawell@nspcc.ru>
2022-10-06 23:18:46 +0300

    [#1329] tree: Sync tree on startup
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- cmd/neofs-node/config.go
+++ cmd/neofs-node/config.go
@@ -42,6 +42,7 @@ import (
 	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/writecache"
 	"github.com/nspcc-dev/neofs-node/pkg/metrics"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/client"
+	containerClient "github.com/nspcc-dev/neofs-node/pkg/morph/client/container"
 	nmClient "github.com/nspcc-dev/neofs-node/pkg/morph/client/netmap"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/event"
 	netmap2 "github.com/nspcc-dev/neofs-node/pkg/morph/event/netmap"
@@ -305,6 +306,10 @@ type internals struct {
 	workers []worker
 	closers []func()
 
+	// onlineStateHandlers are executed in a separate
+	// goroutine on every !ONLINE -> ONLINE state transition
+	onlineStateHandlers []func(context.Context)
+
 	apiVersion   version.Version
 	healthStatus *atomic.Int32
 	// is node under maintenance
@@ -346,6 +351,8 @@ type shared struct {
 	// current network map
 	netMap       atomicstd.Value // type netmap.NetMap
 	netMapSource netmapCore.Source
+
+	cnrClient *containerClient.Client
 
 	respSvc *response.Service
 
@@ -831,6 +838,13 @@ func (c *cfg) LocalNodeInfo() (*netmapV2.NodeInfo, error) {
 // Called with nil when storage node is outside the NeoFS network map
 // (before entering the network and after leaving it).
 func (c *cfg) handleLocalNodeInfo(ni *netmap.NodeInfo) {
+	if c.cfgNetmap.state.controlNetmapStatus() != control.NetmapStatus_ONLINE &&
+		ni != nil && ni.IsOnline() {
+		for _, h := range c.onlineStateHandlers {
+			go h(c.ctx)
+		}
+	}
+
 	c.cfgNetmap.state.setNodeInfo(ni)
 }
 
@@ -934,4 +948,8 @@ func (c *cfg) configWatcher(ctx context.Context) {
 			return
 		}
 	}
+}
+
+func (c *cfg) addOnlineStateHandler(h func(ctx context.Context)) {
+	c.onlineStateHandlers = append(c.onlineStateHandlers, h)
 }
