fa231b8c56eb688a49f7342b6e25804bbb2768e6
Pavel Karpy <carpawell@nspcc.ru>
2022-11-15 15:33:48 +0300

    [#2057] blobstor: Block operations on a mode change
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/local_object_storage/blobstor/info.go
+++ pkg/local_object_storage/blobstor/info.go
@@ -2,6 +2,9 @@ package blobstor
 
 // DumpInfo returns information about blob stor.
 func (b *BlobStor) DumpInfo() Info {
+	b.modeMtx.RLock()
+	defer b.modeMtx.RUnlock()
+
 	sub := make([]SubStorageInfo, len(b.storage))
 	for i := range b.storage {
 		sub[i].Path = b.storage[i].Storage.Path()
