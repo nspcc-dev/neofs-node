53283be78a92bb22fede28d12908846fb0396d57
Pavel Karpy <carpawell@nspcc.ru>
2022-11-24 17:28:50 +0300

    [#2095] node: Do not allow `GETRANGE` requests with zero length
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/services/object/get/prm.go
+++ pkg/services/object/get/prm.go
@@ -1,6 +1,7 @@
 package getsvc
 
 import (
+	"errors"
 	"hash"
 
 	coreclient "github.com/nspcc-dev/neofs-node/pkg/core/client"
@@ -19,6 +20,30 @@ type RangePrm struct {
 	commonPrm
 
 	rng *object.Range
+}
+
+var (
+	errRangeZeroLength = errors.New("zero range length")
+	errRangeOverflow   = errors.New("range overflow")
+)
+
+// Validate pre-validates `OBJECTRANGE` request's parameters content
+// without access to the requested object's payload.
+func (p RangePrm) Validate() error {
+	if p.rng != nil {
+		off := p.rng.GetOffset()
+		l := p.rng.GetLength()
+
+		if l == 0 {
+			return errRangeZeroLength
+		}
+
+		if off+l <= off {
+			return errRangeOverflow
+		}
+	}
+
+	return nil
 }
 
 // RangeHashPrm groups parameters of GetRange service call.
