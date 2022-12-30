088465eb2843ad9735854c035bce7424b37ffdf8
Evgenii Stratonikov <e.stratonikov@yadro.com>
2022-12-30 13:31:59 +0300

    [#2190] services/object: Log service error with INFO level
    Signed-off-by: Evgenii Stratonikov <e.stratonikov@yadro.com>

--- pkg/services/object/util/log.go
+++ pkg/services/object/util/log.go
@@ -6,9 +6,9 @@ import (
 	"go.uber.org/zap"
 )
 
-// LogServiceError writes debug error message of object service to provided logger.
+// LogServiceError writes error message of object service to provided logger.
 func LogServiceError(l *logger.Logger, req string, node network.AddressGroup, err error) {
-	l.Debug("object service error",
+	l.Error("object service error",
 		zap.String("node", network.StringifyGroup(node)),
 		zap.String("request", req),
 		zap.String("error", err.Error()),
@@ -17,7 +17,7 @@ func LogServiceError(l *logger.Logger, req string, node network.AddressGroup, er
 
 // LogWorkerPoolError writes debug error message of object worker pool to provided logger.
 func LogWorkerPoolError(l *logger.Logger, req string, err error) {
-	l.Debug("could not push task to worker pool",
+	l.Error("could not push task to worker pool",
 		zap.String("request", req),
 		zap.String("error", err.Error()),
 	)
