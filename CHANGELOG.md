130ab451ed82fb8bed81be50a55213a8b809347e
Leonard Lyubich <leonard@morphbits.io>
2023-08-15 14:52:46 +0400

    writecache: Fix initialization in read-only mode
    Write-cache must not perform migration ops in read-only mode.
    
    Exec `initFlushMarks` and `runFlushLoop` in non-RO mode only. As a
    consequence, repairs potential NPE in `Init` method.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -89,6 +89,7 @@ Docker images now contain a single executable file and SSL certificates only.
 - Iterating over just removed files by FSTree (#98)
 - IR metrics moved to `neofs_ir` namespace and epoch metrics to `object` subsystem (#2344)
 - IR metrics config reading (#2344)
+- Write-cache initialization in read-only mode (#2504)
 
 ### Removed
 - Non-notary mode support for sidechain (#2321)
---
+++ pkg/local_object_storage/writecache/writecache_test.go
@@ -0,0 +1,47 @@
+package writecache_test
+
+import (
+	"testing"
+
+	objectcore "github.com/nspcc-dev/neofs-node/pkg/core/object"
+	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/blobstor/common"
+	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/writecache"
+	objecttest "github.com/nspcc-dev/neofs-sdk-go/object/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCache_InitReadOnly(t *testing.T) {
+	dir := t.TempDir()
+
+	wc := writecache.New(
+		writecache.WithPath(dir),
+	)
+
+	// open in rw mode first to create underlying BoltDB with some object
+	// (otherwise 'bad file descriptor' error on Open occurs)
+	err := wc.Open(false)
+	require.NoError(t, err)
+
+	err = wc.Init()
+	require.NoError(t, err)
+
+	obj := objecttest.Object(t)
+
+	_, err = wc.Put(common.PutPrm{
+		Address: objectcore.AddressOf(obj),
+		Object:  obj,
+	})
+	require.NoError(t, err)
+
+	err = wc.Close()
+	require.NoError(t, err)
+
+	// try Init in read-only mode
+	err = wc.Open(true)
+	require.NoError(t, err)
+
+	t.Cleanup(func() { wc.Close() })
+
+	err = wc.Init()
+	require.NoError(t, err)
+}
