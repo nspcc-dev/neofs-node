6dcaf22cfd1f5c2bfdac62bd27ca64f17ce52529
Leonard Lyubich <leonard@morphbits.io>
2023-08-16 12:51:21 +0400

    lens: Add commands to work with Peapod
    Recently introduced storage component named Peapod is a good candidate
    to be inspected by NeoFS Lens app.
    
    Add `peapod` command with `inspect` and `list` sub-commands similar to
    `blobovnicza` and `write-cache` commands.
    
    Closes #2507.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

---
+++ cmd/neofs-lens/internal/peapod/list.go
@@ -0,0 +1,37 @@
+package peapod
+
+import (
+	"fmt"
+	"io"
+
+	common "github.com/nspcc-dev/neofs-node/cmd/neofs-lens/internal"
+	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
+	"github.com/spf13/cobra"
+)
+
+var listCMD = &cobra.Command{
+	Use:   "list",
+	Short: "Object listing",
+	Long:  `List all objects stored in a Peapod.`,
+	Run:   listFunc,
+}
+
+func init() {
+	common.AddComponentPathFlag(listCMD, &vPath)
+}
+
+func listFunc(cmd *cobra.Command, _ []string) {
+	// other targets can be supported
+	w := cmd.OutOrStderr()
+
+	wAddr := func(addr oid.Address) error {
+		_, err := io.WriteString(w, fmt.Sprintf("%s\n", addr))
+		return err
+	}
+
+	ppd := openPeapod(cmd)
+	defer ppd.Close()
+
+	err := ppd.IterateAddresses(wAddr)
+	common.ExitOnErr(cmd, common.Errf("Peapod iterator failure: %w", err))
+}
