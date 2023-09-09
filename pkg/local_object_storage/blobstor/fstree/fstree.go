f8c48dcd5337c883d340cd1b7d743649c40a05b8
Roman Khimov <roman@nspcc.ru>
2023-09-09 09:37:46 +0300

    fstree: move retry logic into a function completely
    This makes it possible to override it. No functional changes.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- pkg/local_object_storage/blobstor/fstree/fstree.go
+++ pkg/local_object_storage/blobstor/fstree/fstree.go
@@ -247,7 +247,10 @@ func (t *FSTree) Put(prm common.PutPrm) (common.PutRes, error) {
 	if !prm.DontCompress {
 		prm.RawData = t.Compress(prm.RawData)
 	}
+	return t.writeData(p, prm.RawData)
+}
 
+func (t *FSTree) writeData(p string, data []byte) (common.PutRes, error) {
 	// Here is a situation:
 	// Feb 09 13:10:37 buky neofs-node[32445]: 2023-02-09T13:10:37.161Z        info        log/log.go:13        local object storage operation        {"shard_id": "SkT8BfjouW6t93oLuzQ79s", "address": "7NxFz4SruSi8TqXacr2Ae22nekMhgYk1sfkddJo9PpWk/5enyUJGCyU1sfrURDnHEjZFdbGqANVhayYGfdSqtA6wA", "op": "PUT", "type": "fstree", "storage_id": ""}
 	// Feb 09 13:10:37 buky neofs-node[32445]: 2023-02-09T13:10:37.183Z        info        log/log.go:13        local object storage operation        {"shard_id": "SkT8BfjouW6t93oLuzQ79s", "address": "7NxFz4SruSi8TqXacr2Ae22nekMhgYk1sfkddJo9PpWk/5enyUJGCyU1sfrURDnHEjZFdbGqANVhayYGfdSqtA6wA", "op": "metabase PUT"}
@@ -273,7 +276,7 @@ func (t *FSTree) Put(prm common.PutPrm) (common.PutRes, error) {
 	const retryCount = 5
 	for i := 0; i < retryCount; i++ {
 		tmpPath := p + "#" + strconv.FormatUint(uint64(i), 10)
-		err := t.writeAndRename(tmpPath, p, prm.RawData)
+		err := t.writeAndRename(tmpPath, p, data)
 		if err != syscall.EEXIST || i == retryCount-1 {
 			return common.PutRes{StorageID: []byte{}}, err
 		}
