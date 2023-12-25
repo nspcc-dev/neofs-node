nspcc-dev/neofs-node

commit d41d7a7d37ecec6a86bed9352e92d177a6f8f41b

adm: Allow generation more than one SN wallet per call
Add `wallets-number` flag that "duplicates" requests to the `neofs-adm`.
Passwords and labels are the same for all the wallets being created. Names
differ with the suffix-numbers. Closes #2425.

Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

cmd/neofs-adm/internal/modules/morph/root.go
@@ -1,6 +1,11 @@
 package morph
 
 import (
+	"fmt"
+
+	"github.com/nspcc-dev/neo-go/pkg/encoding/address"
+	"github.com/nspcc-dev/neo-go/pkg/util"
+	"github.com/nspcc-dev/neo-go/pkg/wallet"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -11,6 +16,7 @@ const (
 	endpointFlag                    = "rpc-endpoint"
 	storageWalletFlag               = "storage-wallet"
 	storageWalletLabelFlag          = "label"
+	storageWalletsNumber            = "wallets-number"
 	storageGasCLIFlag               = "initial-gas"
 	storageGasConfigFlag            = "storage.initial_gas"
 	contractsInitFlag               = "contracts"
@@ -106,7 +112,38 @@ var (
 			_ = viper.BindPFlag(refillGasAmountFlag, cmd.Flags().Lookup(refillGasAmountFlag))
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return refillGas(cmd, refillGasAmountFlag, false)
+			var gasReceiver util.Uint160
+			var err error
+
+			// wallet address is not part of the config
+			walletAddress, _ := cmd.Flags().GetString(walletAddressFlag)
+
+			if len(walletAddress) != 0 {
+				gasReceiver, err = address.StringToUint160(walletAddress)
+				if err != nil {
+					return fmt.Errorf("invalid wallet address %s: %w", walletAddress, err)
+				}
+			} else {
+				// storage wallet path is not part of the config
+				storageWalletPath, _ := cmd.Flags().GetString(storageWalletFlag)
+				if storageWalletPath == "" {
+					return fmt.Errorf("missing wallet path (use '--%s <out.json>')", storageWalletFlag)
+				}
+
+				w, err := wallet.NewWallet(storageWalletPath)
+				if err != nil {
+					return fmt.Errorf("can't open wallet: %w", err)
+				}
+
+				gasReceiver = w.Accounts[0].Contract.ScriptHash()
+			}
+
+			gasAmount, err := parseGASAmount(viper.GetString(refillGasAmountFlag))
+			if err != nil {
+				return err
+			}
+
+			return refillGas(cmd, int64(gasAmount), []util.Uint160{gasReceiver})
 		},
 	}
 
@@ -319,8 +356,9 @@ func init() {
 	RootCmd.AddCommand(generateStorageCmd)
 	generateStorageCmd.Flags().String(alphabetWalletsFlag, "", "Path to alphabet wallets dir")
 	generateStorageCmd.Flags().StringP(endpointFlag, "r", "", "N3 RPC node endpoint")
-	generateStorageCmd.Flags().String(storageWalletFlag, "", "Path to new storage node wallet")
+	generateStorageCmd.Flags().String(storageWalletFlag, "", "Path to new storage node wallet(s)")
 	generateStorageCmd.Flags().String(storageGasCLIFlag, "", "Initial amount of GAS to transfer")
+	generateStorageCmd.Flags().Uint32(storageWalletsNumber, 1, "Number of wallets to generate, if more than 1, suffix-number will be added to the filename")
 	generateStorageCmd.Flags().StringP(storageWalletLabelFlag, "l", "", "Wallet label")
 
 	RootCmd.AddCommand(forceNewEpoch)

cmd/neofs-adm/internal/modules/morph/generate.go
@@ -3,19 +3,18 @@ package morph
 import (
 	"errors"
 	"fmt"
+	"math/big"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 
 	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
-	"github.com/nspcc-dev/neo-go/pkg/encoding/address"
 	"github.com/nspcc-dev/neo-go/pkg/encoding/fixedn"
-	"github.com/nspcc-dev/neo-go/pkg/io"
 	"github.com/nspcc-dev/neo-go/pkg/rpcclient/gas"
+	"github.com/nspcc-dev/neo-go/pkg/rpcclient/nep17"
 	"github.com/nspcc-dev/neo-go/pkg/smartcontract"
-	"github.com/nspcc-dev/neo-go/pkg/smartcontract/callflag"
 	"github.com/nspcc-dev/neo-go/pkg/util"
-	"github.com/nspcc-dev/neo-go/pkg/vm/emit"
-	"github.com/nspcc-dev/neo-go/pkg/vm/opcode"
 	"github.com/nspcc-dev/neo-go/pkg/wallet"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-adm/internal/modules/config"
 	"github.com/nspcc-dev/neofs-node/pkg/util/glagolitsa"
@@ -128,81 +127,67 @@ func addMultisigAccount(w *wallet.Wallet, m int, name, password string, pubs key
 }
 
 func generateStorageCreds(cmd *cobra.Command, _ []string) error {
-	return refillGas(cmd, storageGasConfigFlag, true)
-}
-
-func refillGas(cmd *cobra.Command, gasFlag string, createWallet bool) (err error) {
 	// storage wallet path is not part of the config
 	storageWalletPath, _ := cmd.Flags().GetString(storageWalletFlag)
-	// wallet address is not part of the config
-	walletAddress, _ := cmd.Flags().GetString(walletAddressFlag)
-
-	var gasReceiver util.Uint160
-
-	if len(walletAddress) != 0 {
-		gasReceiver, err = address.StringToUint160(walletAddress)
-		if err != nil {
-			return fmt.Errorf("invalid wallet address %s: %w", walletAddress, err)
-		}
-	} else {
-		if storageWalletPath == "" {
-			return fmt.Errorf("missing wallet path (use '--%s <out.json>')", storageWalletFlag)
-		}
-
-		var w *wallet.Wallet
-
-		if createWallet {
-			w, err = wallet.NewWallet(storageWalletPath)
-		} else {
-			w, err = wallet.NewWalletFromFile(storageWalletPath)
-		}
-
-		if err != nil {
-			return fmt.Errorf("can't create wallet: %w", err)
-		}
-
-		if createWallet {
-			var password string
-
-			label, _ := cmd.Flags().GetString(storageWalletLabelFlag)
-			password, err := config.GetStoragePassword(viper.GetViper(), label)
-			if err != nil {
-				return fmt.Errorf("can't fetch password: %w", err)
-			}
-
-			if label == "" {
-				label = singleAccountName
-			}
-
-			if err := w.CreateAccount(label, password); err != nil {
-				return fmt.Errorf("can't create account: %w", err)
-			}
-		}
-
-		gasReceiver = w.Accounts[0].Contract.ScriptHash()
+	if storageWalletPath == "" {
+		return fmt.Errorf("missing wallet path (use '--%s <out.json>')", storageWalletFlag)
 	}
 
-	gasStr := viper.GetString(gasFlag)
+	walletsNumber, err := cmd.Flags().GetUint32(storageWalletsNumber)
+	if err != nil {
+		return err
+	}
+	if walletsNumber == 0 {
+		walletsNumber = 1
+	}
 
-	gasAmount, err := parseGASAmount(gasStr)
+	label, _ := cmd.Flags().GetString(storageWalletLabelFlag)
+	password, err := config.GetStoragePassword(viper.GetViper(), label)
+	if err != nil {
+		return fmt.Errorf("can't fetch password: %w", err)
+	}
+
+	if label == "" {
+		label = singleAccountName
+	}
+
+	hashes, err := createWallets(storageWalletPath, label, password, walletsNumber)
 	if err != nil {
 		return err
 	}
 
+	gasAmount, err := parseGASAmount(viper.GetString(refillGasAmountFlag))
+	if err != nil {
+		return err
+	}
+
+	return refillGas(cmd, int64(gasAmount), hashes)
+}
+
+func refillGas(cmd *cobra.Command, gasAmount int64, receivers []util.Uint160) (err error) {
 	wCtx, err := newInitializeContext(cmd, viper.GetViper())
 	if err != nil {
 		return err
 	}
 
-	bw := io.NewBufBinWriter()
-	emit.AppCall(bw.BinWriter, gas.Hash, "transfer", callflag.All,
-		wCtx.CommitteeAcc.Contract.ScriptHash(), gasReceiver, int64(gasAmount), nil)
-	emit.Opcodes(bw.BinWriter, opcode.ASSERT)
-	if bw.Err != nil {
-		return fmt.Errorf("BUG: invalid transfer arguments: %w", bw.Err)
+	committeeScriptHash := wCtx.CommitteeAcc.Contract.ScriptHash()
+
+	var pp []nep17.TransferParameters
+	for _, receiver := range receivers {
+		pp = append(pp, nep17.TransferParameters{
+			From:   committeeScriptHash,
+			To:     receiver,
+			Amount: big.NewInt(gasAmount),
+		})
 	}
 
-	if err := wCtx.sendCommitteeTx(bw.Bytes(), false); err != nil {
+	gToken := nep17.New(wCtx.CommitteeAct, gas.Hash)
+	tx, err := gToken.MultiTransferUnsigned(pp)
+	if err != nil {
+		return err
+	}
+
+	if err := wCtx.multiSignAndSend(tx, committeeAccountName); err != nil {
 		return err
 	}
 
@@ -218,4 +203,42 @@ func parseGASAmount(s string) (fixedn.Fixed8, error) {
 		return 0, fmt.Errorf("GAS amount must be positive (got %d)", gasAmount)
 	}
 	return gasAmount, nil
+}
+
+func createWallets(fileNameTemplate, label, password string, number uint32) ([]util.Uint160, error) {
+	var res []util.Uint160
+	ext := path.Ext(fileNameTemplate)
+	base := strings.TrimSuffix(fileNameTemplate, ext)
+	walletNumberFormat := fmt.Sprintf("%%0%dd", digitsNum(number))
+
+	for i := 0; i < int(number); i++ {
+		filename := fileNameTemplate
+		if number != 1 {
+			filename = base + "_" + fmt.Sprintf(walletNumberFormat, i) + ext
+		}
+
+		w, err := wallet.NewWallet(filename)
+		if err != nil {
+			return nil, fmt.Errorf("wallet creation: %w", err)
+		}
+
+		err = w.CreateAccount(label, password)
+		if err != nil {
+			return nil, fmt.Errorf("account creation: %w", err)
+		}
+
+		res = append(res, w.Accounts[0].Contract.ScriptHash())
+	}
+
+	return res, nil
+}
+
+func digitsNum(val uint32) int {
+	var res int
+	for val != 0 {
+		val /= 10
+		res++
+	}
+
+	return res
 }

cmd/neofs-adm/internal/modules/morph/generate_test.go
@@ -2,9 +2,11 @@ package morph
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"math/rand"
 	"os"
+	"path"
 	"path/filepath"
 	"strconv"
 	"testing"
@@ -89,4 +91,35 @@ func setupTestTerminal(t *testing.T) *bytes.Buffer {
 	t.Cleanup(func() { input.Terminal = nil })
 
 	return in
+}
+
+func TestCreateWallets(t *testing.T) {
+	dir := t.TempDir()
+
+	const label = "label"
+	const password = "pass"
+
+	hashes, err := createWallets(dir+"/test.json", label, password, 11)
+	require.NoError(t, err)
+
+	entries, err := os.ReadDir(dir)
+	require.NoError(t, err)
+
+	for i, entry := range entries {
+		if i < 10 {
+			require.Equal(t, fmt.Sprintf("test_0%d.json", i), entry.Name())
+		} else {
+			require.Equal(t, fmt.Sprintf("test_%d.json", i), entry.Name())
+		}
+	}
+
+	for i, entry := range entries {
+		w, err := wallet.NewWalletFromFile(path.Join(dir, entry.Name()))
+		require.NoError(t, err)
+
+		acc := w.GetAccount(hashes[i])
+		require.NoError(t, acc.Decrypt(password, keys.NEP2ScryptParams()))
+
+		require.Equal(t, label, acc.Label)
+	}
 }

commit f5018b284dbac695cc814b7bdf87be17b9f615b1

adm: Do not create but open wallet file
Neo-go naming is really confusing here. Regression from
d41d7a7d37ecec6a86bed9352e92d177a6f8f41b. Closes #2706.

Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

cmd/neofs-adm/internal/modules/morph/root.go
@@ -130,7 +130,7 @@ var (
 					return fmt.Errorf("missing wallet path (use '--%s <out.json>')", storageWalletFlag)
 				}
 
-				w, err := wallet.NewWallet(storageWalletPath)
+				w, err := wallet.NewWalletFromFile(storageWalletPath)
 				if err != nil {
 					return fmt.Errorf("can't open wallet: %w", err)
 				}