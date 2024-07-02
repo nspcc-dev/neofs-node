nspcc-dev/neofs-node

commit 8fd1b16964651c6d47d8001b9293f3d9b6126d8f

blobstor: move bbcz code to the migration tool
Closes #2614.

Signed-off-by: Roman Khimov <roman@nspcc.ru>

pkg/local_object_storage/metabase/status.go
@@ -1,7 +1,8 @@
 package meta
 
 import (
-	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/blobovnicza"
+	"errors"
+
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
 	"go.etcd.io/bbolt"
 )
@@ -27,9 +28,8 @@ func (db *DB) ObjectStatus(address oid.Address) (ObjectStatus, error) {
 	storageID.SetAddress(address)
 	resStorageID, err := db.StorageID(storageID)
 	if id := resStorageID.StorageID(); id != nil {
-		res.StorageID = blobovnicza.NewIDFromBytes(id).String()
-	} else {
-		return res, nil
+		res.Error = errors.New("unexpected storage id")
+		return res, res.Error
 	}
 
 	err = db.boltDB.View(func(tx *bbolt.Tx) error {

cmd/blobovnicza-to-peapod/bbczconf/config.go
@@ -1,9 +1,9 @@
 package blobovniczaconfig
 
 import (
+	"github.com/nspcc-dev/neofs-node/cmd/blobovnicza-to-peapod/blobovniczatree"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-node/config"
 	boltdbconfig "github.com/nspcc-dev/neofs-node/cmd/neofs-node/config/engine/shard/boltdb"
-	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/blobstor/blobovniczatree"
 )
 
 // Config is a wrapper over the config section

commit caf4ee18014184c3df6263162f4015ccd352cf7f

meta/ObjectStatus: do not stop if storage's ID is found
Seems like a regression from the 8fd1b16964651c6d47d8001b9293f3d9b6126d8f,
peapod has "peapod" ID.

Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

pkg/local_object_storage/metabase/status.go
@@ -1,7 +1,6 @@
 package meta
 
 import (
-	"errors"
 	"fmt"
 
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
@@ -34,8 +33,7 @@ func (db *DB) ObjectStatus(address oid.Address) (ObjectStatus, error) {
 	}
 
 	if id := resStorageID.StorageID(); id != nil {
-		res.Error = errors.New("unexpected storage id")
-		return res, res.Error
+		res.StorageID = string(id)
 	}
 
 	err = db.boltDB.View(func(tx *bbolt.Tx) error {