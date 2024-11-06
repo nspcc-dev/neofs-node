nspcc-dev/neofs-node

commit 6bf44289a668dd8cf850a58156428dfdc6b5884f

node/pprof,metrics: make them stop at the end of app life
Like 85b31b91735a2a78af0f615caa08a371127bdf1d but for shutdown order. In
practice, there was an SN app that could not stop, but it was impossible to
debug anything because no pprof or metrics were available. Closes #2976.

Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

cmd/neofs-node/main.go
@@ -97,7 +97,7 @@ func preRunAndLog(c *cfg, name string, srv *httputil.Server) {
 		})
 	}()
 
-	c.closers = append(c.closers, func() {
+	c.closers = append(c.veryLastClosers, func() {
 		c.log.Debug(fmt.Sprintf("shutting down %s service", name))
 
 		err := srv.Shutdown()
@@ -184,6 +184,9 @@ func wait(c *cfg) {
 func shutdown(c *cfg) {
 	for _, closer := range c.closers {
 		closer()
+	}
+	for _, lastCloser := range c.veryLastClosers {
+		lastCloser()
 	}
 
 	c.log.Debug("waiting for all processes to stop")

cmd/neofs-node/config.go
@@ -285,6 +285,9 @@ type internals struct {
 	wg      *sync.WaitGroup
 	workers []worker
 	closers []func()
+	// services that are useful for debug (e.g. when a regular closer does not
+	// close), must be close at the very end of application life cycle
+	veryLastClosers []func()
 
 	apiVersion   version.Version
 	healthStatus atomic.Int32

commit a8ff95e68edec0c8ff8ba74803f332b5dd487f9f

node: fix closing deadlock
A regression from 6bf44289a668dd8cf850a58156428dfdc6b5884f. Closes #2997.

Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

cmd/neofs-node/main.go
@@ -97,7 +97,7 @@ func preRunAndLog(c *cfg, name string, srv *httputil.Server) {
 		})
 	}()
 
-	c.closers = append(c.veryLastClosers, func() {
+	c.veryLastClosers = append(c.veryLastClosers, func() {
 		c.log.Debug(fmt.Sprintf("shutting down %s service", name))
 
 		err := srv.Shutdown()