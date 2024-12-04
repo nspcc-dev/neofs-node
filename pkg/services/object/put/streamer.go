nspcc-dev/neofs-node

commit 5076fe224e3cc7a02cbf09fb925bcaa6efbb4054

object/put: Refactor storage policy processing
Continues 2f2933817b45b776cca76d8d32e91c8c3d1139d1 for `ObjectService`'s
`Put` server handler. opens the stage for optimizations of storage policy
imposition. Previously, node selection was hidden behind placement traverser
interface. currently, processing a single request requires preselection of
container nodes in unsorted form and, in general w/ slicing, several sorts to
place the resulting objects. In the previous code structure, cache optimization
would be less seamless. Also, this refactor reveals the entire placement logic
by removing unnecessary abstraction.

Implementation is pretty massive, so unit tests for various scenarios are
added.

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

pkg/services/object/put/streamer.go
@@ -6,10 +6,8 @@ import (
 	"fmt"
 
 	"github.com/nspcc-dev/neofs-node/pkg/core/client"
-	"github.com/nspcc-dev/neofs-node/pkg/core/netmap"
 	"github.com/nspcc-dev/neofs-node/pkg/services/object/internal"
 	"github.com/nspcc-dev/neofs-node/pkg/services/object/util"
-	"github.com/nspcc-dev/neofs-node/pkg/services/object_manager/placement"
 	neofsecdsa "github.com/nspcc-dev/neofs-sdk-go/crypto/ecdsa"
 	"github.com/nspcc-dev/neofs-sdk-go/object"
 	"github.com/nspcc-dev/neofs-sdk-go/user"
@@ -27,6 +25,7 @@ type Streamer struct {
 	maxPayloadSz uint64 // network config
 
 	transport Transport
+	neoFSNet  NeoFSNetwork
 }
 
 var errNotInit = errors.New("stream not initialized")
@@ -148,12 +147,6 @@ func (p *Streamer) preparePrm(prm *PutInitPrm) error {
 		return fmt.Errorf("get local node's private key: %w", err)
 	}
 
-	// get latest network map
-	nm, err := netmap.GetLatestNetworkMap(p.netMapSrc)
-	if err != nil {
-		return fmt.Errorf("(%T) could not get latest network map: %w", p, err)
-	}
-
 	idCnr, ok := prm.hdr.ContainerID()
 	if !ok {
 		return errors.New("missing container ID")
@@ -167,48 +160,27 @@ func (p *Streamer) preparePrm(prm *PutInitPrm) error {
 
 	prm.cnr = cnrInfo.Value
 
-	// add common options
-	prm.traverseOpts = append(prm.traverseOpts,
-		// set processing container
-		placement.ForContainer(prm.cnr),
-	)
-
-	if id, ok := prm.hdr.ID(); ok {
-		prm.traverseOpts = append(prm.traverseOpts,
-			// set identifier of the processing object
-			placement.ForObject(id),
-		)
-	}
-
-	prm.traverseOpts = append(prm.traverseOpts, placement.WithCopiesNumber(prm.copiesNumber))
-
-	// create placement builder from network map
-	builder := placement.NewNetworkMapBuilder(nm)
-
-	if prm.common.LocalOnly() {
-		// restrict success count to 1 stored copy (to local storage)
-		prm.traverseOpts = append(prm.traverseOpts, placement.SuccessAfter(1))
-
-		// use local-only placement builder
-		builder = util.NewLocalPlacement(builder, p.netmapKeys)
-	}
-
-	nodeSets, err := builder.BuildPlacement(idCnr, nil, cnrInfo.Value.PlacementPolicy())
+	prm.containerNodes, err = p.neoFSNet.GetContainerNodes(idCnr)
 	if err != nil {
-		return fmt.Errorf("apply container's storage policy to current network map: %w", err)
+		return fmt.Errorf("select storage nodes for the container: %w", err)
 	}
+	cnrNodes := prm.containerNodes.Unsorted()
 nextSet:
-	for i := range nodeSets {
-		for j := range nodeSets[i] {
-			prm.localNodeInContainer = p.netmapKeys.IsLocalKey(nodeSets[i][j].PublicKey())
+	for i := range cnrNodes {
+		for j := range cnrNodes[i] {
+			prm.localNodeInContainer = p.neoFSNet.IsLocalNodePublicKey(cnrNodes[i][j].PublicKey())
 			if prm.localNodeInContainer {
+				if prm.copiesNumber > 0 {
+					return errors.New("storage of multiple object replicas is requested for a local operation")
+				}
+				prm.localNodePos[0], prm.localNodePos[1] = i, j
 				break nextSet
 			}
 		}
 	}
-
-	// set placement builder
-	prm.traverseOpts = append(prm.traverseOpts, placement.UseBuilder(builder))
+	if !prm.localNodeInContainer && prm.common.LocalOnly() {
+		return errors.New("local operation on the node not compliant with the container storage policy")
+	}
 
 	prm.localNodeSigner = (*neofsecdsa.Signer)(localNodeKey)
 
@@ -219,16 +191,12 @@ func (p *Streamer) newCommonTarget(prm *PutInitPrm) internal.Target {
 	var relay func(nodeDesc) error
 	if p.relay != nil {
 		relay = func(node nodeDesc) error {
-			var info client.NodeInfo
-
-			client.NodeInfoFromNetmapElement(&info, node.info)
-
-			c, err := p.clientConstructor.Get(info)
+			c, err := p.clientConstructor.Get(node.info)
 			if err != nil {
-				return fmt.Errorf("could not create SDK client %s: %w", info.AddressGroup(), err)
+				return fmt.Errorf("could not create SDK client %s: %w", node.info.AddressGroup(), err)
 			}
 
-			return p.relay(info, c)
+			return p.relay(node.info, c)
 		}
 	}
 
@@ -239,13 +207,17 @@ func (p *Streamer) newCommonTarget(prm *PutInitPrm) internal.Target {
 	withBroadcast := !localOnly && (typ == object.TypeTombstone || typ == object.TypeLock)
 
 	return &distributedTarget{
-		traversalState: traversal{
-			opts: prm.traverseOpts,
-
-			extraBroadcastEnabled: withBroadcast,
+		placementIterator: placementIterator{
+			log:            p.log,
+			neoFSNet:       p.neoFSNet,
+			localPool:      p.localPool,
+			remotePool:     p.remotePool,
+			containerNodes: prm.containerNodes,
+			localOnly:      localOnly,
+			localNodePos:   prm.localNodePos,
+			linearReplNum:  uint(prm.copiesNumber),
+			broadcast:      withBroadcast,
 		},
-		remotePool: p.remotePool,
-		localPool:  p.localPool,
 		nodeTargetInitializer: func(node nodeDesc) preparedObjectTarget {
 			if node.local {
 				return &localTarget{
@@ -257,21 +229,15 @@ func (p *Streamer) newCommonTarget(prm *PutInitPrm) internal.Target {
 				ctx:               p.ctx,
 				keyStorage:        p.keyStorage,
 				commonPrm:         prm.common,
+				nodeInfo:          node.info,
 				clientConstructor: p.clientConstructor,
 				transport:         p.transport,
 			}
 
-			client.NodeInfoFromNetmapElement(&rt.nodeInfo, node.info)
-
 			return rt
 		},
-		relay: relay,
-		fmt:   p.fmtValidator,
-		log:   p.log,
-
-		isLocalKey: p.netmapKeys.IsLocalKey,
-
-		localOnly:            localOnly,
+		relay:                relay,
+		fmt:                  p.fmtValidator,
 		localNodeInContainer: prm.localNodeInContainer,
 		localNodeSigner:      prm.localNodeSigner,
 	}

cmd/neofs-node/object.go
@@ -256,14 +256,13 @@ func initObjectService(c *cfg) {
 		searchsvcV2.WithKeyStorage(keyStorage),
 	)
 
-	sPut := putsvc.NewService(&transport{clients: putConstructor},
+	sPut := putsvc.NewService(&transport{clients: putConstructor}, c,
 		putsvc.WithKeyStorage(keyStorage),
 		putsvc.WithClientConstructor(putConstructor),
 		putsvc.WithMaxSizeSource(newCachedMaxObjectSizeSource(c)),
 		putsvc.WithObjectStorage(storageEngine{engine: ls}),
 		putsvc.WithContainerSource(c.cfgObject.cnrSource),
 		putsvc.WithNetworkMapSource(c.netMapSource),
-		putsvc.WithNetmapKeys(c),
 		putsvc.WithNetworkState(c.cfgNetmap.state),
 		putsvc.WithWorkerPools(c.cfgObject.pool.putRemote, c.cfgObject.pool.putLocal),
 		putsvc.WithLogger(c.log),
@@ -757,4 +756,53 @@ func (n netmapSourceWithNodes) ServerInContainer(cID cid.ID) (bool, error) {
 	}
 
 	return serverInContainer, nil
+}
+
+// GetContainerNodes reads storage policy of the referenced container from the
+// underlying container storage, reads current network map from the underlying
+// storage, applies the storage policy to it, gathers storage nodes matching the
+// policy and returns sort interface.
+//
+// GetContainerNodes implements [putsvc.NeoFSNetwork].
+func (c *cfg) GetContainerNodes(cnrID cid.ID) (putsvc.ContainerNodes, error) {
+	cnr, err := c.cfgObject.containerNodes.containers.Get(cnrID)
+	if err != nil {
+		return nil, fmt.Errorf("read container by ID: %w", err)
+	}
+	curEpoch, err := c.cfgObject.containerNodes.network.Epoch()
+	if err != nil {
+		return nil, fmt.Errorf("read current NeoFS epoch: %w", err)
+	}
+	networkMap, err := c.cfgObject.containerNodes.network.GetNetMapByEpoch(curEpoch)
+	if err != nil {
+		return nil, fmt.Errorf("read network map at the current epoch #%d: %w", curEpoch, err)
+	}
+	policy := cnr.Value.PlacementPolicy()
+	nodeSets, err := networkMap.ContainerNodes(cnr.Value.PlacementPolicy(), cnrID)
+	if err != nil {
+		return nil, fmt.Errorf("apply container storage policy to the network map at current epoch #%d: %w", curEpoch, err)
+	}
+	repCounts := make([]uint, policy.NumberOfReplicas())
+	for i := range repCounts {
+		repCounts[i] = uint(policy.ReplicaNumberByIndex(i))
+	}
+	return containerNodesSorter{
+		policy: storagePolicyRes{
+			nodeSets:  nodeSets,
+			repCounts: repCounts,
+		},
+		networkMap: networkMap,
+	}, nil
+}
+
+// implements [putsvc.ContainerNodes].
+type containerNodesSorter struct {
+	policy     storagePolicyRes
+	networkMap *netmapsdk.NetMap
+}
+
+func (x containerNodesSorter) Unsorted() [][]netmapsdk.NodeInfo { return x.policy.nodeSets }
+func (x containerNodesSorter) PrimaryCounts() []uint            { return x.policy.repCounts }
+func (x containerNodesSorter) SortForObject(obj oid.ID) ([][]netmapsdk.NodeInfo, error) {
+	return x.networkMap.PlacementVectors(x.policy.nodeSets, obj)
 }

pkg/services/object/put/distibuted_test.go
@@ -1,0 +1,494 @@
+package putsvc
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"strconv"
+	"sync"
+	"testing"
+
+	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
+	"github.com/nspcc-dev/neofs-sdk-go/netmap"
+	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
+	oidtest "github.com/nspcc-dev/neofs-sdk-go/object/id/test"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+func allocNodes(n []uint) [][]netmap.NodeInfo {
+	res := make([][]netmap.NodeInfo, len(n))
+	for i := range res {
+		res[i] = make([]netmap.NodeInfo, n[i])
+		for j := range res[i] {
+			res[i][j].SetPublicKey([]byte(fmt.Sprintf("pub_%d_%d", i, j)))
+			res[i][j].SetNetworkEndpoints(
+				"localhost:"+strconv.Itoa(1e4+i*100+2*j),
+				"localhost:"+strconv.Itoa(1e4+i*100+2*j+1),
+			)
+			res[i][j].SetExternalAddresses(
+				"external:"+strconv.Itoa(1e4+i*100+2*j),
+				"external:"+strconv.Itoa(1e4+i*100+2*j+1),
+			)
+		}
+	}
+	return res
+}
+
+type testContainerNodes struct {
+	objID oid.ID
+
+	sortErr  error
+	cnrNodes [][]netmap.NodeInfo
+
+	primCounts []uint
+}
+
+func (x testContainerNodes) Unsorted() [][]netmap.NodeInfo { return x.cnrNodes }
+
+func (x testContainerNodes) SortForObject(obj oid.ID) ([][]netmap.NodeInfo, error) {
+	if x.objID != obj {
+		return nil, errors.New("[test] unexpected object ID")
+	}
+	return x.cnrNodes, x.sortErr
+}
+
+func (x testContainerNodes) PrimaryCounts() []uint { return x.primCounts }
+
+type testNetwork struct {
+	localPubKey []byte
+}
+
+func (x testNetwork) IsLocalNodePublicKey(pk []byte) bool { return bytes.Equal(x.localPubKey, pk) }
+
+func (x testNetwork) GetContainerNodes(cid.ID) (ContainerNodes, error) { panic("unimplemented") }
+
+type testWorkerPool struct {
+	nCalls int
+	nFail  int
+	err    error
+}
+
+func (x *testWorkerPool) Release() {}
+func (x *testWorkerPool) Submit(f func()) error {
+	x.nCalls++
+	if x.err != nil && (x.nFail == 0 || x.nCalls == x.nFail) {
+		return x.err
+	}
+	go f()
+	return nil
+}
+
+func TestIterateNodesForObject(t *testing.T) {
+	// nodes: [A B C] [D C E F G] [B H I J]
+	// policy: [2 3 2]
+	// C is local. B, E and H fail, all others succeed
+	//
+	// expected order of candidates: [A B C] [D E F] [H I J]
+	objID := oidtest.ID()
+	cnrNodes := allocNodes([]uint{3, 5, 4})
+	cnrNodes[2][0].SetPublicKey(cnrNodes[0][1].PublicKey())
+	cnrNodes[1][1].SetPublicKey(cnrNodes[0][2].PublicKey())
+	var lwp, rwp testWorkerPool
+	iter := placementIterator{
+		log: zap.NewNop(),
+		neoFSNet: testNetwork{
+			localPubKey: cnrNodes[0][2].PublicKey(),
+		},
+		remotePool: &rwp,
+		localPool:  &lwp,
+		containerNodes: testContainerNodes{
+			objID:      objID,
+			cnrNodes:   cnrNodes,
+			primCounts: []uint{2, 3, 2},
+		},
+	}
+	var handlerMtx sync.Mutex
+	var handlerCalls []nodeDesc
+	err := iter.iterateNodesForObject(objID, func(node nodeDesc) error {
+		handlerMtx.Lock()
+		handlerCalls = append(handlerCalls, node)
+		handlerMtx.Unlock()
+		if bytes.Equal(node.info.PublicKey(), cnrNodes[0][1].PublicKey()) ||
+			bytes.Equal(node.info.PublicKey(), cnrNodes[1][2].PublicKey()) ||
+			bytes.Equal(node.info.PublicKey(), cnrNodes[2][1].PublicKey()) {
+			return errors.New("any node error")
+		}
+		return nil
+	})
+	require.NoError(t, err)
+	require.Len(t, handlerCalls, 9)
+	withLocal := false
+	for _, node := range handlerCalls[:3] {
+		if !withLocal {
+			withLocal = node.local
+		}
+		if node.local {
+			continue
+		}
+		require.Contains(t, [][]byte{
+			cnrNodes[0][0].PublicKey(), cnrNodes[0][1].PublicKey(),
+		}, node.info.PublicKey())
+		require.Len(t, node.info.AddressGroup(), 2)
+		var expNetAddrs, expNetAddrsExt []string
+		if bytes.Equal(node.info.PublicKey(), cnrNodes[0][0].PublicKey()) {
+			expNetAddrs = []string{"localhost:10000", "localhost:10001"}
+			expNetAddrsExt = []string{"external:10000", "external:10001"}
+		} else {
+			expNetAddrs = []string{"localhost:10002", "localhost:10003"}
+			expNetAddrsExt = []string{"external:10002", "external:10003"}
+		}
+		require.ElementsMatch(t, expNetAddrs, []string{node.info.AddressGroup()[0].URIAddr(), node.info.AddressGroup()[1].URIAddr()})
+		require.ElementsMatch(t, expNetAddrsExt, []string{node.info.ExternalAddressGroup()[0].URIAddr(), node.info.ExternalAddressGroup()[1].URIAddr()})
+	}
+	require.True(t, withLocal)
+	for _, node := range handlerCalls[3:6] {
+		require.False(t, node.local)
+		require.Contains(t, [][]byte{
+			cnrNodes[1][0].PublicKey(), cnrNodes[1][2].PublicKey(), cnrNodes[1][3].PublicKey(),
+		}, node.info.PublicKey())
+		require.Len(t, node.info.AddressGroup(), 2)
+		var expNetAddrs, expNetAddrsExt []string
+		switch key := node.info.PublicKey(); {
+		case bytes.Equal(key, cnrNodes[1][0].PublicKey()):
+			expNetAddrs = []string{"localhost:10100", "localhost:10101"}
+			expNetAddrsExt = []string{"external:10100", "external:10101"}
+		case bytes.Equal(key, cnrNodes[1][2].PublicKey()):
+			expNetAddrs = []string{"localhost:10104", "localhost:10105"}
+			expNetAddrsExt = []string{"external:10104", "external:10105"}
+		case bytes.Equal(key, cnrNodes[1][3].PublicKey()):
+			expNetAddrs = []string{"localhost:10106", "localhost:10107"}
+			expNetAddrsExt = []string{"external:10106", "external:10107"}
+		}
+		require.ElementsMatch(t, expNetAddrs, []string{node.info.AddressGroup()[0].URIAddr(), node.info.AddressGroup()[1].URIAddr()})
+		require.ElementsMatch(t, expNetAddrsExt, []string{node.info.ExternalAddressGroup()[0].URIAddr(), node.info.ExternalAddressGroup()[1].URIAddr()})
+	}
+	for _, node := range handlerCalls[6:] {
+		require.False(t, node.local)
+		require.Contains(t, [][]byte{
+			cnrNodes[2][1].PublicKey(), cnrNodes[2][2].PublicKey(), cnrNodes[2][3].PublicKey(),
+		}, node.info.PublicKey())
+		require.Len(t, node.info.AddressGroup(), 2)
+		var expNetAddrs, expNetAddrsExt []string
+		switch key := node.info.PublicKey(); {
+		case bytes.Equal(key, cnrNodes[2][1].PublicKey()):
+			expNetAddrs = []string{"localhost:10202", "localhost:10203"}
+			expNetAddrsExt = []string{"external:10202", "external:10203"}
+		case bytes.Equal(key, cnrNodes[2][2].PublicKey()):
+			expNetAddrs = []string{"localhost:10204", "localhost:10205"}
+			expNetAddrsExt = []string{"external:10204", "external:10205"}
+		case bytes.Equal(key, cnrNodes[2][3].PublicKey()):
+			expNetAddrs = []string{"localhost:10206", "localhost:10207"}
+			expNetAddrsExt = []string{"external:10206", "external:10207"}
+		}
+		require.ElementsMatch(t, expNetAddrs, []string{node.info.AddressGroup()[0].URIAddr(), node.info.AddressGroup()[1].URIAddr()})
+		require.ElementsMatch(t, expNetAddrsExt, []string{node.info.ExternalAddressGroup()[0].URIAddr(), node.info.ExternalAddressGroup()[1].URIAddr()})
+	}
+
+	t.Run("local only", func(t *testing.T) {
+		objID := oidtest.ID()
+		cnrNodes := allocNodes([]uint{2, 3, 1})
+		var lwp, rwp testWorkerPool
+		iter := placementIterator{
+			log: zap.NewNop(),
+			neoFSNet: testNetwork{
+				localPubKey: cnrNodes[1][1].PublicKey(),
+			},
+			remotePool: &rwp,
+			localPool:  &lwp,
+			containerNodes: testContainerNodes{
+				cnrNodes: cnrNodes,
+			},
+			localOnly:    true,
+			localNodePos: [2]int{1, 1},
+			broadcast:    true,
+		}
+		var handlerMtx sync.Mutex
+		var handlerCalls []nodeDesc
+		err := iter.iterateNodesForObject(objID, func(node nodeDesc) error {
+			handlerMtx.Lock()
+			handlerCalls = append(handlerCalls, node)
+			handlerMtx.Unlock()
+			return nil
+		})
+		require.NoError(t, err)
+		require.Len(t, handlerCalls, 1)
+		require.True(t, handlerCalls[0].local)
+		require.EqualValues(t, 1, lwp.nCalls)
+		require.Zero(t, rwp.nCalls)
+	})
+	t.Run("linear num of replicas", func(t *testing.T) {
+		// nodes: [A B C] [D B E] [F G]
+		// policy: [2 1 2]
+		// B fails, all others succeed
+		// linear num: 4
+		//
+		// expected order of candidates: [A B C D E]
+		objID := oidtest.ID()
+		cnrNodes := allocNodes([]uint{3, 3, 2})
+		cnrNodes[1][1].SetPublicKey(cnrNodes[0][1].PublicKey())
+		iter := placementIterator{
+			log:        zap.NewNop(),
+			neoFSNet:   new(testNetwork),
+			remotePool: new(testWorkerPool),
+			localPool:  new(testWorkerPool),
+			containerNodes: testContainerNodes{
+				objID:      objID,
+				cnrNodes:   cnrNodes,
+				primCounts: []uint{2, 1, 2},
+			},
+			linearReplNum: 4,
+		}
+		var handlerMtx sync.Mutex
+		var handlerCalls [][]byte
+		err := iter.iterateNodesForObject(objID, func(node nodeDesc) error {
+			handlerMtx.Lock()
+			handlerCalls = append(handlerCalls, node.info.PublicKey())
+			handlerMtx.Unlock()
+			if bytes.Equal(node.info.PublicKey(), cnrNodes[0][1].PublicKey()) {
+				return errors.New("any node error")
+			}
+			return nil
+		})
+		require.NoError(t, err)
+		require.Len(t, handlerCalls, 5)
+		require.ElementsMatch(t, [][]byte{
+			cnrNodes[0][0].PublicKey(),
+			cnrNodes[0][1].PublicKey(),
+			cnrNodes[0][2].PublicKey(),
+			cnrNodes[1][0].PublicKey(),
+			cnrNodes[1][2].PublicKey(),
+		}, handlerCalls)
+	})
+	t.Run("broadcast", func(t *testing.T) {
+		// nodes: [A B] [C D B] [E F]
+		// policy: [1 1 1]
+		// D fails, all others succeed
+		//
+		// expected order candidates: [A C E B D F]
+		objID := oidtest.ID()
+		cnrNodes := allocNodes([]uint{2, 3, 2})
+		cnrNodes[1][2].SetPublicKey(cnrNodes[0][1].PublicKey())
+		iter := placementIterator{
+			log:        zap.NewNop(),
+			neoFSNet:   new(testNetwork),
+			remotePool: new(testWorkerPool),
+			localPool:  new(testWorkerPool),
+			containerNodes: testContainerNodes{
+				objID:      objID,
+				cnrNodes:   cnrNodes,
+				primCounts: []uint{1, 1, 1},
+			},
+			broadcast: true,
+		}
+		var handlerMtx sync.Mutex
+		var handlerCalls [][]byte
+		err := iter.iterateNodesForObject(objID, func(node nodeDesc) error {
+			handlerMtx.Lock()
+			handlerCalls = append(handlerCalls, node.info.PublicKey())
+			handlerMtx.Unlock()
+			if bytes.Equal(node.info.PublicKey(), cnrNodes[1][1].PublicKey()) {
+				return errors.New("any node error")
+			}
+			return nil
+		})
+		require.NoError(t, err)
+		require.Len(t, handlerCalls, 6)
+		require.ElementsMatch(t, [][]byte{
+			cnrNodes[0][0].PublicKey(),
+			cnrNodes[1][0].PublicKey(),
+			cnrNodes[2][0].PublicKey(),
+		}, handlerCalls[:3])
+		for _, key := range handlerCalls[3:] {
+			require.Contains(t, [][]byte{
+				cnrNodes[0][1].PublicKey(),
+				cnrNodes[1][1].PublicKey(),
+				cnrNodes[2][1].PublicKey(),
+			}, key)
+		}
+	})
+	t.Run("sort nodes for object failure", func(t *testing.T) {
+		objID := oidtest.ID()
+		iter := placementIterator{
+			log: zap.NewNop(),
+			containerNodes: testContainerNodes{
+				objID:   objID,
+				sortErr: errors.New("any sort error"),
+			},
+		}
+		err := iter.iterateNodesForObject(objID, func(nodeDesc) error {
+			t.Fatal("must not be called")
+			return nil
+		})
+		require.EqualError(t, err, "sort container nodes for the object: any sort error")
+	})
+	t.Run("worker pool failure", func(t *testing.T) {
+		// nodes: [A B] [C D E] [F]
+		// policy: [2 2 1]
+		// C fails network op, worker pool fails for E
+		objID := oidtest.ID()
+		replCounts := []uint{2, 3, 1}
+		cnrNodes := allocNodes(replCounts)
+		wp := testWorkerPool{
+			nFail: 5,
+			err:   errors.New("any worker pool error"),
+		}
+		iter := placementIterator{
+			log:        zap.NewNop(),
+			neoFSNet:   new(testNetwork),
+			localPool:  &wp,
+			remotePool: &wp,
+			containerNodes: testContainerNodes{
+				objID:      objID,
+				cnrNodes:   cnrNodes,
+				primCounts: []uint{2, 2, 1},
+			},
+		}
+		var handlerMtx sync.Mutex
+		var handlerCalls [][]byte
+		err := iter.iterateNodesForObject(objID, func(node nodeDesc) error {
+			handlerMtx.Lock()
+			handlerCalls = append(handlerCalls, node.info.PublicKey())
+			handlerMtx.Unlock()
+			if bytes.Equal(node.info.PublicKey(), cnrNodes[1][0].PublicKey()) {
+				return errors.New("any node error")
+			}
+			return nil
+		})
+		require.Len(t, handlerCalls, 4)
+		require.ElementsMatch(t, handlerCalls[:2], [][]byte{
+			cnrNodes[0][0].PublicKey(), cnrNodes[0][1].PublicKey(),
+		})
+		require.ElementsMatch(t, handlerCalls[2:], [][]byte{
+			cnrNodes[1][0].PublicKey(), cnrNodes[1][1].PublicKey(),
+		})
+		require.EqualError(t, err, "incomplete object PUT by placement: "+
+			"submit next job to save an object to the worker pool: any worker pool error "+
+			"(last node error: any node error)")
+	})
+	t.Run("not enough nodes a priori", func(t *testing.T) {
+		// nodes: [A B] [C D E] [F]
+		// policy: [2 4 1]
+		//
+		// after 1st list is finished, 2nd list has not enough nodes, so service should
+		// not try to access them
+		//
+		// this test is a bit synthetic. Actually, selection of container nodes must
+		// fail if there is not enough nodes. But for complete coverage, detection of
+		// such cases is also tested
+		objID := oidtest.ID()
+		cnrNodes := allocNodes([]uint{2, 3, 1})
+		var wp testWorkerPool
+		iter := placementIterator{
+			log:        zap.NewNop(),
+			neoFSNet:   new(testNetwork),
+			remotePool: &wp,
+			containerNodes: testContainerNodes{
+				objID:      objID,
+				cnrNodes:   cnrNodes,
+				primCounts: []uint{2, 4, 1},
+			},
+		}
+		var handlerMtx sync.Mutex
+		var handlerCalls [][]byte
+		err := iter.iterateNodesForObject(objID, func(node nodeDesc) error {
+			handlerMtx.Lock()
+			handlerCalls = append(handlerCalls, node.info.PublicKey())
+			handlerMtx.Unlock()
+			if bytes.Equal(node.info.PublicKey(), cnrNodes[1][0].PublicKey()) {
+				return errors.New("any node error")
+			}
+			return nil
+		})
+		require.EqualError(t, err, "incomplete object PUT by placement: "+
+			"number of replicas cannot be met for list #1: 4 required, 3 nodes remaining")
+		// assert that we processed 1st list and did not even try to access
+		// nodes from the 2nd one
+		require.Len(t, handlerCalls, 2)
+		require.ElementsMatch(t, handlerCalls, [][]byte{
+			cnrNodes[0][0].PublicKey(), cnrNodes[0][1].PublicKey(),
+		})
+	})
+	t.Run("not enough nodes due to decoding network endpoints failure", func(t *testing.T) {
+		// nodes: [A B] [C D E] [F]
+		// policy: [2 2 1]
+		// node C fails network op, E has incorrect network endpoints. Service should try it but fail
+		objID := oidtest.ID()
+		cnrNodes := allocNodes([]uint{2, 3, 1})
+		cnrNodes[1][2].SetNetworkEndpoints("definitely invalid network address")
+		var wp testWorkerPool
+		iter := placementIterator{
+			log:        zap.NewNop(),
+			neoFSNet:   new(testNetwork),
+			remotePool: &wp,
+			containerNodes: testContainerNodes{
+				objID:      objID,
+				cnrNodes:   cnrNodes,
+				primCounts: []uint{2, 2, 1},
+			},
+		}
+		var handlerMtx sync.Mutex
+		var handlerCalls [][]byte
+		err := iter.iterateNodesForObject(objID, func(node nodeDesc) error {
+			handlerMtx.Lock()
+			handlerCalls = append(handlerCalls, node.info.PublicKey())
+			handlerMtx.Unlock()
+			if bytes.Equal(node.info.PublicKey(), cnrNodes[1][0].PublicKey()) {
+				return errors.New("any node error")
+			}
+			return nil
+		})
+		require.ErrorContains(t, err, "incomplete object PUT by placement: "+
+			"number of replicas cannot be met for list #1: 1 required, 0 nodes remaining "+
+			"(last node error: failed to decode network addresses:")
+		// assert that we processed 1st list and did not even try to access
+		// nodes from the 2nd one
+		require.Len(t, handlerCalls, 4)
+		require.ElementsMatch(t, handlerCalls[:2], [][]byte{
+			cnrNodes[0][0].PublicKey(), cnrNodes[0][1].PublicKey(),
+		})
+		require.ElementsMatch(t, handlerCalls[2:], [][]byte{
+			cnrNodes[1][0].PublicKey(), cnrNodes[1][1].PublicKey(),
+		})
+	})
+	t.Run("not enough nodes left after RPC failures", func(t *testing.T) {
+		// nodes: [A B] [C D E] [F]
+		// policy: [2 3 1]
+		// C and D fail network op
+		objID := oidtest.ID()
+		cnrNodes := allocNodes([]uint{2, 3, 1})
+		var wp testWorkerPool
+		iter := placementIterator{
+			log:        zap.NewNop(),
+			neoFSNet:   new(testNetwork),
+			remotePool: &wp,
+			containerNodes: testContainerNodes{
+				objID:      objID,
+				cnrNodes:   cnrNodes,
+				primCounts: []uint{2, 2, 1},
+			},
+		}
+		var handlerMtx sync.Mutex
+		var handlerCalls [][]byte
+		err := iter.iterateNodesForObject(objID, func(node nodeDesc) error {
+			handlerMtx.Lock()
+			handlerCalls = append(handlerCalls, node.info.PublicKey())
+			handlerMtx.Unlock()
+			if bytes.Equal(node.info.PublicKey(), cnrNodes[1][0].PublicKey()) ||
+				bytes.Equal(node.info.PublicKey(), cnrNodes[1][1].PublicKey()) {
+				return errors.New("any node error")
+			}
+			return nil
+		})
+		require.EqualError(t, err, "incomplete object PUT by placement: "+
+			"number of replicas cannot be met for list #1: 2 required, 1 nodes remaining "+
+			"(last node error: any node error)")
+		require.Len(t, handlerCalls, 4)
+		require.ElementsMatch(t, handlerCalls[:2], [][]byte{
+			cnrNodes[0][0].PublicKey(), cnrNodes[0][1].PublicKey(),
+		})
+		require.ElementsMatch(t, handlerCalls[2:], [][]byte{
+			cnrNodes[1][0].PublicKey(), cnrNodes[1][1].PublicKey(),
+		})
+	})
+}

commit 5a972a435ff89d6ecf9916ca16bbb35de02288ef

node/object: Fix Put op condition checking requests with copies number
Regression from 5076fe224e3cc7a02cbf09fb925bcaa6efbb4054.

Previously, the operation always failed on in-container nodes for
requests with specified number of copies. Actually, it must fail
requests with TTL=1 regardless of container composition.

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

pkg/services/object/put/streamer.go
@@ -140,6 +140,10 @@ func (p *Streamer) initTarget(prm *PutInitPrm) error {
 }
 
 func (p *Streamer) preparePrm(prm *PutInitPrm) error {
+	localOnly := prm.common.LocalOnly()
+	if localOnly && prm.copiesNumber > 0 {
+		return errors.New("storage of multiple object replicas is requested for a local operation")
+	}
 	var err error
 
 	localNodeKey, err := p.keyStorage.GetKey(nil)
@@ -170,15 +174,12 @@ nextSet:
 		for j := range cnrNodes[i] {
 			prm.localNodeInContainer = p.neoFSNet.IsLocalNodePublicKey(cnrNodes[i][j].PublicKey())
 			if prm.localNodeInContainer {
-				if prm.copiesNumber > 0 {
-					return errors.New("storage of multiple object replicas is requested for a local operation")
-				}
 				prm.localNodePos[0], prm.localNodePos[1] = i, j
 				break nextSet
 			}
 		}
 	}
-	if !prm.localNodeInContainer && prm.common.LocalOnly() {
+	if !prm.localNodeInContainer && localOnly {
 		return errors.New("local operation on the node not compliant with the container storage policy")
 	}
 