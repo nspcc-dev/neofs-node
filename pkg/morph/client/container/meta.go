72c84709581e32fad0b08c944d10d6e6ffc9475e
Pavel Karpy <carpawell@nspcc.ru>
2024-12-11 11:26:47 +0300

    morph,node/object/put: support "strict" meta-data policy
    According to https://github.com/nspcc-dev/neofs-api/pull/309, "strict" must wait
    for meta-data handling on the contract-side and every PUT request is responded
    only with transaction acceptance.
    
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/morph/client/container/meta.go
+++ pkg/morph/client/container/meta.go
@@ -6,10 +6,11 @@ import (
 	"github.com/nspcc-dev/neofs-node/pkg/morph/client"
 )
 
-// SubmitObjectPut puts object meta information.
+// SubmitObjectPut puts object meta information. With awaitTX it blocks until
+// TX is accepted in chain or is expired.
 //
 // Returns any error encountered that caused the saving to interrupt.
-func (c *Client) SubmitObjectPut(meta []byte, sigs [][]byte) error {
+func (c *Client) SubmitObjectPut(awaitTX bool, meta []byte, sigs [][]byte) error {
 	if len(meta) == 0 || len(sigs) == 0 {
 		return errNilArgument
 	}
@@ -17,6 +18,9 @@ func (c *Client) SubmitObjectPut(meta []byte, sigs [][]byte) error {
 	var prm client.InvokePrm
 	prm.SetMethod(submitObjectPutMethod)
 	prm.SetArgs(meta, sigs)
+	if awaitTX {
+		prm.Await()
+	}
 
 	err := c.client.Invoke(prm)
 	if err != nil {
