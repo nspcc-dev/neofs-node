nspcc-dev/neofs-node

commit 9ac84e30c21d61640d81409d860fb103a40f6d1e

cmd/neofs-cli: add error handler after execution command
Use `RunE` instead of `Run` and throw an error to main function. Wrap error with
`common.WrapError` to specify the status code. Then handle error with custom
error handler.

Refs #2890.

Signed-off-by: Andrey Butusov <andrey@nspcc.io>

cmd/neofs-cli/modules/control/shards_dump.go
@@ -1,8 +1,9 @@
 package control
 
 import (
+	"fmt"
+
 	"github.com/nspcc-dev/neofs-api-go/v2/rpc/client"
-	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/common"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/commonflags"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/key"
 	"github.com/nspcc-dev/neofs-node/pkg/services/control"
@@ -19,17 +20,24 @@ var dumpShardCmd = &cobra.Command{
 	Short: "Dump objects from shard",
 	Long:  "Dump objects from shard to a file",
 	Args:  cobra.NoArgs,
-	Run:   dumpShard,
+	RunE:  dumpShard,
 }
 
-func dumpShard(cmd *cobra.Command, _ []string) {
+func dumpShard(cmd *cobra.Command, _ []string) error {
 	ctx, cancel := commonflags.GetCommandContext(cmd)
 	defer cancel()
 
-	pk := key.Get(cmd)
+	pk, err := key.Get(cmd)
+	if err != nil {
+		return err
+	}
 
 	body := new(control.DumpShardRequest_Body)
-	body.SetShardID(getShardID(cmd))
+	id, err := getShardID(cmd)
+	if err != nil {
+		return err
+	}
+	body.SetShardID(id)
 
 	p, _ := cmd.Flags().GetString(dumpFilepathFlag)
 	body.SetFilepath(p)
@@ -40,21 +48,29 @@ func dumpShard(cmd *cobra.Command, _ []string) {
 	req := new(control.DumpShardRequest)
 	req.SetBody(body)
 
-	signRequest(cmd, pk, req)
+	err = signRequest(pk, req)
+	if err != nil {
+		return err
+	}
 
-	cli := getClient(ctx, cmd)
+	cli, err := getClient(ctx)
+	if err != nil {
+		return err
+	}
 
 	var resp *control.DumpShardResponse
-	var err error
 	err = cli.ExecRaw(func(client *client.Client) error {
 		resp, err = control.DumpShard(client, req)
 		return err
 	})
-	common.ExitOnErr(cmd, "rpc error: %w", err)
+	if err != nil {
+		return fmt.Errorf("rpc error: %w", err)
+	}
 
-	verifyResponse(cmd, resp.GetSignature(), resp.GetBody())
+	err = verifyResponse(resp.GetSignature(), resp.GetBody())
 
 	cmd.Println("Shard has been dumped successfully.")
+	return nil
 }
 
 func initControlDumpShardCmd() {

cmd/neofs-cli/modules/control/synchronize_tree.go
@@ -3,9 +3,9 @@ package control
 import (
 	"crypto/sha256"
 	"errors"
+	"fmt"
 
 	rawclient "github.com/nspcc-dev/neofs-api-go/v2/rpc/client"
-	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/common"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/commonflags"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/key"
 	"github.com/nspcc-dev/neofs-node/pkg/services/control"
@@ -24,7 +24,7 @@ var synchronizeTreeCmd = &cobra.Command{
 	Short: "Synchronize log for the tree",
 	Long:  "Synchronize log for the tree in an object tree service.",
 	Args:  cobra.NoArgs,
-	Run:   synchronizeTree,
+	RunE:  synchronizeTree,
 }
 
 func initControlSynchronizeTreeCmd() {
@@ -36,19 +36,21 @@ func initControlSynchronizeTreeCmd() {
 	flags.Uint64(synchronizeTreeHeightFlag, 0, "Starting height")
 }
 
-func synchronizeTree(cmd *cobra.Command, _ []string) {
+func synchronizeTree(cmd *cobra.Command, _ []string) error {
 	ctx, cancel := commonflags.GetCommandContext(cmd)
 	defer cancel()
 
-	pk := key.Get(cmd)
+	pk, err := key.Get(cmd)
 
 	var cnr cid.ID
 	cidStr, _ := cmd.Flags().GetString(commonflags.CIDFlag)
-	common.ExitOnErr(cmd, "can't decode container ID: %w", cnr.DecodeString(cidStr))
+	if err := cnr.DecodeString(cidStr); err != nil {
+		return fmt.Errorf("can't decode container ID: %w", err)
+	}
 
 	treeID, _ := cmd.Flags().GetString("tree-id")
 	if treeID == "" {
-		common.ExitOnErr(cmd, "", errors.New("tree ID must not be empty"))
+		return errors.New("tree ID must not be empty")
 	}
 
 	height, _ := cmd.Flags().GetUint64("height")
@@ -64,19 +66,30 @@ func synchronizeTree(cmd *cobra.Command, _ []string) {
 		},
 	}
 
-	err := controlSvc.SignMessage(pk, req)
-	common.ExitOnErr(cmd, "could not sign request: %w", err)
+	err = controlSvc.SignMessage(pk, req)
+	if err != nil {
+		return fmt.Errorf("could not sign request: %w", err)
+	}
 
-	cli := getClient(ctx, cmd)
+	cli, err := getClient(ctx)
+	if err != nil {
+		return err
+	}
 
 	var resp *control.SynchronizeTreeResponse
 	err = cli.ExecRaw(func(client *rawclient.Client) error {
 		resp, err = control.SynchronizeTree(client, req)
 		return err
 	})
-	common.ExitOnErr(cmd, "rpc error: %w", err)
+	if err != nil {
+		return fmt.Errorf("rpc error: %w", err)
+	}
 
-	verifyResponse(cmd, resp.GetSignature(), resp.GetBody())
+	err = verifyResponse(resp.GetSignature(), resp.GetBody())
+	if err != nil {
+		return err
+	}
 
 	cmd.Println("Tree has been synchronized successfully.")
+	return nil
 }

commit 2520d09781b2e9d5b20d0a2da736de85e3b92af1

cli/control: Do not lose errors
Regression from 9ac84e30c21d61640d81409d860fb103a40f6d1e. Detected by
the linter:
```
cmd/neofs-cli/modules/control/shards_dump.go:65:2    ineffassign  ineffectual assignment to err
cmd/neofs-cli/modules/control/synchronize_tree.go:41:6 ineffassign  ineffectual assignment to err
```

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

cmd/neofs-cli/modules/control/shards_dump.go
@@ -62,7 +62,9 @@ func dumpShard(cmd *cobra.Command, _ []string) error {
 		return fmt.Errorf("rpc error: %w", err)
 	}
 
-	err = verifyResponse(resp.GetSignature(), resp.GetBody())
+	if err = verifyResponse(resp.GetSignature(), resp.GetBody()); err != nil {
+		return err
+	}
 
 	cmd.Println("Shard has been dumped successfully.")
 	return nil

cmd/neofs-cli/modules/control/synchronize_tree.go
@@ -39,6 +39,9 @@ func synchronizeTree(cmd *cobra.Command, _ []string) error {
 	defer cancel()
 
 	pk, err := key.Get(cmd)
+	if err != nil {
+		return err
+	}
 
 	var cnr cid.ID
 	cidStr, _ := cmd.Flags().GetString(commonflags.CIDFlag)