nspcc-dev/neofs-node

commit 9ac84e30c21d61640d81409d860fb103a40f6d1e

cmd/neofs-cli: add error handler after execution command
Use `RunE` instead of `Run` and throw an error to main function. Wrap error with
`common.WrapError` to specify the status code. Then handle error with custom
error handler.

Refs #2890.

Signed-off-by: Andrey Butusov <andrey@nspcc.io>

cmd/neofs-cli/modules/object/delete.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	internalclient "github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/client"
-	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/common"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/commonflags"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/key"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
@@ -18,7 +17,7 @@ var objectDelCmd = &cobra.Command{
 	Short:   "Delete object from NeoFS",
 	Long:    "Delete object from NeoFS",
 	Args:    cobra.NoArgs,
-	Run:     deleteObject,
+	RunE:    deleteObject,
 }
 
 func initObjectDeleteCmd() {
@@ -36,7 +35,7 @@ func initObjectDeleteCmd() {
 	_ = objectDelCmd.MarkFlagRequired(commonflags.OIDFlag)
 }
 
-func deleteObject(cmd *cobra.Command, _ []string) {
+func deleteObject(cmd *cobra.Command, _ []string) error {
 	ctx, cancel := commonflags.GetCommandContext(cmd)
 	defer cancel()
 
@@ -47,17 +46,23 @@ func deleteObject(cmd *cobra.Command, _ []string) {
 	if binary {
 		filename, _ := cmd.Flags().GetString(fileFlag)
 		if filename == "" {
-			common.ExitOnErr(cmd, "", fmt.Errorf("required flag \"%s\" not set", fileFlag))
+			return fmt.Errorf("required flag \"%s\" not set", fileFlag)
 		}
 		var obj oid.ID
-		objAddr := readObjectAddressBin(cmd, &cnr, &obj, filename)
+		objAddr, err := readObjectAddressBin(&cnr, &obj, filename)
+		if err != nil {
+			return err
+		}
 
 		objAddrs = []oid.Address{objAddr}
 	} else {
-		readCID(cmd, &cnr)
+		err := readCID(cmd, &cnr)
+		if err != nil {
+			return err
+		}
 		oIDVals, _ := cmd.Flags().GetStringSlice(commonflags.OIDFlag)
 		if len(oIDVals) == 0 {
-			common.ExitOnErr(cmd, "", fmt.Errorf("provide at least one object via %s flag", commonflags.OIDFlag))
+			return fmt.Errorf("provide at least one object via %s flag", commonflags.OIDFlag)
 		}
 
 		var obj oid.ID
@@ -66,29 +71,43 @@ func deleteObject(cmd *cobra.Command, _ []string) {
 
 		for _, oIDraw := range oIDVals {
 			err := obj.DecodeString(oIDraw)
-			common.ExitOnErr(cmd, "decode object ID ("+oIDraw+") string: %w", err)
+			if err != nil {
+				return fmt.Errorf("decode object ID (\"+oIDraw+\") string: %w", err)
+			}
 
 			objAddr.SetObject(obj)
 			objAddrs = append(objAddrs, objAddr)
 		}
 	}
 
-	pk := key.GetOrGenerate(cmd)
+	pk, err := key.GetOrGenerate(cmd)
+	if err != nil {
+		return err
+	}
 
 	var prm internalclient.DeleteObjectPrm
 	prm.SetPrivateKey(*pk)
-	Prepare(cmd, &prm)
+	err = Prepare(cmd, &prm)
+	if err != nil {
+		return err
+	}
 
 	for _, addr := range objAddrs {
-		ReadOrOpenSession(ctx, cmd, &prm, pk, cnr, addr.Object())
+		err := ReadOrOpenSession(ctx, cmd, &prm, pk, cnr, addr.Object())
+		if err != nil {
+			return err
+		}
 		prm.SetAddress(addr)
 
 		res, err := internalclient.DeleteObject(ctx, prm)
-		common.ExitOnErr(cmd, "rpc error: deleting "+addr.Object().String()+" object: %w", err)
+		if err != nil {
+			return fmt.Errorf("rpc error: deleting \"+addr.Object().String()+\" object: %w", err)
+		}
 
 		tomb := res.Tombstone()
 
 		cmd.Printf("Object %s removed successfully.\n", addr.Object())
 		cmd.Printf("  ID: %s\n  CID: %s\n", tomb, cnr)
 	}
+	return nil
 }

cmd/neofs-cli/modules/container/delete.go
@@ -19,16 +19,28 @@ var deleteContainerCmd = &cobra.Command{
 	Long: `Delete existing container. 
 Only owner of the container has a permission to remove container.`,
 	Args: cobra.NoArgs,
-	Run: func(cmd *cobra.Command, _ []string) {
+	RunE: func(cmd *cobra.Command, _ []string) error {
 		ctx, cancel := getAwaitContext(cmd)
 		defer cancel()
 
-		id := parseContainerID(cmd)
+		id, err := parseContainerID()
+		if err != nil {
+			return err
+		}
 
-		tok := getSession(cmd)
+		tok, err := getSession(cmd)
+		if err != nil {
+			return err
+		}
 
-		pk := key.Get(cmd)
-		cli := internalclient.GetSDKClientByFlag(ctx, cmd, commonflags.RPC)
+		pk, err := key.Get(cmd)
+		if err != nil {
+			return err
+		}
+		cli, err := internalclient.GetSDKClientByFlag(ctx, commonflags.RPC)
+		if err != nil {
+			return err
+		}
 
 		if force, _ := cmd.Flags().GetBool(commonflags.ForceFlag); !force {
 			common.PrintVerbose(cmd, "Reading the container to check ownership...")
@@ -38,7 +50,9 @@ Only owner of the container has a permission to remove container.`,
 			getPrm.SetContainer(id)
 
 			resGet, err := internalclient.GetContainer(ctx, getPrm)
-			common.ExitOnErr(cmd, "can't get the container: %w", err)
+			if err != nil {
+				return fmt.Errorf("can't get the container: %w", err)
+			}
 
 			owner := resGet.Container().Owner()
 
@@ -46,7 +60,7 @@ Only owner of the container has a permission to remove container.`,
 				common.PrintVerbose(cmd, "Checking session issuer...")
 
 				if !tok.Issuer().Equals(owner) {
-					common.ExitOnErr(cmd, "", fmt.Errorf("session issuer differs with the container owner: expected %s, has %s", owner, tok.Issuer()))
+					return fmt.Errorf("session issuer differs with the container owner: expected %s, has %s", owner, tok.Issuer())
 				}
 			} else {
 				common.PrintVerbose(cmd, "Checking provided account...")
@@ -54,7 +68,7 @@ Only owner of the container has a permission to remove container.`,
 				acc := user.ResolveFromECDSAPublicKey(pk.PublicKey)
 
 				if !acc.Equals(owner) {
-					common.ExitOnErr(cmd, "", fmt.Errorf("provided account differs with the container owner: expected %s, has %s", owner, acc))
+					return fmt.Errorf("provided account differs with the container owner: expected %s, has %s", owner, acc)
 				}
 			}
 
@@ -76,12 +90,13 @@ Only owner of the container has a permission to remove container.`,
 				common.PrintVerbose(cmd, "Searching for LOCK objects...")
 
 				res, err := internalclient.SearchObjects(ctx, searchPrm)
-				common.ExitOnErr(cmd, "can't search for LOCK objects: %w", err)
+				if err != nil {
+					return fmt.Errorf("can't search for LOCK objects: %w", err)
+				}
 
 				if len(res.IDList()) != 0 {
-					common.ExitOnErr(cmd, "",
-						fmt.Errorf("Container wasn't removed because LOCK objects were found.\n"+
-							"Use --%s flag to remove anyway.", commonflags.ForceFlag))
+					return fmt.Errorf("Container wasn't removed because LOCK objects were found.\n"+
+						"Use --%s flag to remove anyway.", commonflags.ForceFlag)
 				}
 			}
 		}
@@ -95,8 +110,10 @@ Only owner of the container has a permission to remove container.`,
 			delPrm.WithinSession(*tok)
 		}
 
-		_, err := internalclient.DeleteContainer(ctx, delPrm)
-		common.ExitOnErr(cmd, "rpc error: %w", err)
+		_, err = internalclient.DeleteContainer(ctx, delPrm)
+		if err != nil {
+			return fmt.Errorf("rpc error: %w", err)
+		}
 
 		cmd.Println("container removal request accepted for processing (the operation may not be completed yet)")
 
@@ -115,17 +132,18 @@ Only owner of the container has a permission to remove container.`,
 			for ; ; t.Reset(waitInterval) {
 				select {
 				case <-ctx.Done():
-					common.ExitOnErr(cmd, "container deletion: %s", common.ErrAwaitTimeout)
+					return fmt.Errorf("container deletion: %s", common.ErrAwaitTimeout)
 				case <-t.C:
 				}
 
 				_, err := internalclient.GetContainer(ctx, getPrm)
 				if err != nil {
 					cmd.Println("container has been removed:", containerID)
-					return
+					return nil
 				}
 			}
 		}
+		return nil
 	},
 }
 

cmd/neofs-cli/modules/storagegroup/delete.go
@@ -1,8 +1,9 @@
 package storagegroup
 
 import (
+	"fmt"
+
 	internalclient "github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/client"
-	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/common"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/commonflags"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-cli/internal/key"
 	objectCli "github.com/nspcc-dev/neofs-node/cmd/neofs-cli/modules/object"
@@ -16,7 +17,7 @@ var sgDelCmd = &cobra.Command{
 	Short: "Delete storage group from NeoFS",
 	Long:  "Delete storage group from NeoFS",
 	Args:  cobra.NoArgs,
-	Run:   delSG,
+	RunE:  delSG,
 }
 
 func initSGDeleteCmd() {
@@ -31,28 +32,44 @@ func initSGDeleteCmd() {
 	_ = sgDelCmd.MarkFlagRequired(sgIDFlag)
 }
 
-func delSG(cmd *cobra.Command, _ []string) {
+func delSG(cmd *cobra.Command, _ []string) error {
 	ctx, cancel := commonflags.GetCommandContext(cmd)
 	defer cancel()
 
-	pk := key.GetOrGenerate(cmd)
+	pk, err := key.GetOrGenerate(cmd)
+	if err != nil {
+		return err
+	}
 
 	var cnr cid.ID
 	var obj oid.ID
 
-	addr := readObjectAddress(cmd, &cnr, &obj)
+	addr, err := readObjectAddress(cmd, &cnr, &obj)
+	if err != nil {
+		return err
+	}
 
 	var prm internalclient.DeleteObjectPrm
 	prm.SetPrivateKey(*pk)
-	objectCli.OpenSession(ctx, cmd, &prm, pk, cnr, obj)
-	objectCli.Prepare(cmd, &prm)
+	err = objectCli.OpenSession(ctx, cmd, &prm, pk, cnr, obj)
+	if err != nil {
+		return err
+	}
+	err = objectCli.Prepare(cmd, &prm)
+	if err != nil {
+		return err
+	}
 	prm.SetAddress(addr)
 
 	res, err := internalclient.DeleteObject(ctx, prm)
-	common.ExitOnErr(cmd, "rpc error: %w", err)
+	if err != nil {
+		return fmt.Errorf("rpc error: %w", err)
+	}
 
 	tombstone := res.Tombstone()
 
 	cmd.Println("Storage group removed successfully.")
 	cmd.Printf("  Tombstone: %s\n", tombstone)
+
+	return nil
 }

commit ed40476cede16bcb08b4bbdd5258c481f3fe6c10

cli: Fix `object delete` command output
Regression from 9ac84e30c21d61640d81409d860fb103a40f6d1e. Could raise
following error:
```
rpc error: deleting "+addr.Object().String()+" object: ...
```

Fixes #3056.

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

cmd/neofs-cli/modules/object/delete.go
@@ -101,7 +101,7 @@ func deleteObject(cmd *cobra.Command, _ []string) error {
 
 		res, err := internalclient.DeleteObject(ctx, prm)
 		if err != nil {
-			return fmt.Errorf("rpc error: deleting \"+addr.Object().String()+\" object: %w", err)
+			return fmt.Errorf("rpc error: deleting %s object: %w", addr.Object(), err)
 		}
 
 		tomb := res.Tombstone()