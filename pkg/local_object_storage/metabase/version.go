eea3ce1bc73cda70c93f4c51644f5abcf0267354
Roman Khimov <roman@nspcc.ru>
2025-03-06 21:33:39 +0300

    *: deobfuscate some if statements
    We have enough complexity in this code base already, no need to add more
    with things like this.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- pkg/local_object_storage/metabase/version.go
+++ pkg/local_object_storage/metabase/version.go
@@ -205,7 +205,8 @@ func migrateContainerToMetaBucket(l *zap.Logger, tx *bbolt.Tx, c *bbolt.Cursor, 
 			return 0, nil, err
 		}
 		if ok {
-			if done++; done == limit {
+			done++
+			if done == limit {
 				break
 			}
 		}
