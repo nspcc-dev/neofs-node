62177a13375b570d4eb24580698e8d3b72c7681a
Pavel Karpy <carpawell@nspcc.ru>
2025-03-19 13:34:32 +0300

    meta: index parent object the same way metabase does
    It is the only way to get root object's header.
    
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/services/meta/notifications_test.go
+++ pkg/services/meta/notifications_test.go
@@ -419,6 +419,7 @@ func TestObjectPut(t *testing.T) {
 		size := uint64(testObjectSize)
 
 		o := objecttest.Object()
+		o.ResetRelations()
 		o.SetContainerID(cID)
 		o.SetID(objToDeleteOID)
 		o.SetPayloadSize(size)
@@ -484,7 +485,7 @@ func TestObjectPut(t *testing.T) {
 			}
 
 			tempM := make(map[string][]byte)
-			fillObjectIndex(tempM, o)
+			fillObjectIndex(tempM, o, false)
 			// dbKeys := maps.Keys(tempM) // go 1.23+
 			dbKeys := make([][]byte, 0, len(tempM))
 			for k := range tempM {
@@ -623,6 +624,7 @@ func TestValidation(t *testing.T) {
 func TestCompatibility(t *testing.T) {
 	o := objecttest.Object()
 	o.SetSplitID(nil) // no split info is expected for split V2 era
+	o.ResetRelations()
 
 	// database from engine's metabases
 
@@ -653,7 +655,7 @@ func TestCompatibility(t *testing.T) {
 	// batch for meta-data service
 
 	serviceMap := make(map[string][]byte)
-	fillObjectIndex(serviceMap, o)
+	fillObjectIndex(serviceMap, o, false)
 
 	require.Equal(t, len(metabaseMap), len(serviceMap))
 	for k := range metabaseMap {
