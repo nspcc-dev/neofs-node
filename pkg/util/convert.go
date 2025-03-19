4ef0f73cc1f98ffefe019997da1fd994399c342f
Roman Khimov <roman@nspcc.ru>
2025-03-19 17:47:09 +0300

    util: drop now unused MapToSlice
    It's obsolete and it's not used now.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- pkg/util/convert.go
+++ pkg/util/convert.go
@@ -1,16 +1,5 @@
 package util
 
-// MapToSlice converts the map to a slice. Order is not
-// fixed and _is not_ ascending or descending.
-func MapToSlice[V comparable](m map[V]struct{}) []V {
-	res := make([]V, 0, len(m))
-	for v := range m {
-		res = append(res, v)
-	}
-
-	return res
-}
-
 // SliceToMap converts any slice with comparable elements to a map.
 func SliceToMap[V comparable](s []V) map[V]struct{} {
 	res := make(map[V]struct{}, len(s))
