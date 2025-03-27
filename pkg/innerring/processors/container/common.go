nspcc-dev/neofs-node

commit 4b7eee5c02938b9c2e84fe1e78a61f40581af4a4

ir: do not use neofsid for container checks
neofsid is obsolete, this mechanism is completely irrelevant for proper
N3 accounts. Notice that this makes keys mandatory in signatures for now,
but that's expected to be OK (no one actually uses neofsid anyway).

Notice that container deletion requests don't have any key attached, so we
recover it from the container data for now.

Signed-off-by: Roman Khimov <roman@nspcc.ru>

pkg/innerring/processors/container/common.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/nspcc-dev/neofs-node/pkg/morph/client/neofsid"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
 	neofsecdsa "github.com/nspcc-dev/neofs-sdk-go/crypto/ecdsa"
 	"github.com/nspcc-dev/neofs-sdk-go/session"
@@ -49,13 +48,14 @@ type signatureVerificationData struct {
 func (cp *Processor) verifySignature(v signatureVerificationData) error {
 	var err error
 	var key neofsecdsa.PublicKeyRFC6979
-	keyProvided := v.binPublicKey != nil
 
-	if keyProvided {
-		err = key.Decode(v.binPublicKey)
-		if err != nil {
-			return fmt.Errorf("decode public key: %w", err)
-		}
+	if v.binPublicKey == nil {
+		return errors.New("can't verify signature, key missing")
+	}
+
+	err = key.Decode(v.binPublicKey)
+	if err != nil {
+		return fmt.Errorf("decode public key: %w", err)
 	}
 
 	if len(v.binTokenSession) > 0 {
@@ -75,12 +75,11 @@ func (cp *Processor) verifySignature(v signatureVerificationData) error {
 			return fmt.Errorf("invalid issuer public key: %w", err)
 		}
 
-		// TODO(@cthulhu-rider): #1387 check bound keys via NeoFSID contract?
 		if user.NewFromECDSAPublicKey(ecdsa.PublicKey(signerPub)) != v.ownerContainer {
 			return errors.New("session token is not signed by the container owner")
 		}
 
-		if keyProvided && !tok.AssertAuthKey(&key) {
+		if !tok.AssertAuthKey(&key) {
 			return errors.New("signed with a non-session key")
 		}
 
@@ -108,31 +107,15 @@ func (cp *Processor) verifySignature(v signatureVerificationData) error {
 		return nil
 	}
 
-	if keyProvided {
-		// TODO(@cthulhu-rider): #1387 use another approach after neofs-sdk-go#233
-		idFromKey := user.NewFromECDSAPublicKey(ecdsa.PublicKey(key))
+	// TODO(@cthulhu-rider): #1387 use another approach after neofs-sdk-go#233
+	idFromKey := user.NewFromECDSAPublicKey(ecdsa.PublicKey(key))
 
-		if v.ownerContainer == idFromKey {
-			if key.Verify(v.signedData, v.signature) {
-				return nil
-			}
-
-			return errors.New("invalid signature calculated by container owner's key")
-		}
-	} else {
-		var prm neofsid.AccountKeysPrm
-		prm.SetID(v.ownerContainer)
-
-		ownerKeys, err := cp.idClient.AccountKeys(prm)
-		if err != nil {
-			return fmt.Errorf("receive owner keys %s: %w", v.ownerContainer, err)
+	if v.ownerContainer == idFromKey {
+		if key.Verify(v.signedData, v.signature) {
+			return nil
 		}
 
-		for i := range ownerKeys {
-			if (*neofsecdsa.PublicKeyRFC6979)(ownerKeys[i]).Verify(v.signedData, v.signature) {
-				return nil
-			}
-		}
+		return errors.New("invalid signature calculated by container owner's key")
 	}
 
 	return errors.New("signature is invalid or calculated with the key not bound to the container owner")

pkg/innerring/innerring.go
@@ -940,7 +940,6 @@ func New(ctx context.Context, log *zap.Logger, cfg *viper.Viper, errChan chan<- 
 		PoolSize:        cfg.GetInt("workers.container"),
 		AlphabetState:   server,
 		ContainerClient: cnrClient,
-		NeoFSIDClient:   neofsIDClient,
 		NetworkState:    server.netmapClient,
 		MetaEnabled:     cfg.GetBool("experimental.chain_meta_data"),
 	})

pkg/innerring/processors/container/process_container.go
@@ -202,6 +202,7 @@ func (cp *Processor) checkDeleteContainer(e *containerEvent.Delete) error {
 		verb:            session.VerbContainerDelete,
 		idContainerSet:  true,
 		idContainer:     idCnr,
+		binPublicKey:    cnr.Signature.PublicKeyBytes(),
 		binTokenSession: e.SessionToken(),
 		signature:       e.Signature(),
 		signedData:      binCnr,

pkg/innerring/processors/container/processor.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/nspcc-dev/neofs-node/pkg/morph/client/container"
-	"github.com/nspcc-dev/neofs-node/pkg/morph/client/neofsid"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/event"
 	containerEvent "github.com/nspcc-dev/neofs-node/pkg/morph/event/container"
 	"github.com/nspcc-dev/neofs-sdk-go/netmap"
@@ -25,7 +24,6 @@ type (
 		pool          *ants.Pool
 		alphabetState AlphabetState
 		cnrClient     *container.Client // notary must be enabled
-		idClient      *neofsid.Client
 		netState      NetworkState
 		metaEnabled   bool
 	}
@@ -36,7 +34,6 @@ type (
 		PoolSize        int
 		AlphabetState   AlphabetState
 		ContainerClient *container.Client
-		NeoFSIDClient   *neofsid.Client
 		NetworkState    NetworkState
 		MetaEnabled     bool
 	}
@@ -72,8 +69,6 @@ func New(p *Params) (*Processor, error) {
 		return nil, errors.New("ir/container: global state is not set")
 	case p.ContainerClient == nil:
 		return nil, errors.New("ir/container: Container client is not set")
-	case p.NeoFSIDClient == nil:
-		return nil, errors.New("ir/container: NeoFS ID client is not set")
 	case p.NetworkState == nil:
 		return nil, errors.New("ir/container: network state is not set")
 	}
@@ -90,7 +85,6 @@ func New(p *Params) (*Processor, error) {
 		pool:          pool,
 		alphabetState: p.AlphabetState,
 		cnrClient:     p.ContainerClient,
-		idClient:      p.NeoFSIDClient,
 		netState:      p.NetworkState,
 		metaEnabled:   p.MetaEnabled,
 	}, nil

commit 8968f55364eab0536b188bcc63900cc2085e1e2c

ir: drop session key check
We've got test_static_session_token_container_delete failing after
4b7eee5c02938b9c2e84fe1e78a61f40581af4a4. This happens because deletion event
has no real key and we're substituting it by the key used when container was
created. It's an issuer key effectively, not a session key. And IMO this check
is not very useful, we're checking signature against session-bound key anyway
adn we trust this key because the token is issued by correct owner. The key from
event is not very useful from the security perspective then.

The code extracting the key is moved as well because we can avoid decoding for
the tokenized case.

Signed-off-by: Roman Khimov <roman@nspcc.ru>

pkg/innerring/processors/container/common.go
@@ -79,10 +79,6 @@ func (cp *Processor) verifySignature(v signatureVerificationData) error {
 			return errors.New("session token is not signed by the container owner")
 		}
 
-		if !tok.AssertAuthKey(&key) {
-			return errors.New("signed with a non-session key")
-		}
-
 		if !tok.AssertVerb(v.verb) {
 			return errWrongSessionVerb
 		}