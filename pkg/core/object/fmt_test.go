de67552cf397fe4943bd5a82802dee2f000c7c19
Leonard Lyubich <leonard@morphbits.io>
2025-04-01 14:33:02 +0300

    node/object: Increase coverage of object signatures' verification tests
    Continues 8d3054d70d0561304ca73ee03f5844133d80dffe. Same changes are
    going to be applied for object handling too. By that time it is
    desirable to have a deeper coverage.
    
    Also, fix unsigned case: it was incorrect due to wrong assertion.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- pkg/core/object/fmt_test.go
+++ pkg/core/object/fmt_test.go
@@ -2,10 +2,12 @@ package object
 
 import (
 	"context"
+	"slices"
 	"strconv"
 	"testing"
 
 	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
+	"github.com/nspcc-dev/neofs-sdk-go/checksum"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
 	cidtest "github.com/nspcc-dev/neofs-sdk-go/container/id/test"
 	neofscrypto "github.com/nspcc-dev/neofs-sdk-go/crypto"
@@ -18,6 +20,8 @@ import (
 	"github.com/nspcc-dev/neofs-sdk-go/storagegroup"
 	"github.com/nspcc-dev/neofs-sdk-go/user"
 	usertest "github.com/nspcc-dev/neofs-sdk-go/user/test"
+	"github.com/nspcc-dev/neofs-sdk-go/version"
+	"github.com/nspcc-dev/tzhash/tz"
 	"github.com/stretchr/testify/require"
 )
 
@@ -27,6 +31,18 @@ func blankValidObject(signer user.Signer) *object.Object {
 	obj.SetOwner(signer.UserID())
 
 	return obj
+}
+
+// returns an object which is valid when signed by the resulting signer.
+func minUnsignedObject(t testing.TB) (object.Object, usertest.UserSigner) {
+	usr := usertest.User()
+
+	var obj object.Object
+	obj.SetContainerID(cidtest.ID())
+	obj.SetOwner(usr.ID)
+	require.NoError(t, obj.CalculateAndSetID())
+
+	return obj, usr
 }
 
 type testNetState struct {
@@ -87,12 +103,85 @@ func TestFormatValidator_Validate(t *testing.T) {
 		require.ErrorIs(t, v.Validate(obj, true), errNilCID)
 	})
 
-	t.Run("unsigned object", func(t *testing.T) {
-		obj := object.New()
-		obj.SetContainerID(cidtest.ID())
-		obj.SetID(oidtest.ID())
+	t.Run("invalid signature", func(t *testing.T) {
+		t.Run("unsigned", func(t *testing.T) {
+			obj, _ := minUnsignedObject(t)
+			require.EqualError(t, v.Validate(&obj, false), "(*object.FormatValidator) could not validate signature key: missing signature")
+		})
+		t.Run("unsupported scheme", func(t *testing.T) {
+			obj, signer := minUnsignedObject(t)
 
-		require.Error(t, v.Validate(obj, false))
+			sigBytes, err := signer.Sign(obj.GetID().Marshal())
+			require.NoError(t, err)
+			sig := neofscrypto.NewSignature(3, signer.Public(), sigBytes)
+			obj.SetSignature(&sig)
+
+			require.EqualError(t, v.Validate(&obj, false), "(*object.FormatValidator) could not validate header fields: invalid signature")
+		})
+		t.Run("wrong scheme", func(t *testing.T) {
+			obj, signer := minUnsignedObject(t)
+
+			sigBytes, err := signer.RFC6979.Sign(obj.GetID().Marshal())
+			require.NoError(t, err)
+			sig := neofscrypto.NewSignature(neofscrypto.ECDSA_WALLETCONNECT, signer.Public(), sigBytes)
+			obj.SetSignature(&sig)
+
+			require.EqualError(t, v.Validate(&obj, false), "(*object.FormatValidator) could not validate header fields: invalid signature")
+		})
+		t.Run("invalid public key", func(t *testing.T) {
+			obj, signer := minUnsignedObject(t)
+			sigBytes, err := signer.Sign(obj.GetID().Marshal())
+			require.NoError(t, err)
+			sig := neofscrypto.NewSignatureFromRawKey(signer.Scheme(), signer.PublicKeyBytes, sigBytes)
+
+			for _, tc := range []struct {
+				name      string
+				changePub func([]byte) []byte
+				skip      bool
+			}{
+				{name: "nil", changePub: func([]byte) []byte { return nil }},
+				{name: "empty", changePub: func([]byte) []byte { return []byte{} }},
+				{name: "undersize", changePub: func(k []byte) []byte { return k[:len(k)-1] }},
+				{name: "oversize", changePub: func(k []byte) []byte { return append(k, 1) }},
+				{name: "prefix 0", changePub: func(k []byte) []byte { return []byte{0x00} }, skip: true},
+				{name: "prefix 1", changePub: func(k []byte) []byte { return []byte{0x01} }},
+				{name: "prefix 4", changePub: func(k []byte) []byte { return []byte{0x04} }},
+				{name: "prefix 5", changePub: func(k []byte) []byte { return []byte{0x05} }},
+			} {
+				t.Run(tc.name, func(t *testing.T) {
+					if tc.skip {
+						t.Skip()
+					}
+					pub := slices.Clone(signer.PublicKeyBytes)
+					sig.SetPublicKeyBytes(tc.changePub(pub))
+					obj.SetSignature(&sig)
+					require.EqualError(t, v.Validate(&obj, false), "(*object.FormatValidator) could not validate header fields: invalid signature")
+				})
+			}
+		})
+		t.Run("mismatch", func(t *testing.T) {
+			for _, tc := range []struct {
+				scheme neofscrypto.Scheme
+				object object.Object
+			}{
+				{scheme: neofscrypto.ECDSA_SHA512, object: smallECDSASHA512},
+				{scheme: neofscrypto.ECDSA_DETERMINISTIC_SHA256, object: smallECDSARFC6979},
+				{scheme: neofscrypto.ECDSA_WALLETCONNECT, object: smallECDSAWalletConnect},
+			} {
+				t.Run(tc.scheme.String(), func(t *testing.T) {
+					sig := tc.object.Signature()
+					require.NotNil(t, sig)
+					validSig := sig.Value()
+					for i := range validSig {
+						cp := slices.Clone(validSig)
+						cp[i]++
+						newSig := neofscrypto.NewSignatureFromRawKey(sig.Scheme(), sig.PublicKeyBytes(), cp)
+						tc.object.SetSignature(&newSig)
+						require.EqualError(t, v.Validate(&tc.object, false), "(*object.FormatValidator) could not validate header fields: invalid signature")
+					}
+				})
+			}
+		})
 	})
 
 	t.Run("correct w/ session token", func(t *testing.T) {
@@ -378,6 +467,18 @@ func TestFormatValidator_Validate(t *testing.T) {
 			})
 		})
 	})
+	for _, tc := range []struct {
+		scheme neofscrypto.Scheme
+		object object.Object
+	}{
+		{scheme: neofscrypto.ECDSA_SHA512, object: smallECDSASHA512},
+		{scheme: neofscrypto.ECDSA_DETERMINISTIC_SHA256, object: smallECDSARFC6979},
+		{scheme: neofscrypto.ECDSA_WALLETCONNECT, object: smallECDSAWalletConnect},
+	} {
+		t.Run(tc.scheme.String(), func(t *testing.T) {
+			require.NoError(t, v.Validate(&tc.object, false))
+		})
+	}
 }
 
 type testSplitVerifier struct {
@@ -426,4 +527,88 @@ func TestLinkObjectSplitV2(t *testing.T) {
 			require.ErrorContains(t, v.Validate(obj, true), "middle part does not have previous object ID")
 		})
 	})
+}
+
+// set in init.
+var (
+	smallECDSASHA512        object.Object
+	smallECDSARFC6979       object.Object
+	smallECDSAWalletConnect object.Object
+)
+
+var (
+	// ECDSA private key used in tests.
+	// privECDSA = ecdsa.PrivateKey{
+	// 	PublicKey: ecdsa.PublicKey{
+	// 		Curve: elliptic.P256(),
+	// 		X: new(big.Int).SetBytes([]byte{206, 67, 193, 231, 254, 180, 127, 78, 101, 154, 23, 161, 134, 77, 122, 34, 234, 85,
+	// 			149, 44, 32, 223, 244, 140, 28, 194, 76, 214, 239, 121, 174, 40}),
+	// 		Y: new(big.Int).SetBytes([]byte{170, 190, 155, 176, 31, 11, 4, 14, 103, 210, 53, 0, 73, 46, 81, 129, 163, 217, 81, 51, 111,
+	// 			135, 223, 253, 48, 104, 240, 197, 122, 37, 197, 78}),
+	// 	},
+	// 	D: new(big.Int).SetBytes([]byte{185, 97, 226, 151, 175, 3, 234, 11, 168, 211, 53, 141, 136, 102, 100, 222, 73, 174, 234, 157,
+	// 		139, 192, 66, 145, 13, 173, 12, 120, 22, 134, 52, 180}),
+	// }
+	// corresponds to the private key.
+	pubECDSA = []byte{2, 206, 67, 193, 231, 254, 180, 127, 78, 101, 154, 23, 161, 134, 77, 122, 34, 234, 85, 149, 44, 32, 223,
+		244, 140, 28, 194, 76, 214, 239, 121, 174, 40}
+	// corresponds to pubECDSA.
+	ownerECDSA = user.ID{53, 57, 243, 96, 136, 255, 217, 227, 204, 13, 243, 228, 109, 31, 226, 226, 236, 62, 13, 190, 156, 135, 252, 236, 8}
+)
+
+func getUnsignedObject() object.Object {
+	const typ = 523555632
+	const creationEpoch = 10734578609111205341
+	ver := version.New(123, 456)
+	cnr := cid.ID{61, 208, 16, 128, 106, 78, 90, 196, 156, 65, 180, 142, 62, 137, 245, 242, 69, 250, 212, 176, 35, 114, 239, 114, 53,
+		231, 19, 14, 46, 67, 163, 155}
+	payload := []byte("Hello, world!")
+	payloadLen := uint64(len(payload))
+
+	var obj object.Object
+	obj.SetVersion(&ver)
+	obj.SetContainerID(cnr)
+	obj.SetOwner(ownerECDSA)
+	obj.SetCreationEpoch(creationEpoch)
+	obj.SetPayloadSize(payloadLen)
+	obj.SetType(typ)
+	obj.SetAttributes(
+		*object.NewAttribute("2958922649728181678", "13190161871171818625"),
+		*object.NewAttribute("15660925904602412174", "67216741819241411480"),
+	)
+
+	obj.SetPayload(payload)
+	obj.CalculateAndSetPayloadChecksum()
+	obj.SetPayloadHomomorphicHash(checksum.NewTillichZemor(tz.Sum(payload)))
+
+	obj.SetID(oid.ID{231, 249, 6, 213, 114, 154, 74, 74, 49, 179, 107, 109, 34, 56, 68, 54, 226, 226, 16, 54, 217, 41, 138, 188, 245,
+		97, 133, 227, 199, 159, 163, 21}) // header checksum
+
+	return obj
+}
+
+func init() {
+	smallECDSASHA512 = getUnsignedObject()
+	smallECDSASHA512Sig := neofscrypto.NewSignatureFromRawKey(neofscrypto.ECDSA_SHA512, pubECDSA, []byte{
+		4, 155, 202, 63, 77, 61, 145, 168, 242, 244, 233, 217, 104, 177, 168, 238, 60, 245, 248, 110, 173, 244, 253, 25, 106, 241,
+		155, 255, 146, 133, 58, 142, 37, 252, 48, 246, 71, 183, 118, 139, 122, 51, 167, 225, 52, 172, 144, 76, 79, 208, 210, 66, 118,
+		141, 198, 206, 220, 180, 57, 5, 105, 179, 58, 21, 255,
+	})
+	smallECDSASHA512.SetSignature(&smallECDSASHA512Sig)
+
+	smallECDSARFC6979 = getUnsignedObject()
+	smallECDSARFC6979Sig := neofscrypto.NewSignatureFromRawKey(neofscrypto.ECDSA_DETERMINISTIC_SHA256, pubECDSA, []byte{
+		86, 187, 47, 50, 36, 253, 213, 202, 229, 147, 51, 192, 54, 230, 52, 223, 69, 50, 136, 141, 103, 119, 72, 251, 86, 44, 224,
+		46, 3, 225, 37, 71, 43, 190, 33, 99, 64, 218, 92, 80, 215, 40, 144, 165, 208, 139, 51, 212, 223, 109, 82, 209, 167, 66, 147,
+		198, 168, 41, 160, 93, 93, 30, 195, 230,
+	})
+	smallECDSARFC6979.SetSignature(&smallECDSARFC6979Sig)
+
+	smallECDSAWalletConnect = getUnsignedObject()
+	smallECDSAWalletConnectSig := neofscrypto.NewSignatureFromRawKey(neofscrypto.ECDSA_WALLETCONNECT, pubECDSA, []byte{
+		226, 1, 51, 87, 97, 60, 78, 234, 198, 247, 158, 205, 143, 163, 146, 100, 46, 146, 77, 86, 91, 102, 74, 235, 138, 12, 46, 59, 33,
+		166, 149, 7, 185, 247, 253, 2, 12, 45, 33, 53, 59, 200, 255, 137, 115, 101, 29, 165, 127, 14, 178, 130, 34, 43, 65, 232, 9, 180,
+		164, 37, 91, 103, 137, 183, 250, 198, 162, 23, 131, 43, 36, 243, 142, 150, 255, 142, 219, 254, 19, 130,
+	})
+	smallECDSAWalletConnect.SetSignature(&smallECDSAWalletConnectSig)
 }
