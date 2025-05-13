e8311cfe8edbcf7ca1457cd2251eaed10908f12a
Roman Khimov <roman@nspcc.ru>
2025-05-13 22:16:52 +0300

    balance: simplify Transfer() code
    util.Uint160 can always be extracted from user.ID, no errors possible.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- pkg/morph/client/balance/transfer.go
+++ pkg/morph/client/balance/transfer.go
@@ -3,7 +3,6 @@ package balance
 import (
 	"fmt"
 
-	"github.com/nspcc-dev/neo-go/pkg/encoding/address"
 	"github.com/nspcc-dev/neofs-node/pkg/morph/client"
 	"github.com/nspcc-dev/neofs-sdk-go/user"
 )
@@ -22,22 +21,12 @@ type TransferPrm struct {
 // TransferX transfers p.Amount of GASe-12 from p.From to p.To
 // with details p.Details through direct smart contract call.
 func (c *Client) TransferX(p TransferPrm) error {
-	from, err := address.StringToUint160(p.From.EncodeToString())
-	if err != nil {
-		return err
-	}
-
-	to, err := address.StringToUint160(p.To.EncodeToString())
-	if err != nil {
-		return err
-	}
-
 	prm := client.InvokePrm{}
 	prm.SetMethod(transferXMethod)
-	prm.SetArgs(from, to, p.Amount, p.Details)
+	prm.SetArgs(p.From.ScriptHash(), p.To.ScriptHash(), p.Amount, p.Details)
 	prm.InvokePrmOptional = p.InvokePrmOptional
 
-	err = c.client.Invoke(prm)
+	err := c.client.Invoke(prm)
 	if err != nil {
 		return fmt.Errorf("could not invoke method (%s): %w", transferXMethod, err)
 	}
