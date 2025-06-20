efd8bc204eeff4693ef4f88d1ac3d84ff8950c84
Leonard Lyubich <leonard@morphbits.io>
2025-06-20 13:19:10 +0300

    sn/object: Avoid useless logger cloning in ACL processor
    Follow 91bb4e267b16678a9f683a473f14f164b3e90b9f.
    
    Same test showed following alloc objects
    ```
    1299856  0.19% 88.10%    5310102  0.76%  go.uber.org/zap/zapcore.(*ioCore).clone (inline)
    ```
    and space
    ```
    1.27GB  1.90% 64.69%     1.27GB  1.90%  go.uber.org/zap/internal/bufferpool.init.NewPool.func1
    ```
    profiles.
    
    This replaces logger cloning with explicit field parameterization. The
    behavior is kept the same. Allocations mentioned above are no longer
    visible.
    
    Go benchmark test:
    ```
    goos: linux
    goarch: amd64
    pkg: github.com/nspcc-dev/neofs-node/pkg/services/object/acl/v2
    cpu: Intel(R) Core(TM) i5-10210U CPU @ 1.60GHz
                                 │   old.txt    │               new.txt               │
                                 │    sec/op    │   sec/op     vs base                │
    ClassifierLoggerProduction-8   3855.0n ± 2%   661.1n ± 8%  -82.85% (p=0.000 n=10)
    
                                 │   old.txt    │                new.txt                │
                                 │     B/op     │     B/op      vs base                 │
    ClassifierLoggerProduction-8   1.704Ki ± 0%   0.000Ki ± 0%  -100.00% (p=0.000 n=10)
    
                                 │  old.txt   │              new.txt               │
                                 │ allocs/op  │ allocs/op  vs base                 │
    ClassifierLoggerProduction-8   13.00 ± 0%   0.00 ± 0%  -100.00% (p=0.000 n=10)
    ```
    
    Refs #3409.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

---
+++ pkg/services/object/acl/v2/classifier_test.go
@@ -0,0 +1,51 @@
+package v2
+
+import (
+	"testing"
+
+	"github.com/nspcc-dev/neofs-sdk-go/container/acl"
+	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
+	cidtest "github.com/nspcc-dev/neofs-sdk-go/container/id/test"
+	neofscryptotest "github.com/nspcc-dev/neofs-sdk-go/crypto/test"
+	usertest "github.com/nspcc-dev/neofs-sdk-go/user/test"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+type nopIR struct{}
+
+func (nopIR) InnerRingKeys() ([][]byte, error) {
+	return nil, nil
+}
+
+type nopFSChain struct {
+	FSChain
+}
+
+func (nopFSChain) InContainerInLastTwoEpochs(cid.ID, []byte) (bool, error) {
+	return false, nil
+}
+
+func BenchmarkClassifierLoggerProduction(b *testing.B) {
+	l, err := zap.NewProduction()
+	require.NoError(b, err)
+	require.NotEqual(b, l.Level(), zap.DebugLevel)
+
+	cnrID := cidtest.ID()
+	cnrOwner := usertest.ID()
+	reqOwner := usertest.OtherID(cnrOwner)
+	reqAuthorPub := neofscryptotest.Signer().PublicKeyBytes
+	c := senderClassifier{
+		log:       l,
+		innerRing: nopIR{},
+		fsChain:   nopFSChain{},
+	}
+
+	b.ResetTimer()
+
+	for range b.N {
+		role, err := c.classify(cnrID, cnrOwner, reqOwner, reqAuthorPub)
+		require.NoError(b, err)
+		require.Equal(b, role, acl.RoleOthers)
+	}
+}
