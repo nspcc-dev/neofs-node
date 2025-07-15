2958d297d7f69756153a470f65ab0eca9825c7bd
Andrey Butusov <andrey@nspcc.io>
2025-07-16 01:11:01 +0300

    storage: implement `GetStream` operation
    Make `GetStream` API in writecache, shard and engine. Expose `GetStream` metric.
    
    Signed-off-by: Andrey Butusov <andrey@nspcc.io>

--- pkg/local_object_storage/writecache/get_test.go
+++ pkg/local_object_storage/writecache/get_test.go
@@ -1,6 +1,7 @@
 package writecache
 
 import (
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -23,4 +24,27 @@ func TestCache_GetBytes(t *testing.T) {
 	b, err = c.GetBytes(o.addr)
 	require.NoError(t, err)
 	require.Equal(t, objBin, b)
+}
+
+func TestCache_GetStream(t *testing.T) {
+	const maxObjSize = 4 << 10
+	c, _ := newCache(t)
+
+	testStream := func(t *testing.T, size int) {
+		o := putObject(t, c, size)
+		objBin := o.obj.Payload()
+
+		header, reader, err := c.GetStream(o.addr)
+		require.NoError(t, err)
+		require.Equal(t, o.obj.CutPayload(), header)
+
+		require.NotNil(t, reader)
+		b, err := io.ReadAll(reader)
+		require.NoError(t, err)
+		require.Equal(t, objBin, b)
+		require.NoError(t, reader.Close())
+	}
+
+	testStream(t, maxObjSize/2)
+	testStream(t, 2*maxObjSize)
 }
