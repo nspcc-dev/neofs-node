3a16e06cb16d4c53c51321413020ddcd65cd8f6c
Leonard Lyubich <leonard@morphbits.io>
2025-08-14 17:27:02 +0300

    storage/metabase: Provide method to resolve EC part of the object
    Will be used to implement #3422.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

---
+++ pkg/local_object_storage/metabase/ec.go
@@ -0,0 +1,111 @@
+package meta
+
+import (
+	"bytes"
+	"fmt"
+	"slices"
+	"strconv"
+
+	iec "github.com/nspcc-dev/neofs-node/internal/ec"
+	objectcore "github.com/nspcc-dev/neofs-node/pkg/core/object"
+	apistatus "github.com/nspcc-dev/neofs-sdk-go/client/status"
+	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
+	"github.com/nspcc-dev/neofs-sdk-go/object"
+	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
+	"go.etcd.io/bbolt"
+)
+
+// ResolveECPart resolves object that carries EC part produced within cnr for
+// parent object and indexed by pi, checks its availability and returns its ID.
+//
+// If DB is disabled by mode (e.g. [DB.SetMode]), ResolveECPart returns
+// [ErrDegradedMode].
+//
+// If object has expired, ResolveECPart returns [ErrObjectIsExpired].
+//
+// If object exists but tombstoned (via [DB.Inhume] or stored tombstone object),
+// ResolveECPart returns [apistatus.ErrObjectAlreadyRemoved].
+//
+// If object is marked as garbage (via [DB.MarkGarbage]), ResolveECPart returns
+// [apistatus.ErrObjectNotFound].
+//
+// If object is locked (via [DB.Lock] or stored locker object), ResolveECPart
+// ignores expiration, tombstone and garbage marks.
+func (db *DB) ResolveECPart(cnr cid.ID, parent oid.ID, pi iec.PartInfo) (oid.ID, error) {
+	db.modeMtx.RLock()
+	defer db.modeMtx.RUnlock()
+	if db.mode.NoMetabase() {
+		return oid.ID{}, ErrDegradedMode
+	}
+
+	var res oid.ID
+	err := db.boltDB.View(func(tx *bbolt.Tx) error {
+		var err error
+		res, err = db.resolveECPartTx(tx, cnr, parent, pi)
+		return err
+	})
+	return res, err
+}
+
+func (db *DB) resolveECPartTx(tx *bbolt.Tx, cnr cid.ID, parent oid.ID, pi iec.PartInfo) (oid.ID, error) {
+	metaBkt := tx.Bucket(metaBucketKey(cnr))
+	if metaBkt == nil {
+		return oid.ID{}, apistatus.ErrObjectNotFound
+	}
+
+	metaBktCursor := metaBkt.Cursor()
+
+	id, err := db.resolveECPartInMetaBucket(metaBktCursor, parent, pi)
+	if err != nil {
+		return oid.ID{}, err
+	}
+
+	switch objectStatus(tx, metaBktCursor, oid.NewAddress(cnr, id), db.epochState.CurrentEpoch()) {
+	case statusGCMarked:
+		return oid.ID{}, apistatus.ErrObjectNotFound
+	case statusTombstoned:
+		return oid.ID{}, apistatus.ErrObjectAlreadyRemoved
+	case statusExpired:
+		return oid.ID{}, ErrObjectIsExpired
+	}
+
+	return id, nil
+}
+
+func (db *DB) resolveECPartInMetaBucket(crs *bbolt.Cursor, parent oid.ID, pi iec.PartInfo) (oid.ID, error) {
+	pref := slices.Concat([]byte{metaPrefixAttrIDPlain}, []byte(object.FilterParentID), objectcore.MetaAttributeDelimiter,
+		parent[:], objectcore.MetaAttributeDelimiter,
+	)
+
+	k, _ := crs.Seek(pref)
+	partID, ok := bytes.CutPrefix(k, pref)
+	if !ok {
+		return oid.ID{}, apistatus.ErrObjectNotFound
+	}
+	if len(partID) != oid.Size {
+		return oid.ID{}, invalidMetaBucketKeyErr(k, fmt.Errorf("wrong OID len %d", len(partID)))
+	}
+	allZeros := true
+	for i := range partID {
+		if partID[i] != 0 {
+			allZeros = false
+			break
+		}
+	}
+	if allZeros {
+		return oid.ID{}, invalidMetaBucketKeyErr(k, oid.ErrZero)
+	}
+
+	// TODO: make and reuse one buffer for all keys
+	pref = slices.Concat([]byte{metaPrefixIDAttr}, partID, []byte(iec.AttributeRuleIdx), objectcore.MetaAttributeDelimiter, []byte(strconv.Itoa(pi.RuleIndex)))
+	if k, _ = crs.Seek(pref); !bytes.Equal(k, pref) { // Cursor.Seek is more lightweight than Bucket.Get making cursor inside
+		return oid.ID{}, apistatus.ErrObjectNotFound
+	}
+
+	pref = slices.Concat([]byte{metaPrefixIDAttr}, partID, []byte(iec.AttributePartIdx), objectcore.MetaAttributeDelimiter, []byte(strconv.Itoa(pi.Index)))
+	if k, _ = crs.Seek(pref); !bytes.Equal(k, pref) {
+		return oid.ID{}, apistatus.ErrObjectNotFound
+	}
+
+	return oid.ID(partID), nil
+}
