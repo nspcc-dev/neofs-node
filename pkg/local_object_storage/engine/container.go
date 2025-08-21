d631df1077ce79dcfad55a690742171883ace80e
Pavel Karpy <carpawell@nspcc.ru>
2025-08-21 08:49:49 +0300

    engine: track container's number of object
    The same way container's size was tracked before. Required metabase adoption
    and, therefore, its version update. Refs #3460.
    
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/local_object_storage/engine/container.go
+++ pkg/local_object_storage/engine/container.go
@@ -11,10 +11,10 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-// ContainerSize returns the sum of estimation container sizes among all shards.
+// ContainerSize returns container statistics' pair: (totalStorageSize, totalObjectsNumber).
 //
 // Returns an error if executions are blocked (see BlockExecution).
-func (e *StorageEngine) ContainerSize(cnr cid.ID) (uint64, error) {
+func (e *StorageEngine) ContainerSize(cnr cid.ID) (uint64, uint64, error) {
 	if e.metrics != nil {
 		defer elapsed(e.metrics.AddEstimateContainerSizeDuration)()
 	}
@@ -23,23 +23,25 @@ func (e *StorageEngine) ContainerSize(cnr cid.ID) (uint64, error) {
 	defer e.blockMtx.RUnlock()
 
 	if e.blockErr != nil {
-		return 0, e.blockErr
+		return 0, 0, e.blockErr
 	}
 
 	var size uint64
+	var objsNumber uint64
 
 	for _, sh := range e.unsortedShards() {
-		shardSize, err := sh.Shard.ContainerSize(cnr)
+		shardInfo, err := sh.Shard.ContainerSize(cnr)
 		if err != nil {
 			e.reportShardError(sh, "can't get container size", err,
 				zap.Stringer("container_id", cnr))
 			continue
 		}
 
-		size += shardSize
+		size += shardInfo.StorageSize
+		objsNumber += shardInfo.ObjectsNumber
 	}
 
-	return size, nil
+	return size, objsNumber, nil
 }
 
 // ListContainers returns a unique container IDs presented in the engine objects.
