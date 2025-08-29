5ff44aae859d38117a4439e06898c838e10f3788
Leonard Lyubich <leonard@morphbits.io>
2025-08-29 16:06:01 +0300

    sn/object: Drop redundant `Builder` interface
    It was used only in tests with a single local implementation.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- pkg/services/object_manager/placement/netmap.go
+++ pkg/services/object_manager/placement/netmap.go
@@ -3,21 +3,8 @@ package placement
 import (
 	"slices"
 
-	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
 	netmapSDK "github.com/nspcc-dev/neofs-sdk-go/netmap"
-	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
 )
-
-// Builder is an interface of the
-// object placement vector builder.
-type Builder interface {
-	// BuildPlacement returns the list of placement vectors
-	// for object according to the placement policy.
-	//
-	// Must return all container nodes if object identifier
-	// is nil.
-	BuildPlacement(cid.ID, *oid.ID, netmapSDK.PlacementPolicy) ([][]netmapSDK.NodeInfo, error)
-}
 
 // FlattenNodes appends each row to the flat list.
 func FlattenNodes(ns [][]netmapSDK.NodeInfo) []netmapSDK.NodeInfo {
