8a05444720abc8ed916cbd4f823feddd6896251f
Andrey Butusov <andrey@nspcc.io>
2025-09-23 17:47:44 +0300

    cmderr: move printed message prefix to `ExitOnErr`
    Signed-off-by: Andrey Butusov <andrey@nspcc.io>

--- cmd/neofs-adm/main.go
+++ cmd/neofs-adm/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/nspcc-dev/neofs-node/cmd/internal/cmderr"
 	"github.com/nspcc-dev/neofs-node/cmd/neofs-adm/internal/modules"
 )
 
 func main() {
 	err := modules.Execute()
-	cmderr.ExitOnErr(fmt.Errorf("Error: %w\n", err))
+	cmderr.ExitOnErr(err)
 }
