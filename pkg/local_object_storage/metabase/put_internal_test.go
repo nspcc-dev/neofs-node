366e7687f03072da965b78ae690dd054361e18eb
Leonard Lyubich <leonard@morphbits.io>
2025-11-13 19:40:41 +0300

    storage/metabase: Index metadata for nested parents
    Previously, metabase did not index metadata for objects which are
    parents of some parents. This was causing the search of root objects
    with multi-level nesting to break. In particular, the restriction
    affected split or S3 multipart in containers with EC.
    
    Refs #3500. Refs https://github.com/nspcc-dev/neofs-s3-gw/issues/1236.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

---
+++ pkg/local_object_storage/metabase/put_internal_test.go
@@ -0,0 +1,69 @@
+package meta
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/nspcc-dev/neofs-node/internal/testutil"
+	"github.com/nspcc-dev/neofs-sdk-go/checksum"
+	cidtest "github.com/nspcc-dev/neofs-sdk-go/container/id/test"
+	"github.com/nspcc-dev/neofs-sdk-go/object"
+	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
+	oidtest "github.com/nspcc-dev/neofs-sdk-go/object/id/test"
+	usertest "github.com/nspcc-dev/neofs-sdk-go/user/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDB_Put_Nested(t *testing.T) {
+	const nestingLevelAttribute = "nesting-level"
+	cnr := cidtest.ID()
+	anyOwner := usertest.ID()
+	anyPldChecksum := checksum.NewSHA256([32]byte(testutil.RandByteSlice(32)))
+
+	db := newDB(t)
+
+	prepareObjects := func(nestingDepth int) ([]oid.ID, []object.Object) {
+		ids := oidtest.IDs(nestingDepth + 1)
+		objs := make([]object.Object, len(ids))
+		for i := range objs {
+			objs[i].SetContainerID(cnr)
+			objs[i].SetOwner(anyOwner)
+			objs[i].SetPayloadChecksum(anyPldChecksum)
+			objs[i].SetAttributes(object.NewAttribute(nestingLevelAttribute, strconv.Itoa(i)))
+			objs[i].SetID(ids[i])
+
+			if i > 0 {
+				objs[i].SetParent(&objs[i-1])
+			}
+		}
+
+		return ids, objs
+	}
+
+	t.Run("max nesting level overflow", func(t *testing.T) {
+		_, objs := prepareObjects(3)
+		require.EqualError(t, db.Put(&objs[len(objs)-1]), "max object nesting level 2 overflow")
+	})
+
+	ids, objs := prepareObjects(2)
+
+	last := len(objs) - 1
+	require.NoError(t, db.Put(&objs[last]))
+
+	for i := range ids {
+		var filters object.SearchFilters
+		assertSearchResultWithLimit(t, db, cnr, filters, []string{}, searchResultForIDs(sortObjectIDs(ids)), 1000)
+
+		filters.AddFilter(nestingLevelAttribute, strconv.Itoa(i), object.MatchStringEqual)
+		assertSearchResultWithLimit(t, db, cnr, filters, []string{}, searchResultForIDs([]oid.ID{ids[i]}), 1000)
+
+		exists, err := db.Exists(oid.NewAddress(cnr, ids[i]), false)
+		if i < last {
+			var e *object.SplitInfoError
+			require.ErrorAs(t, err, &e)
+		} else {
+			require.NoError(t, err)
+			require.True(t, exists)
+		}
+	}
+}
