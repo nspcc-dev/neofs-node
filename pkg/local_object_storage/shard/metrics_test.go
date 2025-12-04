nspcc-dev/neofs-node

commit 9a49cd098252c52400e5da4d81d4aa335f217d52

metabase: GC objects using new searchV2 index
1. Cleanup new index based only on current new index's state
2. Keep actual stored objects' size based only on new index's state
3. Delete old indexes using new indexes

Tests for counters and parent objects are based on PUT + DELETE operations, so
they are actual for the new code too. Closes #3245.

Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

pkg/local_object_storage/metabase/delete.go
@@ -2,14 +2,10 @@ package meta
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
-	"slices"
 
-	"github.com/nspcc-dev/neofs-node/pkg/core/object"
 	storagelog "github.com/nspcc-dev/neofs-node/pkg/local_object_storage/internal/log"
-	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/util/logicerr"
-	apistatus "github.com/nspcc-dev/neofs-sdk-go/client/status"
+	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
 	objectSDK "github.com/nspcc-dev/neofs-sdk-go/object"
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
 	"go.etcd.io/bbolt"
@@ -149,70 +145,12 @@ func (db *DB) delete(tx *bbolt.Tx, addr oid.Address, currEpoch uint64) (bool, bo
 		}
 	}
 
-	// unmarshal object, work only with physically stored (raw == true) objects
-	obj, err := db.get(tx, addr, key, false, true, currEpoch)
+	payloadSize, err := deleteMetadata(tx, db.log, addr.Container(), addr.Object(), false)
 	if err != nil {
-		var siErr *objectSDK.SplitInfoError
-		var notFoundErr apistatus.ObjectNotFound
-
-		if errors.As(err, &notFoundErr) || errors.As(err, &siErr) {
-			return false, false, 0, nil
-		}
-
-		if !errors.Is(err, logicerr.Error) {
-			// data corruption, it should be removed anyway
-			err = deleteAllDataByAddress(tx, addr)
-			if err != nil {
-				db.log.Warn("cleaning corrupted object failed", zap.Stringer("addr", addr), zap.Error(err))
-				return false, false, 0, nil
-			}
-		}
-
-		return false, false, 0, err
+		return false, false, 0, fmt.Errorf("can't remove metadata indexes: %w", err)
 	}
 
-	// if object is an only link to a parent, then remove parent
-	if parent := obj.Parent(); parent != nil && !parent.GetID().IsZero() {
-		err = db.deleteObject(tx, parent, true)
-		if err != nil {
-			return false, false, 0, fmt.Errorf("could not remove parent object: %w", err)
-		}
-	}
-
-	// remove object
-	err = db.deleteObject(tx, obj, false)
-	if err != nil {
-		return false, false, 0, fmt.Errorf("could not remove object: %w", err)
-	}
-
-	return true, removeAvailableObject, obj.PayloadSize(), nil
-}
-
-func (db *DB) deleteObject(
-	tx *bbolt.Tx,
-	obj *objectSDK.Object,
-	isParent bool,
-) error {
-	err := delUniqueIndexes(tx, obj, isParent)
-	if err != nil {
-		return fmt.Errorf("can't remove unique indexes")
-	}
-
-	err = updateListIndexes(tx, obj, delListIndexItem)
-	if err != nil {
-		return fmt.Errorf("can't remove list indexes: %w", err)
-	}
-
-	err = updateFKBTIndexes(tx, obj, delFKBTIndexItem)
-	if err != nil {
-		return fmt.Errorf("can't remove fake bucket tree indexes: %w", err)
-	}
-
-	if err = deleteMetadata(tx, obj.GetContainerID(), obj.GetID()); err != nil {
-		return fmt.Errorf("can't remove metadata indexes: %w", err)
-	}
-
-	return nil
+	return true, removeAvailableObject, payloadSize, nil
 }
 
 func delUniqueIndexItem(tx *bbolt.Tx, item namedBucketItem) {
@@ -222,30 +160,29 @@ func delUniqueIndexItem(tx *bbolt.Tx, item namedBucketItem) {
 	}
 }
 
-func delFKBTIndexItem(tx *bbolt.Tx, item namedBucketItem) error {
+func delFKBTIndexItem(tx *bbolt.Tx, item namedBucketItem) {
 	bkt := tx.Bucket(item.name)
 	if bkt == nil {
-		return nil
+		return
 	}
 
 	fkbtRoot := bkt.Bucket(item.key)
 	if fkbtRoot == nil {
-		return nil
+		return
 	}
 
 	_ = fkbtRoot.Delete(item.val) // ignore error, best effort there
-	return nil
 }
 
-func delListIndexItem(tx *bbolt.Tx, item namedBucketItem) error {
+func delListIndexItem(tx *bbolt.Tx, item namedBucketItem) {
 	bkt := tx.Bucket(item.name)
 	if bkt == nil {
-		return nil
+		return
 	}
 
 	lst, err := decodeList(bkt.Get(item.key))
 	if err != nil || len(lst) == 0 {
-		return nil
+		return
 	}
 
 	// remove element from the list
@@ -261,30 +198,28 @@ func delListIndexItem(tx *bbolt.Tx, item namedBucketItem) error {
 	if len(lst) == 0 {
 		_ = bkt.Delete(item.key) // ignore error, best effort there
 
-		return nil
+		return
 	}
 
 	// if list is not empty, then update it
 	encodedLst, err := encodeList(lst)
 	if err != nil {
-		return nil // ignore error, best effort there
+		return // ignore error, best effort there
 	}
 
 	_ = bkt.Put(item.key, encodedLst) // ignore error, best effort there
-	return nil
 }
 
-func delUniqueIndexes(tx *bbolt.Tx, obj *objectSDK.Object, isParent bool) error {
-	addr := object.AddressOf(obj)
+func delUniqueIndexes(tx *bbolt.Tx, cnr cid.ID, oID oid.ID, typ objectSDK.Type, isParent bool) error {
+	addr := oid.NewAddress(cnr, oID)
 
-	objKey := objectKey(addr.Object(), make([]byte, objectKeySize))
+	objKey := objectKey(oID, make([]byte, objectKeySize))
 	addrKey := addressKey(addr, make([]byte, addressKeySize))
-	cnr := addr.Container()
 	bucketName := make([]byte, bucketKeySize)
 
 	// add value to primary unique bucket
 	if !isParent {
-		switch obj.Type() {
+		switch typ {
 		case objectSDK.TypeRegular:
 			bucketName = primaryBucketName(cnr, bucketName)
 		case objectSDK.TypeTombstone:
@@ -322,42 +257,6 @@ func delUniqueIndexes(tx *bbolt.Tx, obj *objectSDK.Object, isParent bool) error 
 		name: toMoveItBucketName,
 		key:  addrKey,
 	})
-
-	return nil
-}
-
-func deleteAllDataByAddress(tx *bbolt.Tx, addr oid.Address) error {
-	cID := addr.Container()
-	oID := addr.Object()
-	objKey := objectKey(oID, make([]byte, objectKeySize))
-	addrKey := addressKey(addr, make([]byte, addressKeySize))
-	bucketName := make([]byte, bucketKeySize)
-
-	bkts := [][]byte{
-		slices.Clone(primaryBucketName(cID, bucketName)),
-		slices.Clone(tombstoneBucketName(cID, bucketName)),
-		slices.Clone(storageGroupBucketName(cID, bucketName)),
-		slices.Clone(bucketNameLockers(cID, bucketName)),
-		slices.Clone(linkObjectsBucketName(cID, bucketName)),
-		slices.Clone(parentBucketName(cID, bucketName)),
-		slices.Clone(smallBucketName(cID, bucketName)),
-		slices.Clone(rootBucketName(cID, bucketName)),
-	}
-	for _, bkt := range bkts {
-		delUniqueIndexItem(tx, namedBucketItem{
-			name: bkt,
-			key:  objKey,
-		})
-	}
-	delUniqueIndexItem(tx, namedBucketItem{
-		name: toMoveItBucketName,
-		key:  addrKey,
-	})
-
-	err := deleteMetadata(tx, cID, oID)
-	if err != nil {
-		return fmt.Errorf("metadata removal: %w", err)
-	}
 
 	return nil
 }

pkg/local_object_storage/metabase/metadata.go
@@ -13,8 +13,10 @@ import (
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
 	"github.com/nspcc-dev/neofs-sdk-go/object"
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
+	"github.com/nspcc-dev/neofs-sdk-go/user"
 	"github.com/nspcc-dev/neofs-sdk-go/version"
 	"go.etcd.io/bbolt"
+	"go.uber.org/zap"
 )
 
 const (
@@ -131,14 +133,24 @@ func PutMetadataForObject(tx *bbolt.Tx, hdr object.Object, hasParent, phy bool) 
 	return nil
 }
 
-func deleteMetadata(tx *bbolt.Tx, cnr cid.ID, id oid.ID) error {
+func deleteMetadata(tx *bbolt.Tx, l *zap.Logger, cnr cid.ID, id oid.ID, isParent bool) (uint64, error) {
 	metaBkt := tx.Bucket(metaBucketKey(cnr))
 	if metaBkt == nil {
-		return nil
+		return 0, nil
 	}
+	var err error
+	var parent oid.ID
+	var size uint64
+	if !isParent {
+		v := metaBkt.Get(slices.Concat([]byte{metaPrefixIDAttr}, id[:], []byte(object.FilterPhysical), objectcore.MetaAttributeDelimiter, []byte(binPropMarker)))
+		if v == nil {
+			return 0, nil
+		}
+	}
+	bktKeyBuff := make([]byte, bucketKeySize)
 	pref := slices.Concat([]byte{metaPrefixID}, id[:])
 	if err := metaBkt.Delete(pref); err != nil {
-		return err
+		return 0, err
 	}
 	// removed keys must be pre-collected according to BoltDB docs.
 	var ks [][]byte
@@ -147,7 +159,75 @@ func deleteMetadata(tx *bbolt.Tx, cnr cid.ID, id oid.ID) error {
 	for kIDAttr, _ := c.Seek(pref); bytes.HasPrefix(kIDAttr, pref); kIDAttr, _ = c.Next() {
 		sepInd := bytes.LastIndex(kIDAttr, objectcore.MetaAttributeDelimiter)
 		if sepInd < 1+oid.Size {
-			return fmt.Errorf("invalid key with prefix 0x%X in meta bucket: missing delimiter", kIDAttr[0])
+			return 0, fmt.Errorf("invalid key with prefix 0x%X in meta bucket: missing delimiter", kIDAttr[0])
+		}
+		attrK := kIDAttr[1+oid.Size : sepInd]
+		attrV := kIDAttr[sepInd+attributeDelimiterLen:]
+		switch kStr := string(attrK); kStr {
+		case object.FilterType:
+			var typ object.Type
+			if !isParent && typ.DecodeString(string(attrV)) {
+				err := delUniqueIndexes(tx, cnr, id, typ, isParent)
+				if err != nil {
+					return 0, err
+				}
+			}
+		case object.FilterOwnerID:
+			if len(attrV) == user.IDSize {
+				owner := user.ID(attrV)
+				delFKBTIndexItem(tx, namedBucketItem{
+					name: ownerBucketName(cnr, bktKeyBuff),
+					key:  []byte(owner.EncodeToString()),
+					val:  id[:],
+				})
+			} else {
+				l.Warn("unexpected object's owner index",
+					zap.Stringer("addr", oid.NewAddress(cnr, id)),
+					zap.Int("len", len(attrV)),
+				)
+			}
+		case object.FilterPayloadChecksum:
+			delListIndexItem(tx, namedBucketItem{
+				name: payloadHashBucketName(cnr, bktKeyBuff),
+				key:  attrV,
+				val:  id[:],
+			})
+		case object.FilterParentID:
+			if len(attrV) == oid.Size {
+				parent = oid.ID(attrV)
+			}
+			delUniqueIndexItem(tx, namedBucketItem{
+				name: parentBucketName(cnr, bktKeyBuff),
+				key:  attrV,
+			})
+			delListIndexItem(tx, namedBucketItem{
+				name: parentBucketName(cnr, bktKeyBuff),
+				key:  attrV,
+				val:  id[:],
+			})
+		case object.FilterSplitID:
+			delListIndexItem(tx, namedBucketItem{
+				name: splitBucketName(cnr, bktKeyBuff),
+				key:  attrV,
+				val:  id[:],
+			})
+		case object.FilterFirstSplitObject:
+			delListIndexItem(tx, namedBucketItem{
+				name: firstObjectIDBucketName(cnr, bktKeyBuff),
+				key:  attrV,
+				val:  id[:],
+			})
+		case object.FilterPayloadSize:
+			size, _ = strconv.ParseUint(string(attrV), 10, 64)
+		default:
+			const systemFieldPrefix = "$Object:"
+			if !bytes.HasPrefix(attrK, []byte(systemFieldPrefix)) {
+				delFKBTIndexItem(tx, namedBucketItem{
+					name: attributeBucketName(cnr, kStr, bktKeyBuff),
+					key:  attrV,
+					val:  id[:],
+				})
+			}
 		}
 		kAttrID := make([]byte, len(kIDAttr)+attributeDelimiterLen)
 		kAttrID[0] = metaPrefixAttrIDPlain
@@ -167,10 +247,21 @@ func deleteMetadata(tx *bbolt.Tx, cnr cid.ID, id oid.ID) error {
 	}
 	for i := range ks {
 		if err := metaBkt.Delete(ks[i]); err != nil {
-			return err
+			return 0, err
 		}
 	}
-	return nil
+
+	if !parent.IsZero() {
+		_, err = deleteMetadata(tx, l, cnr, parent, true)
+		if err != nil {
+			l.Warn("parent removal",
+				zap.Stringer("child", oid.NewAddress(cnr, id)),
+				zap.Stringer("parent", oid.NewAddress(cnr, parent)),
+				zap.Error(err))
+		}
+	}
+
+	return size, nil
 }
 
 // Search selects up to count container's objects from the given container

commit f27ca9fab8043fd55f42a6dbad1120f531551423

storage/metabase: Fix `Delete()` of parent objects
Regression from 9a49cd098252c52400e5da4d81d4aa335f217d52. Before it,
deletion code strongly relied that parent objects go no-op. Then it
started to treat parent objects as physically stored ones. This led to
incorrect metric tracking. In particular, shard counters.

This was reproduced in DevEnv: when split objects expired, metrics
became:
```
neofs_node_object_counter{shard="6pmbXUjzhKuvYnzrkUbeuP",type="logic"} -9
neofs_node_object_counter{shard="6pmbXUjzhKuvYnzrkUbeuP",type="phy"} -9
neofs_node_object_counter{shard="LpCtgJvdezqupmWtbM71rJ",type="logic"} -10
neofs_node_object_counter{shard="LpCtgJvdezqupmWtbM71rJ",type="phy"} -10
neofs_node_engine_payload_size{shard="6pmbXUjzhKuvYnzrkUbeuP"} -1.048743e+06
neofs_node_engine_payload_size{shard="LpCtgJvdezqupmWtbM71rJ"} -1167
```

Refs #3477.

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

pkg/local_object_storage/metabase/delete.go
@@ -2,10 +2,13 @@ package meta
 
 import (
 	"fmt"
+	"slices"
 
 	"github.com/nspcc-dev/bbolt"
+	objectcore "github.com/nspcc-dev/neofs-node/pkg/core/object"
 	storagelog "github.com/nspcc-dev/neofs-node/pkg/local_object_storage/internal/log"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
+	"github.com/nspcc-dev/neofs-sdk-go/object"
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
 	"go.uber.org/zap"
 )
@@ -127,12 +130,18 @@ func (db *DB) deleteGroup(tx *bbolt.Tx, addrs []oid.Address, sizes []uint64) (ui
 func (db *DB) delete(tx *bbolt.Tx, addr oid.Address) (bool, bool, uint64, error) {
 	key := make([]byte, addressKeySize)
 	cID := addr.Container()
+	oID := addr.Object()
 	addrKey := addressKey(addr, key)
 	garbageObjectsBKT := tx.Bucket(garbageObjectsBucketName)
 	graveyardBKT := tx.Bucket(graveyardBucketName)
 	metaBucket := tx.Bucket(metaBucketKey(cID))
 	var metaCursor *bbolt.Cursor
 	if metaBucket != nil {
+		phyKey := slices.Concat([]byte{metaPrefixIDAttr}, oID[:], []byte(object.FilterPhysical), objectcore.MetaAttributeDelimiter, []byte(binPropMarker))
+		if metaBucket.Get(phyKey) == nil {
+			return false, false, 0, nil
+		}
+
 		metaCursor = metaBucket.Cursor()
 	}
 
@@ -146,7 +155,7 @@ func (db *DB) delete(tx *bbolt.Tx, addr oid.Address) (bool, bool, uint64, error)
 		}
 	}
 
-	payloadSize, err := deleteMetadata(tx, db.log, addr.Container(), addr.Object(), false)
+	payloadSize, err := deleteMetadata(tx, db.log, addr.Container(), oID, false)
 	if err != nil {
 		return false, false, 0, fmt.Errorf("can't remove metadata indexes: %w", err)
 	}

pkg/local_object_storage/metabase/metadata.go
@@ -141,12 +141,6 @@ func deleteMetadata(tx *bbolt.Tx, l *zap.Logger, cnr cid.ID, id oid.ID, isParent
 	var err error
 	var parent oid.ID
 	var size uint64
-	if !isParent {
-		v := metaBkt.Get(slices.Concat([]byte{metaPrefixIDAttr}, id[:], []byte(object.FilterPhysical), objectcore.MetaAttributeDelimiter, []byte(binPropMarker)))
-		if v == nil {
-			return 0, nil
-		}
-	}
 	pref := slices.Concat([]byte{metaPrefixID}, id[:])
 	if err := metaBkt.Delete(pref); err != nil {
 		return 0, err

pkg/local_object_storage/metabase/delete_test.go
@@ -31,6 +31,13 @@ func TestDB_Delete(t *testing.T) {
 	err := putBig(db, child)
 	require.NoError(t, err)
 
+	// try to delete parent
+	res, err := db.Delete([]oid.Address{object.AddressOf(parent)})
+	require.NoError(t, err)
+	require.Zero(t, res.RawRemoved)
+	require.Zero(t, res.AvailableRemoved)
+	require.Equal(t, []uint64{0}, res.Sizes)
+
 	// fill ToMoveIt index
 	err = metaToMoveIt(db, object.AddressOf(child))
 	require.NoError(t, err)

pkg/local_object_storage/shard/metrics_test.go
@@ -176,6 +176,40 @@ func TestCounters(t *testing.T) {
 		}
 		require.Equal(t, expectedSizes, mm.containerSize)
 		require.Equal(t, totalPayload-int64(totalRemovedpayload), mm.payloadSize)
+
+		t.Run("parent", func(t *testing.T) {
+			cnr := cidtest.ID()
+
+			parent := generateObjectWithCID(cnr)
+
+			child := generateObjectWithCID(cnr)
+			child.SetParent(parent)
+
+			require.NoError(t, sh.Put(child, nil))
+
+			require.EqualValues(t, child.PayloadSize(), mm.containerSize[cnr])
+
+			cnrInfo, err := sh.ContainerInfo(cnr)
+			require.NoError(t, err)
+			require.EqualValues(t, child.PayloadSize(), cnrInfo.StorageSize)
+			require.EqualValues(t, 1, cnrInfo.ObjectsNumber)
+
+			phyBefore := mm.objectCounters[physical]
+			logicNumBefore := mm.objectCounters[logical]
+			sumPldSizeBefore := mm.payloadSize
+
+			require.NoError(t, sh.Delete([]oid.Address{objectcore.AddressOf(parent)}))
+
+			require.EqualValues(t, child.PayloadSize(), mm.containerSize[cnr])
+			require.Equal(t, phyBefore, mm.objectCounters[physical])
+			require.Equal(t, logicNumBefore, mm.objectCounters[logical])
+			require.Equal(t, sumPldSizeBefore, mm.payloadSize)
+
+			cnrInfo, err = sh.ContainerInfo(cnr)
+			require.NoError(t, err)
+			require.EqualValues(t, child.PayloadSize(), cnrInfo.StorageSize)
+			require.EqualValues(t, 1, cnrInfo.ObjectsNumber)
+		})
 	})
 }
 