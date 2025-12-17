ec40563663ea96ceb0423ffed1f72b66f3139552
Roman Khimov <roman@nspcc.ru>
2025-12-17 11:09:49 +0300

    engine: collect child objects without search
    In case LINK is missing we're using search to find objects that need to be
    deleted, but this can work incorrectly for expired children since search
    never returns outdated objects. I believe this fixes #3715, that's the
    scenario (and log) that can be seen in UTs and it leads to the last object
    part being stuck in the GC loop: parent object should be deleted, but
    searching doesn't return an outdated object.
    
    The new API is also somewhat more efficient.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- pkg/local_object_storage/engine/inhume.go
+++ pkg/local_object_storage/engine/inhume.go
@@ -1,15 +1,12 @@
 package engine
 
 import (
-	"encoding/base64"
 	"errors"
 
 	iec "github.com/nspcc-dev/neofs-node/internal/ec"
-	objectcore "github.com/nspcc-dev/neofs-node/pkg/core/object"
 	meta "github.com/nspcc-dev/neofs-node/pkg/local_object_storage/metabase"
 	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/shard"
 	"github.com/nspcc-dev/neofs-node/pkg/local_object_storage/util/logicerr"
-	"github.com/nspcc-dev/neofs-sdk-go/client"
 	apistatus "github.com/nspcc-dev/neofs-sdk-go/client/status"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
 	objectSDK "github.com/nspcc-dev/neofs-sdk-go/object"
@@ -230,59 +227,28 @@ func (e *StorageEngine) collectChildrenWithoutLink(addr oid.Address, si *objectS
 	e.log.Info("root object has no link object in split upload",
 		zap.Stringer("addrBeingInhumed", addr))
 
-	var (
-		children  []oid.Address
-		fs        objectSDK.SearchFilters
-		newCursor []byte
-		res       []client.SearchResultItem
-		cnr       = addr.Container()
-		tmpAddr   oid.Address
-	)
-	tmpAddr.SetContainer(cnr)
-
 	firstID := si.GetFirstPart()
 	splitID := si.SplitID()
 	switch {
 	case !firstID.IsZero():
-		fs.AddFirstSplitObjectFilter(objectSDK.MatchStringEqual, firstID)
-		tmpAddr.SetObject(firstID)
-		children = append(children, tmpAddr)
+		res, err := e.collectRawWithAttribute(addr.Container(), objectSDK.FilterFirstSplitObject, firstID[:])
+		if err == nil {
+			res = append(res, oid.NewAddress(addr.Container(), firstID))
+			return res
+		}
+		e.log.Warn("failed to collect objects with first ID", zap.Stringer("addrBeingInhumed", addr), zap.Error(err))
 	case splitID != nil:
-		fs.AddSplitIDFilter(objectSDK.MatchStringEqual, *splitID)
+		res, err := e.collectRawWithAttribute(addr.Container(), objectSDK.FilterSplitID, splitID.ToV2())
+		if err == nil {
+			return res
+		}
+		e.log.Warn("failed to collect objects with split ID", zap.Stringer("addrBeingInhumed", addr), zap.Error(err))
 	default:
 		e.log.Warn("no first ID and split ID found in split",
 			zap.Stringer("addrBeingInhumed", addr))
-
-		return nil
 	}
 
-	for {
-		fss, cursor, err := objectcore.PreprocessSearchQuery(fs, nil, base64.StdEncoding.EncodeToString(newCursor))
-		if err != nil {
-			e.log.Error("cannot preprocess search query for split-chain",
-				zap.Stringer("addrBeingInhumed", addr),
-				zap.Error(err))
-			break
-		}
-		res, newCursor, err = e.Search(cnr, fss, nil, cursor, uint16(1000))
-		if err != nil {
-			e.log.Error("cannot search for children in split-chain",
-				zap.String("searchBy", fss[0].Value()),
-				zap.Stringer("addrBeingInhumed", addr),
-				zap.Error(err))
-			break
-		}
-
-		for _, item := range res {
-			tmpAddr.SetObject(item.ID)
-			children = append(children, tmpAddr)
-		}
-		if newCursor == nil {
-			break
-		}
-	}
-
-	return children
+	return nil
 }
 
 // IsLocked checks whether an object is locked according to StorageEngine's state.
--- pkg/local_object_storage/engine/select.go
+++ pkg/local_object_storage/engine/select.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 
@@ -107,4 +108,80 @@ func (e *StorageEngine) Search(cnr cid.ID, fs []objectcore.SearchFilter, attrs [
 	// skip cursor calculation and win a bit. At the same time, this would require
 	// merging logic complication, so for now we just always do it.
 	return res[:count], c, nil
+}
+
+func (e *StorageEngine) collectRawWithAttribute(cnr cid.ID, attr string, val []byte) ([]oid.Address, error) {
+	var (
+		err    error
+		shards = e.unsortedShards()
+		ids    = make([][]oid.ID, len(shards))
+	)
+
+	for i, sh := range shards {
+		ids[i], err = sh.CollectRawWithAttribute(cnr, attr, val)
+		if err != nil {
+			return nil, fmt.Errorf("shard %s: %w", sh.ID(), err)
+		}
+	}
+	return mergeOIDs(cnr, ids), nil
+}
+
+// mergeOIDs merges given set of lists of object IDs into a single flat
+// list of addresses in the same given container. ids are expected to be
+// sorted and the result contains no duplicates from different original
+// list (lists are expected to not contain any inner duplicates).
+func mergeOIDs(cnr cid.ID, ids [][]oid.ID) []oid.Address {
+	var numOfRes int
+
+	for i := range ids {
+		numOfRes += len(ids[i])
+	}
+
+	if numOfRes == 0 {
+		return nil
+	}
+
+	var (
+		idx = make([]int, len(ids))
+		res = make([]oid.Address, 0, numOfRes)
+	)
+
+	var haveUnhandledID = func() bool {
+		for i := range ids {
+			if idx[i] < len(ids[i]) {
+				return true
+			}
+		}
+		return false
+	}
+
+	// ids are naturally sorted by ID, rely on that.
+	for haveUnhandledID() {
+		var (
+			minIdx = -1
+			minID  oid.ID
+		)
+
+		for i := range ids {
+			if idx[i] < len(ids[i]) {
+				var id = ids[i][idx[i]]
+				if minIdx == -1 {
+					minIdx = i
+					minID = id
+					continue
+				}
+				switch bytes.Compare(minID[:], id[:]) {
+				case 0:
+					idx[i]++ // Skip duplicating value.
+				case 1:
+					minIdx = i
+					minID = id
+				}
+			}
+		}
+		res = append(res, oid.NewAddress(cnr, minID))
+		idx[minIdx]++
+	}
+
+	return res
 }
