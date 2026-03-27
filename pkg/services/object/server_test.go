nspcc-dev/neofs-node

commit a70cb4d51b5dd92be38091d57cd57ba2c7244d67

sn/object: Do not unmarshal local object header and chunks in GET
Similar to 637a614d92aa7babc161b72b486758d03f9e2dc1.

Refs #3783.

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

pkg/services/object/server.go
@@ -1160,11 +1160,143 @@ func (s *Server) Get(req *protoobject.GetRequest, gStream protoobject.ObjectServ
 		}
 		return s.sendStatusGetResponse(gStream, err, needSignResp)
 	}
+
+	// TODO: consider optimization
+	// We could acquire ~256K buffer (like for chunks) if storage would try to read it full.
+	// Then small objects would fit into a single buffer, and for large ones it'd be possible to
+	// encode the first chunk response using the heading buffer.
+	hdrRespBuf, hdrBuf := getBufferForHeadResponse()
+	defer hdrRespBuf.Free()
+
+	hdrLen := -1
+	var stream io.ReadCloser
+	defer func() {
+		if stream != nil {
+			stream.Close()
+		}
+	}()
+
+	p.WithBuffer(hdrBuf, func(ln int, s io.ReadCloser) { hdrLen, stream = ln, s })
+
 	err = s.handlers.Get(gStream.Context(), p)
 	if err != nil {
 		return s.sendStatusGetResponse(gStream, err, needSignResp)
 	}
+
+	if hdrLen < 0 {
+		return nil
+	}
+
+	idf, sigf, hdrf, err := iobject.GetNonPayloadFieldBounds(hdrBuf[:hdrLen])
+	if err != nil {
+		return s.sendStatusGetResponse(gStream, err, needSignResp)
+	}
+
+	if recheckEACL { // previous check didn't match, but we have a header now.
+		err = s.aclChecker.CheckEACL(hdrBuf[hdrf.ValueFrom:hdrf.To], reqInfo)
+		if err != nil && !errors.Is(err, aclsvc.ErrNotMatched) { // Not matched -> follow basic ACL.
+			err = eACLErr(reqInfo, err) // defer
+			return s.sendStatusGetResponse(gStream, err, needSignResp)
+		}
+	}
+
+	pldFldOff := max(idf.To, sigf.To, hdrf.To)
+
+	err = s.copyGetStream(gStream, hdrRespBuf, hdrBuf, hdrLen, stream, pldFldOff, needSignResp) // defer
+	if err != nil {
+		return s.sendStatusGetResponse(gStream, err, needSignResp)
+	}
+
 	return nil
+}
+
+func (s *Server) copyGetStream(gStream protoobject.ObjectService_GetServer, hdrRespBuf *iprotobuf.MemBuffer, hdrBuf []byte,
+	hdrLen int, stream io.Reader, pldFldOff int, needSignResp bool) error {
+	var chunkRespBuf *iprotobuf.MemBuffer
+	var chunkBuf []byte
+
+	prereadPldLen := hdrLen - pldFldOff
+	if prereadPldLen > 0 {
+		chunkRespBuf, chunkBuf = getBufferForChunkGetResponse()
+		copy(chunkBuf, hdrBuf[pldFldOff:][:prereadPldLen])
+		// TODO: consider optimization
+		// Object can be small and fit entirely within the heading buffer. In this
+		// case, no more buffers are needed. This can be checked by reading
+		// `header.payload_length` field.
+	}
+
+	bodyf := shiftHeaderInGetResponseBuffer(hdrRespBuf.SliceBuffer, hdrBuf[:pldFldOff])
+
+	if needSignResp {
+		n, err := s.signResponse(hdrRespBuf.SliceBuffer[bodyf.To:], hdrRespBuf.SliceBuffer[bodyf.ValueFrom:bodyf.To], nil)
+		if err != nil {
+			if chunkRespBuf != nil {
+				chunkRespBuf.Free()
+			}
+			return fmt.Errorf("sign head response: %w", err)
+		}
+		bodyf.To += n
+	}
+
+	hdrRespBuf.SetBounds(bodyf.From, bodyf.To)
+	hdrRespBuf.Ref() // because Free() is defered
+	// Note that finished SendMsg() does not guarantee that the buffer is free.
+	// Moreover, this is not guaranteed even by returning from the current function.
+	// Therefore, buffer release has to be delegated to gRPC layer.
+	// For the same reason, reusing a single buffer for multiple messages is unsafe.
+	if err := gStream.SendMsg(hdrRespBuf); err != nil {
+		if chunkRespBuf != nil {
+			chunkRespBuf.Free()
+		}
+		return err
+	}
+
+	if chunkRespBuf == nil {
+		chunkRespBuf, chunkBuf = getBufferForChunkGetResponse()
+	}
+
+	for first := true; ; first = false {
+		n, err := io.ReadFull(stream, chunkBuf[prereadPldLen:])
+		streamDone := errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF)
+		if err != nil && !streamDone {
+			chunkRespBuf.Free()
+			return fmt.Errorf("read payload stream: %w", err)
+		}
+
+		n += prereadPldLen
+
+		if first && n > 0 {
+			prereadPldLen, err = parseObjectPayloadFieldTag(chunkBuf[:n])
+			if err != nil {
+				chunkRespBuf.Free()
+				return fmt.Errorf("parse payload field tag: %w", err)
+			}
+
+			bodyf = shiftPayloadChunkInGetResponseBuffer(chunkRespBuf.SliceBuffer, maxChunkOffsetInGetResponse+prereadPldLen, n-prereadPldLen)
+			prereadPldLen = 0
+		} else if n == 0 {
+			chunkRespBuf.Free()
+			return nil
+		} else {
+			bodyf = shiftPayloadChunkInGetResponseBuffer(chunkRespBuf.SliceBuffer, maxChunkOffsetInGetResponse, n)
+		}
+
+		if needSignResp {
+			n, err = s.signResponse(chunkRespBuf.SliceBuffer[bodyf.To:], chunkRespBuf.SliceBuffer[bodyf.ValueFrom:bodyf.To], nil)
+			if err != nil {
+				chunkRespBuf.Free()
+				return fmt.Errorf("sign chunk response: %w", err)
+			}
+			bodyf.To += n
+		}
+
+		chunkRespBuf.SetBounds(bodyf.From, bodyf.To)
+		if err = gStream.SendMsg(chunkRespBuf); err != nil || streamDone {
+			return err
+		}
+
+		chunkRespBuf, chunkBuf = getBufferForChunkGetResponse()
+	}
 }
 
 // converts original request into parameters accepted by the internal handler.

pkg/services/object/get_test.go
@@ -1,0 +1,246 @@
+package object_test
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io"
+	"net"
+	"slices"
+	"strconv"
+	"testing"
+
+	iprotobuf "github.com/nspcc-dev/neofs-node/internal/protobuf"
+	"github.com/nspcc-dev/neofs-node/internal/testutil"
+	. "github.com/nspcc-dev/neofs-node/pkg/services/object"
+	getsvc "github.com/nspcc-dev/neofs-node/pkg/services/object/get"
+	cidtest "github.com/nspcc-dev/neofs-sdk-go/container/id/test"
+	neofscrypto "github.com/nspcc-dev/neofs-sdk-go/crypto"
+	"github.com/nspcc-dev/neofs-sdk-go/netmap"
+	"github.com/nspcc-dev/neofs-sdk-go/object"
+	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
+	protoobject "github.com/nspcc-dev/neofs-sdk-go/proto/object"
+	protosession "github.com/nspcc-dev/neofs-sdk-go/proto/session"
+	usertest "github.com/nspcc-dev/neofs-sdk-go/user/test"
+	"github.com/nspcc-dev/neofs-sdk-go/version"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/test/bufconn"
+	"google.golang.org/protobuf/encoding/protowire"
+)
+
+func TestServer_Get_Local(t *testing.T) {
+	signer := usertest.User()
+	cnr := cidtest.ID()
+	var fsChain nopFSChain
+	var mtrc nopMetrics
+	var aclChecker nopACLChecker
+	var reqInfoExt nopReqInfoExtractor
+
+	storage := newSimpleStorage(t, fsChain)
+
+	handler := getsvc.New(mockHandlerFSChain{
+		repRules:  []uint{3},               // any non-empty
+		nodeLists: [][]netmap.NodeInfo{{}}, // any non-empty
+	},
+		getsvc.WithLocalStorageEngine(storage),
+	)
+	handlers := &getOnlyHandler{svc: handler}
+
+	srv := New(handlers, 0, nil, fsChain, nil, nil, signer.ECDSAPrivateKey, mtrc, aclChecker, reqInfoExt, nil)
+
+	for _, pldLen := range []uint64{
+		0, 1,
+		4 << 10, 100 << 10, 256 << 10,
+		4 << 20, 10 << 20,
+	} {
+		t.Run("payload_len="+strconv.FormatUint(pldLen, 10), func(t *testing.T) {
+			obj := object.New(cnr, signer.ID)
+			obj.SetPayloadSize(pldLen)
+			obj.SetPayload(testutil.RandByteSlice(pldLen))
+			require.NoError(t, obj.SetVerificationFields(signer))
+
+			require.NoError(t, storage.Put(obj, nil))
+
+			assertGetOK(t, srv, *obj, signer)
+
+			t.Run("cut payload in header", func(t *testing.T) {
+				handlers.mockObject = obj.Marshal()
+
+				handlers.mockHeaderLen = len(obj.CutPayload().Marshal())
+
+				t.Run("no payload", func(t *testing.T) {
+					assertGetOK(t, srv, *obj, signer)
+				})
+
+				if pldLen == 0 {
+					return
+				}
+
+				t.Run("no payload bytes", func(t *testing.T) {
+					tagLen := 1 + protowire.SizeVarint(pldLen)
+					for range tagLen {
+						handlers.mockHeaderLen++
+					}
+					assertGetOK(t, srv, *obj, signer)
+				})
+
+				handlers.mockHeaderLen = len(handlers.mockObject) - 1
+				assertGetOK(t, srv, *obj, signer)
+			})
+
+			handlers.mockObject = nil
+		})
+	}
+}
+
+type getOnlyHandler struct {
+	noCallObjectService
+	svc *getsvc.Service
+
+	mockObject    []byte
+	mockHeaderLen int
+}
+
+func (x getOnlyHandler) Get(ctx context.Context, prm getsvc.Prm) error {
+	if hdrBuf, submitStreamFn := prm.GetBuffer(); hdrBuf != nil && x.mockObject != nil {
+		hdrLen := min(x.mockHeaderLen, len(hdrBuf))
+		copy(hdrBuf, x.mockObject[:hdrLen])
+		submitStreamFn(hdrLen, io.NopCloser(bytes.NewReader(x.mockObject[hdrLen:])))
+		return nil
+	}
+
+	return x.svc.Get(ctx, prm)
+}
+
+func newLocalGetRequest(t *testing.T, ver version.Version, addr oid.Address, signer neofscrypto.Signer) *protoobject.GetRequest {
+	req := &protoobject.GetRequest{
+		Body: &protoobject.GetRequest_Body{
+			Address: addr.ProtoMessage(),
+		},
+		MetaHeader: &protosession.RequestMetaHeader{
+			Version: ver.ProtoMessage(),
+			Ttl:     1,
+		},
+	}
+
+	var err error
+	req.VerifyHeader, err = neofscrypto.SignRequestWithBuffer(signer, req, nil)
+	require.NoError(t, err)
+
+	return req
+}
+
+func callGet(t *testing.T, srv *Server, req *protoobject.GetRequest) (grpc.ServerStreamingClient[protoobject.GetResponse], error) {
+	// simulating a full gRPC request lifecycle starting from the client
+	lis := bufconn.Listen(32 << 10)
+
+	grpcSrv := grpc.NewServer(
+		grpc.ForceServerCodecV2(iprotobuf.BufferedCodec{}),
+	)
+	t.Cleanup(grpcSrv.Stop)
+
+	protoobject.RegisterObjectServiceServer(grpcSrv, srv)
+
+	go func() { _ = grpcSrv.Serve(lis) }()
+
+	c, err := grpc.NewClient("localhost:8080",
+		grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) { return lis.DialContext(ctx) }),
+		grpc.WithTransportCredentials(insecure.NewCredentials()), // error otherwise
+	)
+	require.NoError(t, err) // lib misuse, not a request error
+
+	return protoobject.NewObjectServiceClient(c).Get(context.Background(), req)
+}
+
+func assertGetOK(t *testing.T, srv *Server, obj object.Object, signer neofscrypto.Signer) {
+	t.Run("signed responses", func(t *testing.T) {
+		resps := assertGetOKVersioned(t, srv, obj, signer, version.New(2, 17))
+		for _, resp := range resps {
+			require.NotNil(t, resp.VerifyHeader)
+			require.NoError(t, neofscrypto.VerifyResponseWithBuffer(resp, nil))
+		}
+	})
+
+	resps := assertGetOKVersioned(t, srv, obj, signer, version.Current())
+	require.False(t, slices.ContainsFunc(resps, func(resp *protoobject.GetResponse) bool { return resp.VerifyHeader != nil }))
+}
+
+func assertGetOKVersioned(t *testing.T, srv *Server, obj object.Object, signer neofscrypto.Signer, ver version.Version) []*protoobject.GetResponse {
+	req := newLocalGetRequest(t, ver, obj.Address(), signer)
+
+	stream, err := callGet(t, srv, req)
+	require.NoError(t, err)
+
+	var resps []*protoobject.GetResponse
+	for {
+		resp, err := stream.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			require.NoError(t, err)
+		}
+
+		resps = append(resps, resp)
+	}
+
+	assertGetStreamResponses(t, obj, resps)
+
+	return resps
+}
+
+func assertGetStreamResponses(t *testing.T, obj object.Object, resps []*protoobject.GetResponse) {
+	require.NotEmpty(t, resps)
+
+	headResp := resps[0]
+	require.NotNil(t, headResp)
+	require.Nil(t, headResp.MetaHeader)
+	require.NotNil(t, headResp.Body)
+	require.IsType(t, (*protoobject.GetResponse_Body_Init_)(nil), headResp.Body.ObjectPart)
+	in := headResp.Body.ObjectPart.(*protoobject.GetResponse_Body_Init_)
+	require.NotNil(t, in)
+	require.NotNil(t, in.Init)
+	require.NotNil(t, in.Init.ObjectId)
+	require.NotNil(t, in.Init.Signature)
+	require.NotNil(t, in.Init.Header)
+
+	var restoredHdr object.Object
+	require.NoError(t, restoredHdr.FromProtoMessage(&protoobject.Object{
+		ObjectId:  in.Init.ObjectId,
+		Signature: in.Init.Signature,
+		Header:    in.Init.Header,
+	}))
+	require.Equal(t, *obj.CutPayload(), restoredHdr)
+
+	var chunks [][]byte
+	for _, chunkResp := range resps[1:] {
+		require.NotNil(t, chunkResp)
+		require.Nil(t, chunkResp.MetaHeader)
+		require.NotNil(t, chunkResp.Body)
+		require.IsType(t, (*protoobject.GetResponse_Body_Chunk)(nil), chunkResp.Body.ObjectPart)
+		chunks = append(chunks, chunkResp.Body.ObjectPart.(*protoobject.GetResponse_Body_Chunk).Chunk)
+	}
+
+	payloadLen := len(obj.Payload())
+	if payloadLen == 0 {
+		require.Empty(t, chunks)
+		return
+	}
+
+	const maxChunkLen = 256 << 10
+	for i := range chunks[:len(chunks)-1] {
+		if i == 0 {
+			require.EqualValues(t, maxChunkLen-1-protowire.SizeVarint(uint64(payloadLen)), len(chunks[i]))
+		} else {
+			require.EqualValues(t, maxChunkLen, len(chunks[i]))
+		}
+	}
+	require.LessOrEqual(t, len(chunks[len(chunks)-1]), maxChunkLen)
+
+	gotPayload := slices.Concat(chunks...)
+	// bytes.Equal checks len equality, but it's more convenient to distinguish this case
+	require.EqualValues(t, len(obj.Payload()), len(gotPayload))
+	require.True(t, bytes.Equal(obj.Payload(), gotPayload))
+}

pkg/services/object/server_test.go
@@ -693,13 +693,17 @@ func BenchmarkServer_Replicate(b *testing.B) {
 	}
 }
 
-type nopHandlerFSChain struct{}
-
-func (nopHandlerFSChain) GetNodesForObject(oid.Address) ([][]netmap.NodeInfo, []uint, []iec.Rule, error) {
-	return nil, nil, nil, nil
+type mockHandlerFSChain struct {
+	nodeLists [][]netmap.NodeInfo
+	repRules  []uint
+	ecRules   []iec.Rule
 }
 
-func (nopHandlerFSChain) IsLocalNodePublicKey([]byte) bool {
+func (x mockHandlerFSChain) GetNodesForObject(oid.Address) ([][]netmap.NodeInfo, []uint, []iec.Rule, error) {
+	return x.nodeLists, x.repRules, x.ecRules, nil
+}
+
+func (mockHandlerFSChain) IsLocalNodePublicKey([]byte) bool {
 	return false
 }
 

internal/protobuf/buffers.go
@@ -17,16 +17,21 @@ type MemBuffer struct {
 	pool *sync.Pool
 }
 
+// SetBounds seals underlying message with given bounds.
+func (x *MemBuffer) SetBounds(from, to int) {
+	x.from = from
+	x.to = to
+}
+
 // Finalize seals underlying message with given bounds and increments ref
 // counter for x. Finalize is intended to be called before.
 func (x *MemBuffer) Finalize(from, to int) {
-	x.from = from
-	x.to = to
+	x.SetBounds(from, to)
 	x.Ref()
 }
 
 // Len returns final length of the underlying message. Len returns undefined
-// value before [MemBuffer.Finalize].
+// value before [MemBuffer.SetBounds]/[MemBuffer.Finalize].
 //
 // Len implements [mem.Buffer].
 func (x *MemBuffer) ReadOnlyData() []byte {
@@ -34,7 +39,7 @@ func (x *MemBuffer) ReadOnlyData() []byte {
 }
 
 // Len returns final length of the underlying message. Len returns undefined
-// value before [MemBuffer.Finalize].
+// value before [MemBuffer.SetBounds]/[MemBuffer.Finalize].
 //
 // Len implements [mem.Buffer].
 func (x *MemBuffer) Len() int {

pkg/services/object/get/exec.go
@@ -61,6 +61,9 @@ type execCtx struct {
 	// If an error occurs after that, the stream is already corrupted and
 	// no retry should be attempted.
 	headerWritten bool
+
+	localGetBuffer         []byte
+	submitLocalGetStreamFn SubmitStreamFunc
 }
 
 type execOption func(*execCtx)
@@ -101,6 +104,13 @@ func withPreSortedContainerNodes(nodeLists [][]netmap.NodeInfo, repRules []uint)
 	return func(ctx *execCtx) {
 		ctx.nodeLists = nodeLists
 		ctx.repRules = repRules
+	}
+}
+
+func withLocalGetBuffer(buf []byte, submitStreamFn SubmitStreamFunc) execOption {
+	return func(ctx *execCtx) {
+		ctx.localGetBuffer = buf
+		ctx.submitLocalGetStreamFn = submitStreamFn
 	}
 }
 

pkg/services/object/get/get.go
@@ -37,7 +37,8 @@ func (s *Service) Get(ctx context.Context, prm Prm) error {
 	}
 
 	if len(repRules) > 0 { // REP format does not require encoding
-		err := s.get(ctx, prm.commonPrm, withPreSortedContainerNodes(nodeLists[:len(repRules)], repRules)).err
+		bufOpt := withLocalGetBuffer(prm.localGetBuffer, prm.submitLocalGetStreamFn)
+		err := s.get(ctx, prm.commonPrm, withPreSortedContainerNodes(nodeLists[:len(repRules)], repRules), bufOpt).err
 		if len(ecRules) == 0 || !errors.Is(err, apistatus.ErrObjectNotFound) {
 			return err
 		}

pkg/services/object/get/local.go
@@ -30,7 +30,10 @@ func (exec *execCtx) executeLocal() {
 	case err == nil:
 		exec.status = statusOK
 		exec.err = nil
-		exec.writeCollectedObject()
+
+		if exec.collectedHeader != nil || exec.collectedReader != nil {
+			exec.writeCollectedObject()
+		}
 	case errors.Is(err, apistatus.Error):
 		if errors.Is(err, apistatus.ErrObjectNotFound) {
 			exec.status = statusNotFound

pkg/services/object/get/prm.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"hash"
+	"io"
 
 	coreclient "github.com/nspcc-dev/neofs-node/pkg/core/client"
 	"github.com/nspcc-dev/neofs-node/pkg/services/object/internal"
@@ -12,9 +13,15 @@ import (
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
 )
 
+// SubmitStreamFunc is a callback for partially read object stream.
+type SubmitStreamFunc = func(int, io.ReadCloser)
+
 // Prm groups parameters of Get service call.
 type Prm struct {
 	commonPrm
+
+	localGetBuffer         []byte
+	submitLocalGetStreamFn SubmitStreamFunc
 }
 
 // RangePrm groups parameters of GetRange service call.
@@ -151,4 +158,19 @@ func (p *HeadPrm) SetHeaderWriter(w internal.HeaderWriter) {
 func (p *HeadPrm) WithBuffer(buffer []byte, submitLenFn func(int)) {
 	p.buffer = buffer
 	p.submitLenFn = submitLenFn
+}
+
+// WithBuffer specifies a buffer into which header of the requested object is
+// optionally written. The submitStreamFn parameter is a callback for number of
+// bytes written and stream of remaining bytes. If buffer is unused,
+// submitStreamFn is not called. The stream must be finally closed by the
+// caller.
+func (p *Prm) WithBuffer(buffer []byte, submitStreamFn SubmitStreamFunc) {
+	p.localGetBuffer = buffer
+	p.submitLocalGetStreamFn = submitStreamFn
+}
+
+// GetBuffer returns buffer settings set using [Prm.WithBuffer].
+func (p Prm) GetBuffer() ([]byte, SubmitStreamFunc) {
+	return p.localGetBuffer, p.submitLocalGetStreamFn
 }

commit 7a6ec616628b110c7a42c25163d282a063248849

sn/object: Fix missing metrics in local GET
Regression from a70cb4d51b5dd92be38091d57cd57ba2c7244d67.

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

pkg/services/object/server.go
@@ -1255,6 +1255,13 @@ func (s *Server) copyGetStream(gStream protoobject.ObjectService_GetServer, hdrR
 		chunkRespBuf, chunkBuf = getBufferForChunkGetResponse()
 	}
 
+	var sent int
+	defer func() {
+		if sent > 0 {
+			s.metrics.AddGetPayload(sent)
+		}
+	}()
+
 	for first := true; ; first = false {
 		n, err := io.ReadFull(stream, chunkBuf[prereadPldLen:])
 		streamDone := errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF)
@@ -1272,7 +1279,8 @@ func (s *Server) copyGetStream(gStream protoobject.ObjectService_GetServer, hdrR
 				return fmt.Errorf("parse payload field tag: %w", err)
 			}
 
-			bodyf = shiftPayloadChunkInGetResponseBuffer(chunkRespBuf.SliceBuffer, maxChunkOffsetInGetResponse+prereadPldLen, n-prereadPldLen)
+			n -= prereadPldLen
+			bodyf = shiftPayloadChunkInGetResponseBuffer(chunkRespBuf.SliceBuffer, maxChunkOffsetInGetResponse+prereadPldLen, n)
 			prereadPldLen = 0
 		} else if n == 0 {
 			chunkRespBuf.Free()
@@ -1282,7 +1290,7 @@ func (s *Server) copyGetStream(gStream protoobject.ObjectService_GetServer, hdrR
 		}
 
 		if needSignResp {
-			n, err = s.signResponse(chunkRespBuf.SliceBuffer[bodyf.To:], chunkRespBuf.SliceBuffer[bodyf.ValueFrom:bodyf.To], nil)
+			n, err := s.signResponse(chunkRespBuf.SliceBuffer[bodyf.To:], chunkRespBuf.SliceBuffer[bodyf.ValueFrom:bodyf.To], nil)
 			if err != nil {
 				chunkRespBuf.Free()
 				return fmt.Errorf("sign chunk response: %w", err)
@@ -1291,8 +1299,12 @@ func (s *Server) copyGetStream(gStream protoobject.ObjectService_GetServer, hdrR
 		}
 
 		chunkRespBuf.SetBounds(bodyf.From, bodyf.To)
-		if err = gStream.SendMsg(chunkRespBuf); err != nil || streamDone {
+		if err = gStream.SendMsg(chunkRespBuf); err != nil {
 			return err
+		}
+		sent += n
+		if streamDone {
+			return nil
 		}
 
 		chunkRespBuf, chunkBuf = getBufferForChunkGetResponse()

pkg/services/object/get_test.go
@@ -35,7 +35,7 @@ func TestServer_Get_Local(t *testing.T) {
 	signer := usertest.User()
 	cnr := cidtest.ID()
 	var fsChain nopFSChain
-	var mtrc nopMetrics
+	mtrc := new(metricsCollector)
 	var aclChecker nopACLChecker
 	var reqInfoExt nopReqInfoExtractor
 
@@ -66,7 +66,7 @@ func TestServer_Get_Local(t *testing.T) {
 
 			require.NoError(t, storage.Put(obj, nil))
 
-			assertGetOK(t, srv, *obj, signer)
+			assertGetOK(t, srv, mtrc, *obj, signer)
 
 			t.Run("cut payload in header", func(t *testing.T) {
 				handlers.mockObject = obj.Marshal()
@@ -74,7 +74,7 @@ func TestServer_Get_Local(t *testing.T) {
 				handlers.mockHeaderLen = len(obj.CutPayload().Marshal())
 
 				t.Run("no payload", func(t *testing.T) {
-					assertGetOK(t, srv, *obj, signer)
+					assertGetOK(t, srv, mtrc, *obj, signer)
 				})
 
 				if pldLen == 0 {
@@ -86,11 +86,11 @@ func TestServer_Get_Local(t *testing.T) {
 					for range tagLen {
 						handlers.mockHeaderLen++
 					}
-					assertGetOK(t, srv, *obj, signer)
+					assertGetOK(t, srv, mtrc, *obj, signer)
 				})
 
 				handlers.mockHeaderLen = len(handlers.mockObject) - 1
-				assertGetOK(t, srv, *obj, signer)
+				assertGetOK(t, srv, mtrc, *obj, signer)
 			})
 
 			handlers.mockObject = nil
@@ -123,7 +123,7 @@ func TestServer_Get_Local(t *testing.T) {
 		}
 		signGetRequest(t, req, signer)
 
-		assertGetRequest(t, srv, req, part)
+		assertGetRequest(t, srv, mtrc, req, part)
 
 		handlerFSChain.ecRules = nil
 	})
@@ -194,25 +194,25 @@ func callGet(t *testing.T, srv *Server, req *protoobject.GetRequest) (grpc.Serve
 	return protoobject.NewObjectServiceClient(c).Get(context.Background(), req)
 }
 
-func assertGetOK(t *testing.T, srv *Server, obj object.Object, signer neofscrypto.Signer) {
+func assertGetOK(t *testing.T, srv *Server, mtrc *metricsCollector, obj object.Object, signer neofscrypto.Signer) {
 	t.Run("signed responses", func(t *testing.T) {
-		resps := assertGetOKVersioned(t, srv, obj, signer, version.New(2, 17))
+		resps := assertGetOKVersioned(t, srv, mtrc, obj, signer, version.New(2, 17))
 		for _, resp := range resps {
 			require.NotNil(t, resp.VerifyHeader)
 			require.NoError(t, neofscrypto.VerifyResponseWithBuffer(resp, nil))
 		}
 	})
 
-	resps := assertGetOKVersioned(t, srv, obj, signer, version.Current())
+	resps := assertGetOKVersioned(t, srv, mtrc, obj, signer, version.Current())
 	require.False(t, slices.ContainsFunc(resps, func(resp *protoobject.GetResponse) bool { return resp.VerifyHeader != nil }))
 }
 
-func assertGetOKVersioned(t *testing.T, srv *Server, obj object.Object, signer neofscrypto.Signer, ver version.Version) []*protoobject.GetResponse {
+func assertGetOKVersioned(t *testing.T, srv *Server, mtrc *metricsCollector, obj object.Object, signer neofscrypto.Signer, ver version.Version) []*protoobject.GetResponse {
 	req := newLocalGetRequest(t, ver, obj.Address(), signer)
-	return assertGetRequest(t, srv, req, obj)
+	return assertGetRequest(t, srv, mtrc, req, obj)
 }
 
-func assertGetRequest(t *testing.T, srv *Server, req *protoobject.GetRequest, obj object.Object) []*protoobject.GetResponse {
+func assertGetRequest(t *testing.T, srv *Server, mtrc *metricsCollector, req *protoobject.GetRequest, obj object.Object) []*protoobject.GetResponse {
 	stream, err := callGet(t, srv, req)
 	require.NoError(t, err)
 
@@ -230,6 +230,9 @@ func assertGetRequest(t *testing.T, srv *Server, req *protoobject.GetRequest, ob
 	}
 
 	assertGetStreamResponses(t, obj, resps)
+
+	require.EqualValues(t, obj.PayloadSize(), mtrc.getPayloadLen())
+	mtrc.reset()
 
 	return resps
 }

pkg/services/object/server_test.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"io"
 	"path/filepath"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -205,6 +206,23 @@ type nopMetrics struct{}
 func (nopMetrics) HandleOpExecResult(stat.Method, bool, time.Duration) {}
 func (nopMetrics) AddPutPayload(int)                                   {}
 func (nopMetrics) AddGetPayload(int)                                   {}
+
+type metricsCollector struct {
+	nopMetrics
+	getPayloadCounter atomic.Uint64
+}
+
+func (x *metricsCollector) getPayloadLen() uint64 {
+	return x.getPayloadCounter.Load()
+}
+
+func (x *metricsCollector) reset() {
+	x.getPayloadCounter.Store(0)
+}
+
+func (x *metricsCollector) AddGetPayload(n int) {
+	x.getPayloadCounter.Add(uint64(n))
+}
 
 type testFSChain struct {
 	nopFSChain