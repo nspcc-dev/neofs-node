489d9f58b438fd22f5321f27ad93260cf274e6e1
Andrey Butusov <andrey@nspcc.io>
2026-04-14 18:50:32 +0300

    metadata: add `PreprocessSearchQuery` benchmarks
    Signed-off-by: Andrey Butusov <andrey@nspcc.io>

--- pkg/core/object/metadata_bench_test.go
+++ pkg/core/object/metadata_bench_test.go
@@ -1,11 +1,14 @@
 package objectcore
 
 import (
+	"encoding/base64"
 	"fmt"
+	"math/big"
 	"strconv"
 	"testing"
 
 	"github.com/nspcc-dev/neofs-sdk-go/client"
+	"github.com/nspcc-dev/neofs-sdk-go/object"
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
 	"github.com/stretchr/testify/require"
 )
@@ -20,6 +23,182 @@ const (
 	benchMediumObjectSize       = 512 * 1024
 	benchLargeObjectSize  int64 = 8 * 1024 * 1024 * 1024
 )
+
+func BenchmarkPreprocessSearchQuery(b *testing.B) {
+	const (
+		minSigned256Dec      = "-115792089237316195423570985008687907853269984665640564039457584007913129639935"
+		minSigned256PlusOne  = "-115792089237316195423570985008687907853269984665640564039457584007913129639934"
+		maxSigned256Dec      = "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+		maxSigned256MinusOne = "115792089237316195423570985008687907853269984665640564039457584007913129639934"
+		maxUint64Dec         = "18446744073709551615"
+		largePositive256Dec  = "340282366920938463463374607431768211455"
+		largeNegative256Dec  = "-18446744073709551615"
+	)
+
+	var oidCursor oid.ID
+	for i := range oidCursor {
+		oidCursor[i] = byte(i + 1)
+	}
+
+	var intCursorID oid.ID
+	for i := range intCursorID {
+		intCursorID[i] = byte(255 - i)
+	}
+
+	cursorIntKey := make([]byte, len("attr")+attributeDelimiterLen+intValLen+oid.Size)
+	off := copy(cursorIntKey, "attr")
+	off += copy(cursorIntKey[off:], MetaAttributeDelimiter)
+	copy(cursorIntKey[off:], BigIntBytes(big.NewInt(123)))
+	copy(cursorIntKey[off+intValLen:], intCursorID[:])
+
+	nonIntCursorKey := make([]byte, len("attr")+attributeDelimiterLen+len("hello")+attributeDelimiterLen+oid.Size)
+	off = copy(nonIntCursorKey, "attr")
+	off += copy(nonIntCursorKey[off:], MetaAttributeDelimiter)
+	off += copy(nonIntCursorKey[off:], "hello")
+	off += copy(nonIntCursorKey[off:], MetaAttributeDelimiter)
+	copy(nonIntCursorKey[off:], intCursorID[:])
+
+	for _, tc := range []struct {
+		name      string
+		fs        object.SearchFilters
+		attrs     []string
+		cursor    string
+		wantError error
+	}{
+		{
+			name:   "listing_initial",
+			cursor: "",
+		},
+		{
+			name:   "listing_cursor",
+			cursor: base64.StdEncoding.EncodeToString(oidCursor[:]),
+		},
+		{
+			name:  "string_eq_initial",
+			fs:    stringFilters("attr", "hello", object.MatchStringEqual),
+			attrs: []string{"attr"},
+		},
+		{
+			name:  "string_prefix_initial",
+			fs:    stringFilters("attr", "he", object.MatchCommonPrefix),
+			attrs: []string{"attr"},
+		},
+		{
+			name:  "int_small_ge_initial",
+			fs:    numFilters("attr", "123", object.MatchNumGE),
+			attrs: []string{"attr"},
+		},
+		{
+			name:   "int_small_ge_cursor",
+			fs:     numFilters("attr", "123", object.MatchNumGE),
+			attrs:  []string{"attr"},
+			cursor: base64.StdEncoding.EncodeToString(cursorIntKey),
+		},
+		{
+			name:  "int_min_ge_automatch",
+			fs:    numFilters("attr", minSigned256Dec, object.MatchNumGE),
+			attrs: []string{"attr"},
+		},
+		{
+			name:      "int_min_lt_unreachable",
+			fs:        numFilters("attr", minSigned256Dec, object.MatchNumLT),
+			attrs:     []string{"attr"},
+			wantError: ErrUnreachableQuery,
+		},
+		{
+			name:  "int_min_plus_one_ge",
+			fs:    numFilters("attr", minSigned256PlusOne, object.MatchNumGE),
+			attrs: []string{"attr"},
+		},
+		{
+			name:  "int_max_le_automatch",
+			fs:    numFilters("attr", maxSigned256Dec, object.MatchNumLE),
+			attrs: []string{"attr"},
+		},
+		{
+			name:      "int_max_gt_unreachable",
+			fs:        numFilters("attr", maxSigned256Dec, object.MatchNumGT),
+			attrs:     []string{"attr"},
+			wantError: ErrUnreachableQuery,
+		},
+		{
+			name:  "int_max_minus_one_le",
+			fs:    numFilters("attr", maxSigned256MinusOne, object.MatchNumLE),
+			attrs: []string{"attr"},
+		},
+		{
+			name:  "int_max_uint64_ge",
+			fs:    numFilters("attr", maxUint64Dec, object.MatchNumGE),
+			attrs: []string{"attr"},
+		},
+		{
+			name:  "int_large_positive_ge",
+			fs:    numFilters("attr", largePositive256Dec, object.MatchNumGE),
+			attrs: []string{"attr"},
+		},
+		{
+			name:  "int_large_negative_ge",
+			fs:    numFilters("attr", largeNegative256Dec, object.MatchNumGE),
+			attrs: []string{"attr"},
+		},
+		{
+			name:   "non_int_eq_cursor",
+			fs:     stringFilters("attr", "hello", object.MatchStringEqual),
+			attrs:  []string{"attr"},
+			cursor: base64.StdEncoding.EncodeToString(nonIntCursorKey),
+		},
+		{
+			name:  "mixed_numeric_same_attr",
+			fs:    mixedNumericSameAttrFilters(),
+			attrs: []string{"attr"},
+		},
+		{
+			name:  "mixed_numeric_and_string",
+			fs:    mixedNumericAndStringFilters(),
+			attrs: []string{"attr"},
+		},
+	} {
+		b.Run(tc.name, func(b *testing.B) {
+			b.ReportAllocs()
+			b.ResetTimer()
+
+			for b.Loop() {
+				_, _, err := PreprocessSearchQuery(tc.fs, tc.attrs, tc.cursor)
+				if tc.wantError != nil {
+					require.ErrorIs(b, err, tc.wantError)
+				} else {
+					require.NoError(b, err)
+				}
+			}
+		})
+	}
+}
+
+func numFilters(attr, val string, op object.SearchMatchType) object.SearchFilters {
+	var fs object.SearchFilters
+	fs.AddFilter(attr, val, op)
+	return fs
+}
+
+func stringFilters(attr, val string, op object.SearchMatchType) object.SearchFilters {
+	var fs object.SearchFilters
+	fs.AddFilter(attr, val, op)
+	return fs
+}
+
+func mixedNumericSameAttrFilters() object.SearchFilters {
+	var fs object.SearchFilters
+	fs.AddFilter("attr", "123456789", object.MatchNumGE)
+	fs.AddFilter("attr", "999999999999999999", object.MatchNumLT)
+	return fs
+}
+
+func mixedNumericAndStringFilters() object.SearchFilters {
+	var fs object.SearchFilters
+	fs.AddFilter("attr", "123456789", object.MatchNumGE)
+	fs.AddFilter("tag", "hello", object.MatchStringEqual)
+	return fs
+}
 
 func BenchmarkMergeSearchResults(b *testing.B) {
 	for _, lim := range []uint16{1, 2, 1000, 10000} {
