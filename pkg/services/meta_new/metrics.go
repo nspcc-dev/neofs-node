c5d74c2cf7ac97c06a95ec4cd454117f5d1b9a8e
Pavel Karpy <carpawell@nspcc.ru>
2026-04-24 20:32:02 +0300

    make metrics more descriptive
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/services/meta_new/metrics.go
+++ pkg/services/meta_new/metrics.go
@@ -26,12 +26,14 @@ func (m *Meta) addMetrics() {
 			Subsystem: subsystem,
 			Name:      "obj_accept_time",
 			Help:      "Time b/w object is sent to chain and transaction with it is received, seconds",
+			Buckets:   []float64{0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.15},
 		}),
 		objAcceptBlocks: prometheus.NewHistogram(prometheus.HistogramOpts{
 			Namespace: nameSpace,
 			Subsystem: subsystem,
 			Name:      "obj_accept_blocks",
 			Help:      "Number of blocks b/w object is sent to chain and transaction with it is received, blocks",
+			Buckets:   []float64{0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 		}),
 	}
 
