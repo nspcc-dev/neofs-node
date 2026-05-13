nspcc-dev/neofs-node

commit 6d306486f3549c443003541d602745f340e61671

client: drop NodeInfo and associated
It converts multiaddresses from netmap into URI addresses which is only needed
when establishing a connection. This doesn't have to be done before connection
attempt, there is zero value added in the process, only useless error cases,
so leave parsing only for node/cli and connection cache where normalization
should happen.

This fixes #3565, a single conversion when establishing a connection is not
a problem.

Signed-off-by: Roman Khimov <roman@nspcc.ru>

pkg/services/object/put/distributed.go
@@ -13,11 +13,9 @@ import (
 
 	iec "github.com/nspcc-dev/neofs-node/internal/ec"
 	islices "github.com/nspcc-dev/neofs-node/internal/slices"
-	"github.com/nspcc-dev/neofs-node/pkg/core/client"
 	netmapcore "github.com/nspcc-dev/neofs-node/pkg/core/netmap"
 	objectcore "github.com/nspcc-dev/neofs-node/pkg/core/object"
 	chaincontainer "github.com/nspcc-dev/neofs-node/pkg/morph/client/container"
-	"github.com/nspcc-dev/neofs-node/pkg/network"
 	"github.com/nspcc-dev/neofs-node/pkg/services/meta"
 	svcutil "github.com/nspcc-dev/neofs-node/pkg/services/object/util"
 	"github.com/nspcc-dev/neofs-node/pkg/util"
@@ -86,7 +84,7 @@ type distributedTarget struct {
 type nodeDesc struct {
 	local           bool
 	placementVector int
-	info            client.NodeInfo
+	info            netmap.NodeInfo
 }
 
 // errIncompletePut is returned if processing on a container fails.
@@ -679,7 +677,7 @@ func (t *distributedTarget) sendObject(obj object.Object, encObj encodedObject, 
 		// These should technically be errors, but we don't have
 		// a complete implementation now, so errors are substituted with logs.
 		var l = t.placementIterator.log.With(zap.Stringer("oid", obj.GetID()),
-			zap.Stringer("node", node.info.AddressGroup()))
+			zap.String("node", addressLogString(node.info)))
 
 		sigs, err := decodeSignatures(sigsRaw)
 		if err != nil {
@@ -770,9 +768,8 @@ type placementIterator struct {
 }
 
 type nodeResult struct {
-	convertErr error
-	desc       nodeDesc
-	succeeded  bool
+	desc      nodeDesc
+	succeeded bool
 }
 
 type nodeCounters struct {
@@ -851,9 +848,9 @@ func repToNode(l *zap.Logger, prog *repProgress, pubKeyStr string, listInd int, 
 	if err != nil {
 		prog.lastRespErr.Store(err)
 		if listInd >= 0 {
-			svcutil.LogServiceError(l, "PUT", nr.desc.info.AddressGroup(), err)
+			svcutil.LogServiceError(l, "PUT", nr.desc.info.NetworkEndpoints(), err)
 		} else {
-			svcutil.LogServiceError(l, "PUT (extra broadcast)", nr.desc.info.AddressGroup(), err)
+			svcutil.LogServiceError(l, "PUT (extra broadcast)", nr.desc.info.NetworkEndpoints(), err)
 		}
 		return
 	}
@@ -902,29 +899,12 @@ func (x placementIterator) handleREPRule(l *zap.Logger, prog *repProgress, listI
 				continue
 			}
 			if nr.desc.local = x.neoFSNet.IsLocalNodePublicKey(pk); !nr.desc.local {
-				nr.desc.info, nr.convertErr = convertNodeInfo(nodeList[j])
+				nr.desc.info = nodeList[j]
 			}
 			prog.nodeResultsMtx.Lock()
 			prog.nodeResults[pks] = nr
 			prog.nodeResultsMtx.Unlock()
-			if nr.convertErr == nil {
-				prog.nextNodeGroupKeys = append(prog.nextNodeGroupKeys, pks)
-				continue
-			}
-			// critical error that may ultimately block the storage service. Normally it
-			// should not appear because entry into the network map under strict control
-			l.Error("failed to decode network endpoints of the storage node from the network map, skip the node",
-				zap.String("public key", netmap.StringifyPublicKey(nodeList[j])), zap.Error(nr.convertErr))
-			if minReps > prog.nodesCounters[listInd].stored {
-				minRequired = minReps - prog.nodesCounters[listInd].stored
-			}
-			if listLen-prog.nodesCounters[listInd].processed-1 < minRequired { // -1 includes current node failure
-				err := fmt.Errorf("%w (last node error: failed to decode network addresses: %w)",
-					errNotEnoughNodes{listIndex: listInd, required: minRequired, left: listLen - prog.nodesCounters[listInd].processed - 1},
-					nr.convertErr)
-				return prog.nodesCounters[listInd].stored, false, errIncompletePut{singleErr: err}
-			}
-			// continue to try the best to save required number of replicas
+			prog.nextNodeGroupKeys = append(prog.nextNodeGroupKeys, pks)
 		}
 		for j := range prog.nextNodeGroupKeys {
 			pks := prog.nextNodeGroupKeys[j]
@@ -985,18 +965,12 @@ broadcast:
 				continue
 			}
 			if nr.desc.local = x.neoFSNet.IsLocalNodePublicKey(pk); !nr.desc.local {
-				nr.desc.info, nr.convertErr = convertNodeInfo(nodeLists[i][j])
+				nr.desc.info = nodeLists[i][j]
 			}
 			prog.nodeResultsMtx.Lock()
 			prog.nodeResults[pks] = nr
 			prog.nodeResultsMtx.Unlock()
-			if nr.convertErr != nil {
-				// critical error that may ultimately block the storage service. Normally it
-				// should not appear because entry into the network map under strict control
-				l.Error("failed to decode network endpoints of the storage node from the network map, skip the node",
-					zap.String("public key", netmap.StringifyPublicKey(nodeLists[i][j])), zap.Error(nr.convertErr))
-				continue // to send as many replicas as possible
-			}
+
 			if nr.desc.local {
 				prog.wg.Go(func() { repToNode(l, prog, pks, -1, nr, f) })
 				continue
@@ -1014,15 +988,4 @@ broadcast:
 	}
 	prog.wg.Wait()
 	return nil
-}
-
-func convertNodeInfo(nodeInfo netmap.NodeInfo) (client.NodeInfo, error) {
-	var res client.NodeInfo
-	var endpoints network.AddressGroup
-	if err := endpoints.FromNodeInfo(nodeInfo); err != nil {
-		return res, err
-	}
-	res.SetAddressGroup(endpoints)
-	res.SetPublicKey(nodeInfo.PublicKey())
-	return res, nil
 }

cmd/neofs-node/object.go
@@ -174,7 +174,7 @@ func (fn *innerRingFetcherWithNotary) InnerRingKeys() ([][]byte, error) {
 
 type coreClientConstructor reputationClientConstructor
 
-func (x *coreClientConstructor) Get(ctx context.Context, info coreclient.NodeInfo) (coreclient.MultiAddressClient, error) {
+func (x *coreClientConstructor) Get(ctx context.Context, info netmapsdk.NodeInfo) (coreclient.MultiAddressClient, error) {
 	c, err := (*reputationClientConstructor)(x).Get(ctx, info)
 	if err != nil {
 		return nil, err
@@ -384,7 +384,7 @@ type reputationClientConstructor struct {
 	trustStorage *truststorage.Storage
 
 	basicConstructor interface {
-		Get(context.Context, coreclient.NodeInfo) (coreclient.MultiAddressClient, error)
+		Get(context.Context, netmapsdk.NodeInfo) (coreclient.MultiAddressClient, error)
 	}
 }
 
@@ -460,7 +460,7 @@ func (c *reputationClient) ObjectSearchInit(ctx context.Context, containerID cid
 	return res, err
 }
 
-func (c *reputationClientConstructor) Get(ctx context.Context, info coreclient.NodeInfo) (coreclient.Client, error) {
+func (c *reputationClientConstructor) Get(ctx context.Context, info netmapsdk.NodeInfo) (coreclient.Client, error) {
 	cl, err := c.basicConstructor.Get(ctx, info)
 	if err != nil {
 		return nil, err

cmd/neofs-node/reputation/common/remote.go
@@ -14,7 +14,7 @@ import (
 )
 
 type clientCache interface {
-	Get(context.Context, client.NodeInfo) (client.MultiAddressClient, error)
+	Get(context.Context, netmap.NodeInfo) (client.MultiAddressClient, error)
 }
 
 // clientKeyRemoteProvider must provide a remote writer and take into account
@@ -82,14 +82,7 @@ func (rtp *remoteTrustProvider) InitRemote(ctx context.Context, srv netmap.NodeI
 		return trustcontroller.SimpleWriterProvider(new(NopReputationWriter)), nil
 	}
 
-	var info client.NodeInfo
-
-	err := client.NodeInfoFromRawNetmapElement(&info, srv)
-	if err != nil {
-		return nil, fmt.Errorf("parse client node info: %w", err)
-	}
-
-	c, err := rtp.clientCache.Get(ctx, info)
+	c, err := rtp.clientCache.Get(ctx, srv)
 	if err != nil {
 		return nil, fmt.Errorf("could not initialize API client: %w", err)
 	}

cmd/neofs-node/transport.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 
-	coreclient "github.com/nspcc-dev/neofs-node/pkg/core/client"
 	apistatus "github.com/nspcc-dev/neofs-sdk-go/client/status"
+	"github.com/nspcc-dev/neofs-sdk-go/netmap"
 	protoobject "github.com/nspcc-dev/neofs-sdk-go/proto/object"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/encoding"
@@ -19,7 +19,7 @@ type transport struct {
 
 // SendReplicationRequestToNode connects to described node and sends prepared
 // replication request message to it.
-func (x *transport) SendReplicationRequestToNode(ctx context.Context, req []byte, node coreclient.NodeInfo) ([]byte, error) {
+func (x *transport) SendReplicationRequestToNode(ctx context.Context, req []byte, node netmap.NodeInfo) ([]byte, error) {
 	c, err := x.clients.Get(ctx, node)
 	if err != nil {
 		return nil, fmt.Errorf("connect to remote node: %w", err)

pkg/core/client/client.go
@@ -2,10 +2,8 @@ package client
 
 import (
 	"context"
-	"encoding/hex"
 	"io"
 
-	"github.com/nspcc-dev/neofs-node/pkg/network"
 	"github.com/nspcc-dev/neofs-sdk-go/client"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
 	neofscrypto "github.com/nspcc-dev/neofs-sdk-go/crypto"
@@ -40,40 +38,4 @@ type MultiAddressClient interface {
 	// ForAnyGRPCConn executes op over gRPC connections to given multi-address
 	// endpoint-by-endpoint until success.
 	ForAnyGRPCConn(context.Context, func(context.Context, *grpc.ClientConn) error) error
-}
-
-// NodeInfo groups information about a NeoFS storage node needed for Client construction.
-type NodeInfo struct {
-	addrGroup network.AddressGroup
-
-	key []byte
-}
-
-// SetAddressGroup sets a group of network addresses.
-func (x *NodeInfo) SetAddressGroup(v network.AddressGroup) {
-	x.addrGroup = v
-}
-
-// AddressGroup returns a group of network addresses.
-func (x NodeInfo) AddressGroup() network.AddressGroup {
-	return x.addrGroup
-}
-
-// SetPublicKey sets a public key in a binary format.
-//
-// Argument must not be mutated.
-func (x *NodeInfo) SetPublicKey(v []byte) {
-	x.key = v
-}
-
-// PublicKey returns a public key in a binary format.
-//
-// Result must not be mutated.
-func (x *NodeInfo) PublicKey() []byte {
-	return x.key
-}
-
-// String implements fmt.Stringer using the node public key.
-func (x NodeInfo) String() string {
-	return hex.EncodeToString(x.key)
 }

pkg/core/client/util.go
@@ -1,25 +1,0 @@
-package client
-
-import (
-	"fmt"
-
-	"github.com/nspcc-dev/neofs-node/pkg/network"
-	"github.com/nspcc-dev/neofs-sdk-go/netmap"
-)
-
-// NodeInfoFromRawNetmapElement fills NodeInfo structure from the interface of raw netmap member's descriptor.
-//
-// Args must not be nil.
-func NodeInfoFromRawNetmapElement(dst *NodeInfo, ni netmap.NodeInfo) error {
-	var a network.AddressGroup
-
-	err := a.FromNodeInfo(ni)
-	if err != nil {
-		return fmt.Errorf("parse network address: %w", err)
-	}
-
-	dst.SetPublicKey(ni.PublicKey())
-	dst.SetAddressGroup(a)
-
-	return nil
-}

pkg/innerring/processors/netmap/nodevalidation/availability/validator.go
@@ -111,6 +111,7 @@ func compareNodeInfos(niExp, niGot netmap.NodeInfo) error {
 const pingTimeout = 15 * time.Second
 
 func createSDKClient(e string) (*client.Client, error) {
+	// FIXME: pending removal in #3982.
 	var a network.Address
 	err := a.FromString(e)
 	if err != nil {

pkg/network/cache/clients.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
+	"iter"
 	"maps"
 	"slices"
 	"sync"
@@ -79,9 +80,8 @@ func snCacheKey(pub []byte) string { return string(pub) }
 // Get initializes connections to network addresses of described SN and returns
 // interface to access them. All opened connections are cached and kept alive
 // until [Clients.CloseAll].
-func (x *Clients) Get(ctx context.Context, info clientcore.NodeInfo) (clientcore.MultiAddressClient, error) {
-	pub := info.PublicKey()
-	cacheKey := snCacheKey(pub)
+func (x *Clients) Get(ctx context.Context, info netmap.NodeInfo) (clientcore.MultiAddressClient, error) {
+	cacheKey := snCacheKey(info.PublicKey())
 
 	x.mtx.RLock()
 	c, ok := x.conns[cacheKey]
@@ -96,7 +96,7 @@ func (x *Clients) Get(ctx context.Context, info clientcore.NodeInfo) (clientcore
 		return c, nil
 	}
 
-	c, err := x.initConnections(ctx, pub, info.AddressGroup())
+	c, err := x.initConnections(ctx, info.PublicKey(), info.NetworkEndpoints())
 	if err != nil {
 		return nil, fmt.Errorf("init connections: %w", err)
 	}
@@ -138,21 +138,13 @@ func (x *Clients) syncWithNetmapSN(ctx context.Context, sn netmap.NodeInfo) erro
 		return nil
 	}
 
-	as := make(network.AddressGroup, 0, sn.NumberOfNetworkEndpoints())
-	var a network.Address
-	for ma := range sn.NetworkEndpoints() {
-		if err := a.FromString(ma); err != nil {
-			// TODO: if at least one address is OK, SN can be operational
-			return fmt.Errorf("parse network address %q: %w", ma, err)
-		}
-		as = append(as, a)
-	}
+	var as = slices.Collect(sn.NetworkEndpoints())
 
 	conns.mtx.Lock()
 	defer conns.mtx.Unlock()
 
 	maps.DeleteFunc(conns.m, func(ma string, c *client.Client) bool {
-		if slices.ContainsFunc(as, func(a network.Address) bool { return a.String() == ma }) {
+		if slices.Contains(as, ma) {
 			return false
 		}
 		if err := c.Close(); err != nil {
@@ -165,13 +157,12 @@ func (x *Clients) syncWithNetmapSN(ctx context.Context, sn netmap.NodeInfo) erro
 		return true
 	})
 
-	for i := range as {
-		ma := as[i].String()
+	for _, ma := range as {
 		if _, ok := conns.m[ma]; ok {
 			continue
 		}
 		x.log.Info("initializing connection to new SN address in the new network map...", zap.String("address", ma))
-		c, err := x.initConnection(ctx, pub, as[i].URIAddr())
+		c, err := x.initConnection(ctx, pub, ma)
 		if err != nil {
 			x.log.Info("failed to init connection to new SN address in the new network map",
 				zap.String("address", ma), zap.Error(err))
@@ -184,22 +175,21 @@ func (x *Clients) syncWithNetmapSN(ctx context.Context, sn netmap.NodeInfo) erro
 	return nil
 }
 
-func (x *Clients) initConnections(ctx context.Context, pub []byte, as network.AddressGroup) (*connections, error) {
-	m := make(map[string]*client.Client, len(as))
+func (x *Clients) initConnections(ctx context.Context, pub []byte, addrs iter.Seq[string]) (*connections, error) {
+	m := make(map[string]*client.Client)
 	l := x.log.With(zap.String("public key", hex.EncodeToString(pub)))
-	for i := range as {
-		cacheKey := as[i].String()
-		l.Info("initializing connection to the SN...", zap.String("address", cacheKey))
-		c, err := x.initConnection(ctx, pub, as[i].URIAddr())
+	for s := range addrs {
+		l.Info("initializing connection to the SN...", zap.String("address", s))
+		c, err := x.initConnection(ctx, pub, s)
 		if err != nil {
 			// TODO: if at least one address is OK, SN can be operational
 			for cl := range maps.Values(m) {
 				_ = cl.Close()
 			}
-			return nil, fmt.Errorf("init conn to %q: %w", as[i], err)
+			return nil, fmt.Errorf("init conn to %q: %w", s, err)
 		}
-		l.Info("connection to the SN successfully initialized", zap.String("address", cacheKey))
-		m[cacheKey] = c
+		l.Info("connection to the SN successfully initialized", zap.String("address", s))
+		m[s] = c
 	}
 	var hexKey = hex.EncodeToString(pub)
 	return &connections{
@@ -210,7 +200,13 @@ func (x *Clients) initConnections(ctx context.Context, pub []byte, as network.Ad
 }
 
 func (x *Clients) initConnection(ctx context.Context, pub []byte, uri string) (*client.Client, error) {
-	target, withTLS, err := uriutil.Parse(uri)
+	// FIXME: pending removal in #3982.
+	var a network.Address
+	if err := a.FromString(uri); err != nil {
+		return nil, fmt.Errorf("parse network address %q: %w", uri, err)
+	}
+
+	target, withTLS, err := uriutil.Parse(a.URIAddr())
 	if err != nil {
 		return nil, fmt.Errorf("parse URI: %w", err)
 	}

commit 281b2156297630bee3e760801689553eb67741a3

put: fix panic on local write failure log
Regression from 6d306486f3549c443003541d602745f340e61671:

    [signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0xf07580]

    goroutine 74173 [running]:
    slices.AppendSeq[...](...)
    	slices/iter.go:51
    slices.Collect[...](...)
    	slices/iter.go:60
    github.com/nspcc-dev/neofs-node/pkg/services/object/util.LogServiceError(0xc003438500, {0x13ea04f, 0x3}, 0x0, {0x1661c20, 0xc005871700})
    	github.com/nspcc-dev/neofs-node/pkg/services/object/util/log.go:13 +0x100
    github.com/nspcc-dev/neofs-node/pkg/services/object/put.(*distributedTarget).distributeObjectWithMeta(_, {{0x69, 0xc7, 0xf4, 0x97, 0xe9, 0xf2, 0x44, 0xa4, 0x5, ...}, ...}, ...)
    	github.com/nspcc-dev/neofs-node/pkg/services/object/put/distributed.go:544 +0x31a
    github.com/nspcc-dev/neofs-node/pkg/services/object/put.(*distributedTarget).distributeObject(_, {{0x69, 0xc7, 0xf4, 0x97, 0xe9, 0xf2, 0x44, 0xa4, 0x5, ...}, ...}, ...)
    	github.com/nspcc-dev/neofs-node/pkg/services/object/put/distributed.go:531 +0x1b2
    github.com/nspcc-dev/neofs-node/pkg/services/object/put.(*distributedTarget).saveObject(_, {{0x69, 0xc7, 0xf4, 0x97, 0xe9, 0xf2, 0x44, 0xa4, 0x5, ...}, ...}, ...)
    	github.com/nspcc-dev/neofs-node/pkg/services/object/put/distributed.go:182 +0x165
    github.com/nspcc-dev/neofs-node/pkg/services/object/put.(*distributedTarget).Close(0xc000cba8c0)
    	github.com/nspcc-dev/neofs-node/pkg/services/object/put/distributed.go:169 +0x307
    github.com/nspcc-dev/neofs-node/pkg/services/object/put.(*validatingTarget).Close(0xc0035baa00?)
    	github.com/nspcc-dev/neofs-node/pkg/services/object/put/validation.go:155 +0x93
    github.com/nspcc-dev/neofs-node/pkg/services/object/put.(*Streamer).Close(0xc001eb30e0)
    	github.com/nspcc-dev/neofs-node/pkg/services/object/put/streamer.go:303 +0x4b
    github.com/nspcc-dev/neofs-node/pkg/services/object.(*putStream).close(0x1661540?)

pkg/services/object/put/distributed.go
@@ -540,8 +540,7 @@ func (t *distributedTarget) distributeObjectWithMeta(obj object.Object, encObj e
 
 		err = t.writeObjectLocally(obj, encObj)
 		if err != nil {
-			err = fmt.Errorf("write object locally: %w", err)
-			svcutil.LogServiceError(l, "PUT", nil, err)
+			svcutil.LogServiceError(l, "PUT", slices.Values([]string{"local"}), err)
 		}
 	} else {
 		err = placementFn(obj, encObj)