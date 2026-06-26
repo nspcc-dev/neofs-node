b044549800f154160cff000d7f4b4f675911b6a0
Andrey Butusov <andrey@nspcc.io>
2026-06-26 20:06:38 +0300

    fstree: limit ranged payload stream from buffered prefix
    FSTree range streams could return more bytes than requested when a non-zero
    range offset started inside the already buffered payload prefix and the
    requested range also ended there. This made ranged GET over split objects
    occasionally fail with payload size overflow.
    
    Closes #4049.
    
    Signed-off-by: Andrey Butusov <andrey@nspcc.io>

--- pkg/local_object_storage/blobstor/fstree/fstree.go
+++ pkg/local_object_storage/blobstor/fstree/fstree.go
@@ -676,6 +676,11 @@ func (t *FSTree) shiftPayloadRangeStream(prefix []byte, pldLen uint64, pldFldOff
 	}
 
 	prefix = prefix[off:]
+	if ln <= uint64(len(prefix)) {
+		stream.Close()
+		return nopCloser(bytes.NewReader(prefix[:ln])), nil
+	}
+
 	return newPrefixedReadSeekCloser(prefix, &limitedFileReader{ReadSeekCloser: stream, limit: int64(ln) - int64(len(prefix))}), nil
 }
 
