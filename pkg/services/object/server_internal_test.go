05260d57eae347ea7fa8401016fab3630aa2a646
Andrey Butusov <andrey@nspcc.io>
2026-08-28 11:55:52 +0300

    object: query enough EC nodes during search
    Use the candidate-buffer-aware node count for EC Search requests. Previously,
    Search queried only the minimum required number of randomly selected EC nodes,
    ignoring additional candidate-buffer nodes. This could produce incomplete and
    request-dependent object lists. Add coverage for EC and replica placement
    vectors.
    
    Closes #4145.
    
    Signed-off-by: Andrey Butusov <andrey@nspcc.io>

--- pkg/services/object/server_internal_test.go
+++ pkg/services/object/server_internal_test.go
@@ -3,6 +3,8 @@ package object
 import (
 	"testing"
 
+	iec "github.com/nspcc-dev/neofs-node/internal/ec"
+	"github.com/nspcc-dev/neofs-sdk-go/netmap"
 	protoobject "github.com/nspcc-dev/neofs-sdk-go/proto/object"
 	protosession "github.com/nspcc-dev/neofs-sdk-go/proto/session"
 	"github.com/stretchr/testify/require"
@@ -24,4 +26,79 @@ func TestShouldSignOutgoingRequest(t *testing.T) {
 	require.True(t, shouldSignOutgoingRequest(mutuallyAuthenticatedClient(true), new(protoobject.GetRequest)))
 	require.True(t, shouldSignOutgoingRequest(mutuallyAuthenticatedClient(false), requestWithTTL(1)))
 	require.True(t, shouldSignOutgoingRequest(struct{}{}, requestWithTTL(1)))
+}
+
+func TestIterateSearchableContainerNodes(t *testing.T) {
+	for _, tc := range []struct {
+		name     string
+		nodeSets [][]netmap.NodeInfo
+		repRules []uint
+		ecRules  []iec.Rule
+		allNodes bool
+		expected int
+	}{
+		{
+			name:     "EC with candidate buffer",
+			nodeSets: [][]netmap.NodeInfo{make([]netmap.NodeInfo, 7)},
+			ecRules:  []iec.Rule{{DataPartNum: 3, ParityPartNum: 1}},
+			expected: 5,
+		},
+		{
+			name:     "EC with smaller candidate buffer",
+			nodeSets: [][]netmap.NodeInfo{make([]netmap.NodeInfo, 6)},
+			ecRules:  []iec.Rule{{DataPartNum: 3, ParityPartNum: 1}},
+			expected: 4,
+		},
+		{
+			name:     "EC without candidate buffer",
+			nodeSets: [][]netmap.NodeInfo{make([]netmap.NodeInfo, 4)},
+			ecRules:  []iec.Rule{{DataPartNum: 3, ParityPartNum: 1}},
+			expected: 2,
+		},
+		{
+			name:     "EC candidate set smaller than required",
+			nodeSets: [][]netmap.NodeInfo{make([]netmap.NodeInfo, 1)},
+			ecRules:  []iec.Rule{{DataPartNum: 3, ParityPartNum: 1}},
+			expected: 1,
+		},
+		{
+			name:     "all EC nodes requested",
+			nodeSets: [][]netmap.NodeInfo{make([]netmap.NodeInfo, 7)},
+			ecRules:  []iec.Rule{{DataPartNum: 3, ParityPartNum: 1}},
+			allNodes: true,
+			expected: 7,
+		},
+		{
+			name: "replica nodes are not reduced",
+			nodeSets: [][]netmap.NodeInfo{
+				make([]netmap.NodeInfo, 7),
+				make([]netmap.NodeInfo, 7),
+			},
+			repRules: []uint{2},
+			ecRules:  []iec.Rule{{DataPartNum: 3, ParityPartNum: 1}},
+			expected: 12,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			var got int
+			iterateSearchableContainerNodes(tc.nodeSets, tc.repRules, tc.ecRules, tc.allNodes, func(netmap.NodeInfo) bool {
+				got++
+				return true
+			})
+			require.Equal(t, tc.expected, got)
+		})
+	}
+
+	t.Run("stops when callback returns false", func(t *testing.T) {
+		var got int
+		iterateSearchableContainerNodes(
+			[][]netmap.NodeInfo{make([]netmap.NodeInfo, 7)}, nil,
+			[]iec.Rule{{DataPartNum: 3, ParityPartNum: 1}}, false,
+			func(netmap.NodeInfo) bool {
+				got++
+				return got < 3
+			},
+		)
+		require.Equal(t, 3, got)
+	})
 }
