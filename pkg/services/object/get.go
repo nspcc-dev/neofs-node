nspcc-dev/neofs-node

commit 6916f133622293d9ca3883bc129b5c4e1fa7dd63

sn/object: Use default gRPC buffer pool for GET EC requests
Follow 60d9d35be282f97b497d056875e7e6ba76aaf47b.

Signed-off-by: Leonard Liubich <leonard@morphbits.io>

pkg/services/object/get.go
@@ -33,6 +33,8 @@ import (
 	"google.golang.org/protobuf/encoding/protowire"
 )
 
+var getRequestBufferPool = mem.DefaultBufferPool()
+
 var (
 	getStreamDesc = &grpc.StreamDesc{
 		StreamName:    "Get",
@@ -324,7 +326,7 @@ func (x *getProxyContext) handleChunkResponse(streamProg *getStreamProgress, res
 type preparedRangeRequest struct {
 	offset uint64
 	length uint64
-	buffer mem.Buffer
+	buffer *[]byte
 }
 
 type getECTransport struct {
@@ -334,7 +336,7 @@ type getECTransport struct {
 	signResponses    bool
 	responseStream   grpc.ServerStream
 
-	getPartRequest          mem.Buffer
+	getPartRequest          *[]byte
 	getPartRequestRuleIndex int
 
 	getPartRangeRequestsMtx sync.RWMutex
@@ -525,11 +527,15 @@ func (x *getECTransport) CopyECParentHeaderAndPayloadFromRemoteFirstPart(ctx con
 			var err error
 			copiedHdr, parentPldLen, partPldLen, copiedPartPld, err = x.copyRemotePart(ctx, conn)
 			if err != nil {
+				getRequestBufferPool.Put(x.getPartRequest)
 				return err
 			}
 
-			if copiedHdr && copiedPartPld == partPldLen {
-				return nil
+			if copiedHdr {
+				getRequestBufferPool.Put(x.getPartRequest)
+				if copiedPartPld == partPldLen {
+					return nil
+				}
 			}
 
 			return clientcore.ErrSkipConnection
@@ -567,7 +573,7 @@ func (x *getECTransport) copyRemotePart(ctx context.Context, conn *grpc.ClientCo
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	stream, err := callGet(ctx, conn, x.getPartRequest)
+	stream, err := callGet(ctx, conn, mem.SliceBuffer(*x.getPartRequest))
 	if err != nil {
 		err = igrpc.ConvertContextStatus(err)
 		if errors.Is(err, ctx.Err()) {
@@ -673,10 +679,19 @@ func (x *getECTransport) copyRemotePartRange(ctx context.Context, conn *grpc.Cli
 		return 0, fmt.Errorf("make request: %w", err)
 	}
 
+	copied, err := x.copyRemotePartRangeWithRequest(ctx, conn, request, ln, controlCh)
+	if err != nil || copied == ln {
+		getRequestBufferPool.Put(request)
+	}
+
+	return copied, err
+}
+
+func (x *getECTransport) copyRemotePartRangeWithRequest(ctx context.Context, conn *grpc.ClientConn, request *[]byte, ln uint64, controlCh <-chan bool) (uint64, error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	stream, err := callGet(ctx, conn, request)
+	stream, err := callGet(ctx, conn, mem.SliceBuffer(*request))
 	if err != nil {
 		err = igrpc.ConvertContextStatus(err)
 		if errors.Is(err, ctx.Err()) {
@@ -883,7 +898,7 @@ func (x *getECTransport) CopyRemoteECPartRange(ctx context.Context, conn clientc
 	return copiedPld, nil
 }
 
-func (s *Server) makeGetECPartRequest(remoteServerAPIVersion version.Version, cnr cid.ID, parent oid.ID, partInfo iec.PartInfo, rngOff, rngLen uint64, payloadOnly bool) (mem.Buffer, error) {
+func (s *Server) makeGetECPartRequest(remoteServerAPIVersion version.Version, cnr cid.ID, parent oid.ID, partInfo iec.PartInfo, rngOff, rngLen uint64, payloadOnly bool) (*[]byte, error) {
 	ruleIdxStr := strconv.Itoa(partInfo.RuleIndex)
 	partIdxStr := strconv.Itoa(partInfo.Index)
 	xHdrs := []string{
@@ -901,8 +916,8 @@ func (s *Server) makeGetECPartRequest(remoteServerAPIVersion version.Version, cn
 
 	reqLen := protoencoding.CalculateRequestBodyWithMetaHeaderLength(bodyLen, metaHdrLen) + verifHdrFldLen
 
-	// TODO: try with sync.Pool
-	buf := make([]byte, reqLen)
+	bufItem := getRequestBufferPool.Get(reqLen)
+	buf := *bufItem
 
 	// body
 	off := protoencoding.WriteRequestBodyTagAndLength(buf, bodyLen)
@@ -918,10 +933,10 @@ func (s *Server) makeGetECPartRequest(remoteServerAPIVersion version.Version, cn
 		return nil, err
 	}
 
-	return mem.SliceBuffer(buf), nil
+	return bufItem, nil
 }
 
-func (x *getECTransport) makeGetECPartRangeRequest(remoteServerAPIVersion version.Version, partInfo iec.PartInfo, off, ln uint64) (mem.Buffer, error) {
+func (x *getECTransport) makeGetECPartRangeRequest(remoteServerAPIVersion version.Version, partInfo iec.PartInfo, off, ln uint64) (*[]byte, error) {
 	x.getPartRangeRequestsMtx.RLock()
 	req := x.getPartRangeRequests[partInfo]
 	x.getPartRangeRequestsMtx.RUnlock()

commit 26ed4c1b60ed425ed77a838df2229bc3bf41281a

sn/object: Fix too early EC part range request return to buffer pool
Regression from 6916f133622293d9ca3883bc129b5c4e1fa7dd63.

EC GET handler manages request buffers for part ranges from buffer pool.
If buffer is no longer needed in the context of the current request, it
is returned to the pool.

Previously, the return criterion was false if the entire range was
requested with length=0 and 0 bytes were received without a fatal error.
This could lead to a data race with various consequences.

Problem was inconsistent, so reproduction was pretty challenging: it was
necessary to turn off the node storing one EC part at the right time.

It could happen that the buffer would be filled with another range of
another EC part, which in turn would lead to incorrect final data.
Example from CLI:
```
Error: rpc error: copy payload: payload size overflow
```

Also, following panic was caught at least once:
```
panic: too short buffer [recovered, repanicked]

goroutine 120984 [running]:
sync.(*WaitGroup).Go.func1.1()
	sync/waitgroup.go:251 +0x48
panic({0x10609e0c0?, 0x106341150?})
	runtime/panic.go:860 +0x12c
github.com/nspcc-dev/neofs-sdk-go/proto/encoding.MarshalToBytes[...]({0x3501635e62bb?, 0x3d, 0x45}, 0x6390b2e8, {0x350163a345a0?, 0x41, 0x300000010655eb90})
	github.com/nspcc-dev/neofs-sdk-go@v1.0.0-rc.22/proto/encoding/encoding.go:205 +0xd8
github.com/nspcc-dev/neofs-sdk-go/proto/refs.WriteSignature[...]({0x3501635e6298, 0x60, 0x68}, {0x3501637788d0, 0x41?, 0x35016390b378?}, {0x350163a345a0?, 0x41, 0x41}, 0x0)
	github.com/nspcc-dev/neofs-sdk-go@v1.0.0-rc.22/proto/refs/encoding.go:250 +0xa0
github.com/nspcc-dev/neofs-sdk-go/proto/refs.WriteSignatureField[...]({0x3501635e6296, 0x62, 0x6a}, 0x5, {0x3501637788d0, 0x21, 0x21}, {0x350163a345a0, 0x41, 0x41}, ...)
	github.com/nspcc-dev/neofs-sdk-go@v1.0.0-rc.22/proto/refs/encoding.go:243 +0xf0

pkg/services/object/get.go
@@ -683,7 +683,7 @@ func (x *getECTransport) copyRemotePartRange(ctx context.Context, conn *grpc.Cli
 	}
 
 	copied, err := x.copyRemotePartRangeWithRequest(ctx, conn, request, ln, controlCh)
-	if err != nil || copied == ln {
+	if err != nil || (ln > 0 && copied == ln) {
 		defaultGRPCBufferPool.Put(request)
 	}
 